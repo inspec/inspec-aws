33077d33e42d93bf6f53dabb0497b046917c08c6
Stuart Paterson <spaterson@chef.io>
2018-12-03 15:12:20 +0000

    Remove unnecessary output parameter.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- test/integration/build/outputs.tf
+++ test/integration/build/outputs.tf
@@ -6,15 +6,9 @@ output "aws_default_vpc_id" {
   value = "${data.aws_vpc.default.id}"
 }
 
-output "aws_default_vpc_subnet_id" {
-  value = "${data.aws_subnet.}"
-}
-
-
 output "aws_instance_id" {
   value = "${aws_instance.linux_ubuntu_vm.*.id}"
 }
-
 
 output "aws_ec2_ami_id" {
   value = "${data.aws_ami.linux_ubuntu.id}"
