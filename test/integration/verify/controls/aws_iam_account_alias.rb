adcbdc617cf6d5f4479bc73bf83069b2ae1564b3
Ruairi Fennell <rfennell@chef.io>
2019-03-01 11:57:49 +0000

    Update aws_iam_account_alias test
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- test/integration/verify/controls/aws_iam_account_alias.rb
+++ test/integration/verify/controls/aws_iam_account_alias.rb
@@ -4,6 +4,7 @@ control 'aws-iam-account-alias-1.0' do
   title 'Test the AWS IAM Account Alias'
 
   describe aws_iam_account_alias do
-    it { should_not exist }
+    it            { should exist }
+    its('alias')  { should_not be_nil}
   end
 end
