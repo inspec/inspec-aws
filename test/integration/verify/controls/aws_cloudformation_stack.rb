537f4b47fa04d71568d5325d24900c13daa8f27f
Darren Murray <dmurray@chef.io>
2019-06-26 15:20:11 +0100

    fix alignment in control
    Signed-off-by: Darren Murray <dmurray@chef.io>

--- test/integration/verify/controls/aws_cloudformation_stack.rb
+++ test/integration/verify/controls/aws_cloudformation_stack.rb
@@ -8,8 +8,8 @@ control 'aws-cloudformation-stack-1.0' do
   title 'Ensure AWS CloudFormation Stack has the correct properties.'
 
   describe aws_cloudformation_stack(stack_name: aws_cloudformation_stack_name) do
-    it                         { should exist }
-    its ('stack_name')  { should eq aws_cloudformation_stack_name }
-    its ('stack_status')  { should eq 'CREATE_COMPLETE' }
+    it                   { should exist }
+    its ('stack_name')   { should eq aws_cloudformation_stack_name }
+    its ('stack_status') { should eq 'CREATE_COMPLETE' }
   end
 end
