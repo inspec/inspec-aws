12ef1d81a7e0bffb9b3a79c794770feabf5ea880
darren weeks <darren.weeks@iress.com>
2019-09-13 08:49:41 +0100

    basic tests for validating hosted zones

--- libraries/aws_hosted_zones.rb
+++ libraries/aws_hosted_zones.rb
@@ -5,10 +5,34 @@ class AwsHostedZones < AwsResourceBase
   desc 'Verifies hosted zones settings are correct'
 
   example "
-    to be completed
+    describe aws_hosted_zones() do
+      it { should exist }
+      its ('name') { should include ('carry-on.films.com'))}
+    end
   "
+  attr_reader :table
 
+  FilterTable.create
+              .register_column(:name, field: :name)
+              .register_column(:id, field: :id) 
+              .install_filter_methods_on_resource(self, :table)
+
+              
   def initialize(opts = {})
-    print ("Hello")
+    super(opts)
+
+    zones = []
+    catch_aws_errors do
+      resp =@aws.route53_client.list_hosted_zones()
+
+      resp.hosted_zones.each do | zone | 
+        zones += [{
+          name: zone.name,
+          id: zone.id
+        }]
+      end      
+    end
+
+    @table = zones
   end
 end
