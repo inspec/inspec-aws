f087fecb73a3037aca6b18f8e38b9bea6e3c144e
darren weeks <darren.weeks@iress.com>
2019-09-13 07:49:07 +0100

    new class for hosted zones

---
+++ libraries/aws_hosted_zones.rb
@@ -0,0 +1,14 @@
+require 'aws_backend'
+
+class AwsHostedZones < AwsResourceBase
+  name 'aws_hosted_zones'
+  desc 'Verifies hosted zones settings are correct'
+
+  example "
+    to be completed
+  "
+
+  def initialize(opts = {})
+    print ("Hello")
+  end
+end
