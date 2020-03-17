59500163ed1db5a1cc9822d1d3ffe5a4c64d3353
darren weeks <darren.weeks@iress.com>
2020-03-17 16:14:24 +0000

    lambda and lambda now have unit tests, next step functional tests

---
+++ libraries/aws_lambda.rb
@@ -0,0 +1,39 @@
+require 'aws_backend'
+
+class AwsLambda < AwsResourceBase
+  name 'aws_lambda'
+  desc 'Verifies the settings for a set of lambda'
+  example '
+    describe aws_lambda("lambda_fred") do
+      ....
+    end
+  '
+
+  def initialize(opts = {})
+    opts = { lambda_name: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: [:lambda_name])
+    fetch_data (opts[:lambda_name])
+  end
+
+  def fetch_data(target)
+    @display_name = target
+    details = @aws.lambda_client.get_function({ function_name: target })
+    @lambda = details.configuration.to_h
+    create_resource_methods(@lambda)
+  rescue Aws::Errors::ServiceError
+    @lambda = nil
+  end
+
+  def exists?
+    !@lambda.nil? && !@lambda.empty?
+  end
+
+  def name
+    @display_name
+  end
+
+  def to_s
+    "Lambda #{name}"
+  end
+end
