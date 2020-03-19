4695c0ab6ce7aac892a4c572bf9f79f569f75115
darren weeks <darren.weeks@iress.com>
2020-03-19 09:55:44 +0000

    documentation and integration test update.
    Signed-off-by: darren weeks <darren.weeks@iress.com>

---
+++ test/integration/verify/controls/aws_lambda.rb
@@ -0,0 +1,15 @@
+title 'AWS Lambda compliance Tests'
+
+
+control "Check that lambda is correctly configured" do
+  impact  1.0
+  title   "Lambda tests"
+  desc    "Ensure that our lambda is correctly deployed"
+
+  describe aws_lambda("test_Lambda") do
+    it { should exist}    
+    its ('handler') { should eq 'main.on_event'}
+    its ('version') { should eq '$LATEST' }
+    its ('runtime') { should eq 'python3.7' }
+  end
+end
