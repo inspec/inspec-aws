e673a8d4a5d4c440c2c696821e8a10cd4eb7bdb7
Krystan Honour <krystan@gmail.com>
2020-03-25 13:28:50 +0000

    fixed id type in test, transitgateway not kms key
    Signed-off-by: Krystan Honour <krystan@gmail.com>

--- test/integration/verify/controls/aws_transit_gateway.rb
+++ test/integration/verify/controls/aws_transit_gateway.rb
@@ -1,13 +1,13 @@
 title 'Test AWS Transit Gateway'
 
-aws_kms_key_enabled_id = attribute(:aws_transit_gateway_id, default: '', description: 'The AWS Transit Gateway ID.')
+aws_transit_gateway_id = attribute(:aws_transit_gateway_id, default: '', description: 'The AWS Transit Gateway ID.')
 
 control 'aws-transit-gateway-1.0' do
 
   impact 1.0
   title 'Ensure AWS Transit gateway is configured correctly.'
 
-  describe aws_transit_gateway(transit_gateway_id: aws_kms_key_enabled_id) do
+  describe aws_transit_gateway(transit_gateway_id: aws_transit_gateway_id) do
     it { should exist }
   end
 
