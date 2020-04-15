f0badc8df5d777a3a80cb87768d26a15ac6bb8eb
Omer Demirok <odemirok@chef.io>
2020-04-15 13:21:35 +0100

    Ignore efs identifier unless hash or string type
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- libraries/aws_efs_file_system.rb
+++ libraries/aws_efs_file_system.rb
@@ -21,6 +21,7 @@ class AwsEfsFileSystem < AwsResourceBase
   def initialize(opts = {})
     # Assume the argument is a file_system_id when it is a string type object and reject any other type.
     opts = {file_system_id: opts} if opts.is_a?(String)
+    opts = {} unless opts.is_a?(Hash)
 
     # File system identifier can be either the file_system_id or the creation_token.
     if opts[:file_system_id] && !opts[:file_system_id].empty?
@@ -28,7 +29,7 @@ class AwsEfsFileSystem < AwsResourceBase
       file_system_arguments = {file_system_id: opts[:file_system_id]}
     elsif opts[:creation_token] && !opts[:creation_token].empty?
       raise ArgumentError, "#{@__resource_name__}: creation_token must be minimum 1 and maximum 64-character long." unless opts[:creation_token].length.between?(1, 64)
-      file_system_arguments = { creation_token: opts[:creation_token]}
+      file_system_arguments = {creation_token: opts[:creation_token]}
     else
       raise ArgumentError, "#{@__resource_name__}:  `file_system_id` or `creation_token` must be provided."
     end
