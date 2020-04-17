22e28065b4a6a2c43afab06b0f7d4d387deda4a9
Omer Demirok <odemirok@chef.io>
2020-04-17 17:07:10 +0100

    Unit tests for EFS singular and plural resources
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- test/unit/resources/aws_efs_file_system_test.rb
+++ test/unit/resources/aws_efs_file_system_test.rb
@@ -4,6 +4,7 @@ require "aws-sdk-core"
 require_relative 'mock/aws_efs_file_system_mock'
 
 class AwsEfsFileSystemConstructorTest < Minitest::Test
+
   def test_empty_params_not_ok
     assert_raises(ArgumentError) { AwsEfsFileSystem.new() }
   end
@@ -12,8 +13,8 @@ class AwsEfsFileSystemConstructorTest < Minitest::Test
     assert_raises(ArgumentError) { AwsEfsFileSystem.new(1234) }
   end
 
-  def test_rejects_invalid_fs_id
-    assert_raises(ArgumentError) { AwsEfsFileSystem.new('fs-123') }
+  def test_rejects_invalid_f_s_id
+    assert_raises(ArgumentError) { AwsEfsFileSystem.new('f_s-123') }
   end
 
   def test_rejects_long_creation_token
@@ -21,101 +22,91 @@ class AwsEfsFileSystemConstructorTest < Minitest::Test
   end
 
   def test_rejects_unrecognized_identifiers
-    assert_raises(ArgumentError) { AwsEfsFileSystem.new(fs_id: 'fs-12345678') }
+    assert_raises(ArgumentError) { AwsEfsFileSystem.new(rubbish_id: 'fs-12345678') }
   end
 
   def test_accepts_file_system_id_as_identifier
-    assert AwsEfsFileSystem.new(file_system_id: 'fs-12345678')
+    assert AwsEfsFileSystem.new(file_system_id: 'fs-12345678', client_args: { stub_responses: true })
   end
 
   def test_accepts_creation_token_as_identifier
-    assert AwsEfsFileSystem.new(creation_token: 'my_token')
+    assert AwsEfsFileSystem.new(creation_token: 'my_token', client_args: { stub_responses: true })
   end
 
   def test_file_system_non_existing_creation_token
-    refute AwsEfsFileSystem.new(creation_token: 'my_token').exists?
+    refute AwsEfsFileSystem.new(creation_token: 'my_token', client_args: { stub_responses: true }).exists?
   end
 
-  def test_file_system_non_existing_fs_id
-    refute AwsEfsFileSystem.new(file_system_id: 'fs-12345678').exists?
+  def test_file_system_non_existing_f_s_id
+    refute AwsEfsFileSystem.new(file_system_id: 'fs-12345678', client_args: { stub_responses: true }).exists?
   end
 end
 
 class AwsEfsFileSystemTest < Minitest::Test
+
   def setup
     # Create a mock file system
-    @mock_fs = AwsEfsFileSystemMock.new.mock
+    number_of_fs = 1
+    @mock_file_system_list = AwsEfsFileSystemMock.new.multiple(number_of_fs)
 
-    # Create stub data in expected format.
+    # Create stub data.
     data = {}
-    data[:data] = { :file_systems => [@mock_fs] }
+    data[:data] = { :file_systems => @mock_file_system_list }
     data[:client] = Aws::EFS::Client
     data[:method] = :describe_file_systems
 
-    # Create a stub file system description with mock file system.
-    @file_system = AwsEfsFileSystem.new(file_system_id: @mock_fs[:file_system_id], client_args: { stub_responses: true }, stub_data: [data])
+    # This is for brevity.
+    @m_f_s = @mock_file_system_list.first
+
+    # Create a stub file system description with mock file system data.
+    @file_system = AwsEfsFileSystem.new(
+        file_system_id: @m_f_s[:file_system_id],
+        client_args: { stub_responses: true },
+        stub_data: [data]
+    )
   end
 
   def test_file_system_exists
     assert @file_system.exists?
   end
 
-  def test_file_system_name
-    assert_equal(@file_system.tags[@mock_fs[:tags][0][:key]], @mock_fs[:tags][0][:value])
+  def test_file_system_tag
+    assert_equal(@file_system.tags[@m_f_s[:tags][0][:key]], @m_f_s[:tags][0][:value])
   end
 
   def test_file_system_id
-    assert_equal(@file_system.file_system_id, @mock_fs[:file_system_id])
+    assert_equal(@file_system.file_system_id, @m_f_s[:file_system_id])
   end
 
   def test_creation_token
-    assert_equal(@file_system.creation_token, @mock_fs[:creation_token])
+    assert_equal(@file_system.creation_token, @m_f_s[:creation_token])
   end
 
   def test_owner_id
-    assert_equal(@file_system.owner_id, @mock_fs[:owner_id])
+    assert_equal(@file_system.owner_id, @m_f_s[:owner_id])
   end
 
   def test_creation_time
-    assert_equal(@file_system.creation_time, @mock_fs[:creation_time])
+    assert_equal(@file_system.creation_time, @m_f_s[:creation_time])
   end
 
   def test_life_cycle_state
-    assert_equal(@file_system.life_cycle_state, @mock_fs[:life_cycle_state])
+    assert_equal(@file_system.life_cycle_state, @m_f_s[:life_cycle_state])
   end
 
   def test_size_in_bytes
-    assert_equal(@file_system.size_in_bytes.value, @mock_fs[:size_in_bytes][:value])
+    assert_equal(@file_system.size_in_bytes.value, @m_f_s[:size_in_bytes][:value])
   end
 
   def test_performance_mode
-    assert_equal(@file_system.performance_mode, @mock_fs[:performance_mode])
+    assert_equal(@file_system.performance_mode, @m_f_s[:performance_mode])
   end
 
   def test_throughput_mode
-    assert_equal(@file_system.throughput_mode, @mock_fs[:throughput_mode])
+    assert_equal(@file_system.throughput_mode, @m_f_s[:throughput_mode])
   end
 
   def test_encrypted
-    assert_equal(@file_system.encrypted, @mock_fs[:encrypted])
+    assert_equal(@file_system.encrypted, @m_f_s[:encrypted])
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test/unit/resources/mock/aws_efs_file_system_mock.rb
+++ test/unit/resources/mock/aws_efs_file_system_mock.rb
@@ -6,24 +6,24 @@ class AwsEfsFileSystemMock < AwsBaseResourceMock
   ENCRYPTED = [true, false].freeze
   THROUGHPUT_MODE = %w(bursting provisioned).freeze
 
-  attr_reader :mock
-
-  def initialize
-    super
-    # Define required properties of a mock file system.
-
-    @mock = {
-        tags: [{ :key => @aws.any_string(4), :value => @aws.any_string }],
-        file_system_id: "fs-#{@aws.any_hex}",
-        creation_token: @aws.any_string,
-        owner_id: @aws.any_int.to_s,
-        creation_time: Time.parse(@aws.any_date.to_s),
-        life_cycle_state: LIFE_CYCLE.sample,
-        number_of_mount_targets: @aws.any_int(1),
-        size_in_bytes: { :value => @aws.any_int(4) },
-        performance_mode: PERFORMANCE_MODE.sample,
-        encrypted: ENCRYPTED.sample,
-        throughput_mode: THROUGHPUT_MODE.sample,
-    }
+  def multiple(n)
+    # Create multiple EFS file systems
+    mocks = []
+    n.times do
+      mocks << {
+          tags: [{ :key => @aws.any_string(4), :value => @aws.any_string }],
+          file_system_id: "fs-#{@aws.any_hex}",
+          creation_token: @aws.any_string,
+          owner_id: @aws.any_int.to_s,
+          creation_time: Time.parse(@aws.any_date.to_s),
+          life_cycle_state: LIFE_CYCLE.sample,
+          number_of_mount_targets: @aws.any_int(1),
+          size_in_bytes: { :value => @aws.any_int(4) },
+          performance_mode: PERFORMANCE_MODE.sample,
+          encrypted: ENCRYPTED.sample,
+          throughput_mode: THROUGHPUT_MODE.sample,
+      }
+    end
+    mocks.freeze
   end
 end
