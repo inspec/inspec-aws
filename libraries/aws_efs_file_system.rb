e7f50d6ac72be65ebb152881feb14c669f193512
Omer Demirok <odemirok@chef.io>
2020-04-20 15:44:10 +0100

    Refactor condition statements
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- libraries/aws_efs_file_system.rb
+++ libraries/aws_efs_file_system.rb
@@ -24,10 +24,10 @@ class AwsEfsFileSystem < AwsResourceBase
     opts = {} unless opts.is_a?(Hash)
 
     # File system identifier can be either the file_system_id or the creation_token.
-    if opts[:file_system_id] && !opts[:file_system_id].empty?
+    if opts.key?(:file_system_id)
       raise ArgumentError, "#{@__resource_name__}: file_system_id must be in the format 'fs-' followed by 8 or 17 hexadecimal characters." unless /^fs\-([0-9a-f]{8})|(^fs\-[0-9a-f]{17})$/.match?(opts[:file_system_id])
       file_system_arguments = { file_system_id: opts[:file_system_id] }
-    elsif opts[:creation_token] && !opts[:creation_token].empty?
+    elsif opts.key?(:creation_token)
       raise ArgumentError, "#{@__resource_name__}: creation_token must be minimum 1 and maximum 64-character long." unless opts[:creation_token].length.between?(1, 64)
       file_system_arguments = { creation_token: opts[:creation_token] }
     else
@@ -35,8 +35,9 @@ class AwsEfsFileSystem < AwsResourceBase
     end
 
     super(opts)
-    validate_parameters(required: [opts.keys[0]])
-    @display_name = opts.values[0]
+    # file_system_arguments has a single item.
+    validate_parameters(required: [file_system_arguments.keys.first])
+    @display_name = file_system_arguments.values.first
 
     # Ensure the response will have a single file system description.
     file_system_arguments[:max_items] = 1
@@ -44,7 +45,7 @@ class AwsEfsFileSystem < AwsResourceBase
     catch_aws_errors do
       begin
         resp = @aws.efs_client.describe_file_systems(file_system_arguments)
-        @file_system = resp.file_systems[0].to_h
+        @file_system = resp.file_systems.first.to_h
         # Invalid creation_token returns empty file_system_description.
         if @file_system.empty?
           # This is equal to FileSystemNotFound exception in this context.
@@ -58,8 +59,7 @@ class AwsEfsFileSystem < AwsResourceBase
   end
 
   def id
-    return nil unless exists?
-    @file_system[:file_system_id]
+    @file_system[:file_system_id] || nil
   end
 
   def tags
