65e623b740dfebce15774e5987819a7ad503498d
Omer Demirok <odemirok@chef.io>
2020-04-24 12:52:41 +0100

    Add contion to prevent nil class method access
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- test/integration/verify/controls/aws_sqs_queue.rb
+++ test/integration/verify/controls/aws_sqs_queue.rb
@@ -1,11 +1,15 @@
 title 'Test single AWS Queue'
 
 arn = attribute(:aws_sqs_queue_arn, default: '', description: 'The AWS SQS Queue arn.')
-modified_arn = arn.dup
-modified_arn.slice!("arn:aws:sqs:")
-url = modified_arn.split(':', 2)
-aws_sqs_queue_url = ""
-aws_sqs_queue_url =  "https://sqs.#{url[0]}.amazonaws.com/#{url[1].sub(':','/')}" if url
+if arn.empty?
+  aws_sqs_queue_url = ""
+else
+  modified_arn = arn.dup
+  modified_arn.slice!("arn:aws:sqs:")
+  url = modified_arn.split(':', 2)
+  aws_sqs_queue_url = ""
+  aws_sqs_queue_url =  "https://sqs.#{url[0]}.amazonaws.com/#{url[1].sub(':','/')}" if url
+end
 
 control 'aws-sqs-queue-1.0' do
   impact 1.0
