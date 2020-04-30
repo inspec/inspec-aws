c19b8fa7e4229756b3de65526721c9af5895b360
Omer Demirok <odemirok@chef.io>
2020-04-30 13:04:10 +0100

    Add integration tests for ECR resources
    Signed-off-by: Omer Demirok <odemirok@chef.io>

---
+++ test/integration/verify/controls/aws_ecr_image.rb
@@ -0,0 +1,12 @@
+aws_ecr_repository_name = attribute(:aws_ecr_repository_name, value: "", description: "The ECR repository identifier.")
+
+title "Test single AWS ECR Image in a repository"
+control "aws-ecr-image-1.0" do
+
+  impact 1.0
+  title "Check AWS ECR image has the correct properties."
+
+  describe aws_ecr_image(repository_name: aws_ecr_repository_name, image_tag: 'latest') do
+    it { should_not exist }
+  end
+end
