d8ba5de76ef0edf2fce35df7e6031ac121aafb97
Murali M <murali@softrams.com>
2020-05-16 08:53:45 -0500

    Added aws_iam_inline_policy #205
    Signed-off-by: Murali Mallina <murali@softrams.com>

--- test/integration/build/aws.tf
+++ test/integration/build/aws.tf
@@ -32,7 +32,6 @@ variable "aws_cloud_watch_alarm_metric_name" {}
 variable "aws_cloud_watch_alarm_name" {}
 variable "aws_cloud_watch_alarm_name_with_dimensions" {}
 variable "aws_cloud_watch_alarm_metric_name_with_dimensions" {}
-variable "aws_cloud_watch_log_group_name" {}
 variable "aws_cloud_watch_log_metric_filter_namespace_with_dimensions" {}
 variable "aws_cloud_watch_log_metric_filter_log_group_name" {}
 variable "aws_cloud_watch_log_metric_filter_metric_name" {}
@@ -87,6 +86,7 @@ variable "aws_enable_creation" {}
 variable "aws_enable_privileged_resources" {}
 variable "aws_flow_log_bucket_name" {}
 variable "aws_iam_group_name" {}
+variable "aws_iam_group_policy_name" {}
 variable "aws_iam_policy_arn" {}
 variable "aws_iam_role_generic_name" {}
 variable "aws_iam_role_generic_policy_name" {}
@@ -228,23 +228,23 @@ resource "aws_ebs_volume" "inspec_ebs_volume" {
 }
 
 resource "aws_efs_file_system" "inspec_efs_file_system" {
-  count             = var.aws_enable_creation
-  creation_token    = var.aws_efs_creation_token
-  encrypted         = var.aws_efs_encrypted
-  throughput_mode   = var.aws_efs_throughput_mode
-  performance_mode  = var.aws_efs_performance_mode
+  count            = var.aws_enable_creation
+  creation_token   = var.aws_efs_creation_token
+  encrypted        = var.aws_efs_encrypted
+  throughput_mode  = var.aws_efs_throughput_mode
+  performance_mode = var.aws_efs_performance_mode
 
   tags = {
-    Name = var.aws_efs_name
+    Name        = var.aws_efs_name
     companyName = var.aws_efs_company_name
   }
 }
 
 resource "aws_efs_file_system" "inspec_efs_file_systems" {
-  count             = var.aws_enable_creation * var.aws_efs_count
-  encrypted         = var.aws_efs_encrypted
-  throughput_mode   = var.aws_efs_throughput_mode
-  performance_mode  = var.aws_efs_performance_mode
+  count            = var.aws_enable_creation * var.aws_efs_count
+  encrypted        = var.aws_efs_encrypted
+  throughput_mode  = var.aws_efs_throughput_mode
+  performance_mode = var.aws_efs_performance_mode
 
   tags = {
     companyName = var.aws_efs_company_name
@@ -939,16 +939,6 @@ resource "aws_cloudtrail" "trail_2" {
 }
 
 # Cloudwatch
-resource "aws_cloudwatch_log_group" "log_group" {
-  count             = var.aws_enable_creation
-  name              = var.aws_cloudwatch_log_group_name
-  retention_in_days = 7
-
-  tags = {
-    Name = var.aws_cloudwatch_log_group_name
-  }
-}
-
 resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
   count          = var.aws_enable_creation
   name           = var.aws_cloud_watch_log_metric_filter_name
@@ -1174,6 +1164,7 @@ EOF
 
 }
 
+
 resource "aws_iam_group" "aws_iam_group_1" {
   count = var.aws_enable_creation
   name  = var.aws_iam_group_name
@@ -1186,6 +1177,28 @@ resource "aws_iam_user_group_membership" "aws_iam_user_group_membership_1" {
   groups = [
     var.aws_iam_group_name,
   ]
+}
+
+resource "aws_iam_group_policy" "iam_group_policy" {
+  count = var.aws_enable_creation
+  name  = var.aws_iam_group_policy_name
+  group = aws_iam_group.aws_iam_group_1[0].name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
 }
 
 resource "aws_iam_policy" "aws_policy_1" {
@@ -1569,7 +1582,7 @@ resource "aws_rds_cluster" "rds_cluster" {
 }
 
 resource "aws_rds_cluster_instance" "instance1" {
-  count               = var.aws_enable_creation
+  count              = var.aws_enable_creation
   apply_immediately  = true
   cluster_identifier = aws_rds_cluster.rds_cluster.0.cluster_identifier
   identifier         = "instance1"
@@ -1577,7 +1590,7 @@ resource "aws_rds_cluster_instance" "instance1" {
 }
 
 resource "aws_rds_cluster_instance" "instance2" {
-  count               = var.aws_enable_creation
+  count              = var.aws_enable_creation
   apply_immediately  = true
   cluster_identifier = aws_rds_cluster.rds_cluster.0.cluster_identifier
   identifier         = "instance2"
@@ -1590,11 +1603,11 @@ resource "aws_ec2_transit_gateway" "gateway" {
 
 data "aws_iam_policy_document" "lambda_test_policy_document" {
   statement {
-    effect    = "Allow"
-    actions   = [
+    effect = "Allow"
+    actions = [
       "cloudwatch:PutMetricData"
     ]
-    resources = ["*"]  
+    resources = ["*"]
   }
 }
 
@@ -1672,14 +1685,14 @@ resource "aws_elasticache_cluster" "inspec_test_elasticache" {
 }
 
 resource "aws_ecr_repository" "inspec_test_ecr_repository" {
-  count = var.aws_enable_creation
-  name = var.aws_ecr_repository_name
+  count                = var.aws_enable_creation
+  name                 = var.aws_ecr_repository_name
   image_tag_mutability = var.aws_ecr_repository_image_tag_mutability
 
   image_scanning_configuration {
     scan_on_push = var.aws_ecr_repository_scan_on_push_enabled
   }
   tags = {
-    Name        = var.aws_ecr_repository_name
+    Name = var.aws_ecr_repository_name
   }
 }
