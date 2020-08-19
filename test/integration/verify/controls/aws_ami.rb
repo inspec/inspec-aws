3d68db0c466a3958fc96cc3d70eb0a2bbfe77075
Sam Scott <sscott@chef.io>
2020-08-19 17:20:32 +0100

    updated integration tests
    Signed-off-by: Sam Scott <sscott@chef.io>

--- test/integration/verify/controls/aws_ami.rb
+++ test/integration/verify/controls/aws_ami.rb
@@ -5,16 +5,12 @@ control 'aws-ami-1.0' do
   impact 1.0
   title 'Ensure AWS AMI has current properties'
 
-  describe aws_ami(image_id: 'ami-0a13d44dccf1f5cf6') do
-    it { should exist }
-    it { should be_public }
-    its('architecture')      { should eq 'x86_64' }
-    its('image_id')          { should eq 'ami-0a13d44dccf1f5cf6' }
-    its('image_type')        { should eq 'machine' }
-    its('platform_details')  { should eq 'Linux/UNIX' }
-    its('image_owner_alias') { should eq 'amazon' }
-    its('usage_operation')   { should eq 'RunInstances' }
-    its('state')             { should eq 'available' }
+  aws_amis(architecture: 'i386').where { image_type == 'machine' }.image_ids.each do |image|
+    describe aws_ami(image_id: image) do
+      it { should exist }
+      its('architecture') { should eq 'i386' }
+      its('image_type')   { should eq 'machine' }
+    end
   end
 
   describe aws_ami(image_id: '000000') do
