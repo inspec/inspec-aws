a7cb1014f8f66b7b399fae01695e726fecca4c4f
Joe McCrea <joe.mccrea@sap.com>
2020-12-01 10:20:32 +0000

    Pushing rubocop fixes
    Signed-off-by: Joe McCrea <joe.mccrea@sap.com>

--- libraries/aws_vpn_connections.rb
+++ libraries/aws_vpn_connections.rb
@@ -15,14 +15,14 @@ class AwsVpnConnections < AwsResourceBase
 
   # FilterTable setup
   FilterTable.create
-              .register_column(:vpn_connection_ids,  field: :vpn_connection_id)
-              .register_column(:vpn_gateway_ids,     field: :vpn_gateway_id)
-              .register_column(:outside_ip_addresses, field: :outside_ip_address)
-              .register_column(:tunnel_inside_cidrs,     field: :tunnel_inside_cidr)
-              .register_column(:states,              field: :state)
-              .register_column(:types,               field: :type)
-              .register_column(:tags,               field: :tags)
-              .install_filter_methods_on_resource(self, :table)
+             .register_column(:vpn_connection_ids, field: :vpn_connection_id)
+             .register_column(:vpn_gateway_ids, field: :vpn_gateway_id)
+             .register_column(:outside_ip_addresses, field: :outside_ip_address)
+             .register_column(:tunnel_inside_cidrs, field: :tunnel_inside_cidr)
+             .register_column(:states, field: :state)
+             .register_column(:types, field: :type)
+             .register_column(:tags, field: :tags)
+             .install_filter_methods_on_resource(self, :table)
 
   def initialize(opts = {})
     super(opts)
@@ -39,8 +39,8 @@ class AwsVpnConnections < AwsResourceBase
     @api_response.vpn_connections.each do |vpn_connection|
       vpn_connection_rows+=[{ vpn_connection_id: vpn_connection.vpn_connection_id,
         vpn_gateway_id: vpn_connection.vpn_gateway_id,
-        outside_ip_address: vpn_connection.options.tunnel_options.map{ |options| options.outside_ip_address },
-        tunnel_inside_cidr: vpn_connection.options.tunnel_options.map{ |options| options.tunnel_inside_cidr },
+        outside_ip_address: vpn_connection.options.tunnel_options.map(&:outside_ip_address),
+        tunnel_inside_cidr: vpn_connection.options.tunnel_options.map(&:tunnel_inside_cidr),
         state: vpn_connection.state,
         type: vpn_connection.type,
         tags: map_tags(vpn_connection.tags) }]
