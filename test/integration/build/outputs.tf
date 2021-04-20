33d9bd462cb3c90518c51e6e0f23e9110b2717f2
Rohit Joshi <rohit.prasad.joshi@sap.com>
2021-04-19 21:26:28 -0700

    fixing typo
    Signed-off-by: Rohit Joshi <rohit.prasad.joshi@sap.com>

--- test/integration/build/outputs.tf
+++ test/integration/build/outputs.tf
@@ -299,5 +299,5 @@ output "aws_elasticache_replication_group_id" {
 }
 
 output "aws_elasticache_replication_group_node_type" {
-  valuse = aws_elasticache_replication_group.replication_group.node_type
+  value = aws_elasticache_replication_group.replication_group.node_type
 }
