33c89b01c5605e9f1489ca1dd2ce0c998144fd6d
Sathish <sbabu@progress.com>
2021-04-29 01:50:27 +0530

    support singular VPN Gateway
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/aws_vpn_gateway.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsVPNGateway < AwsResourceBase
+  name 'aws_vpn_gateway'
+  desc 'Verifies settings for a single AWS VPN Gateway'
+
+  example "
+   describe aws_vpn_gateway(vpn_gateway_id: 'vgw-014aef8a0689b8f43') do
+     it { should exist }
+   end
+
+   describe aws_vpn_gateway('vgw-014aef8a0689b8f43') do
+     it { should exist }
+   end
+  "
+
+  def initialize(opts = {})
+    opts = { vpn_gateway_id: opts } if opts.is_a?(String)
+    super
+    validate_parameters(required: [:vpn_gateway_id])
+    validate_identifier
+    fetch
+  end
+
+  def exists?
+    !failed_resource? || !@response.blank?
+  end
+
+  def to_s
+    "VPN Gateway ID: #{@opts[:vpn_gateway_id]}"
+  end
+
+  def vpc_id
+    return NullResponse if attachments.blank?
+
+    attachments.first.vpc_id
+  end
+
+  def attached?
+    return false if attachments.blank?
+
+    attachments.first.state == 'attached'
+  end
+
+  def detached?
+    !attached?
+  end
+
+  private
+
+  def fetch
+    params = { vpn_gateway_ids: [@opts[:vpn_gateway_id]] }
+    @response = query_with(params)
+    return empty_response_warn unless @response
+
+    create_resource_methods(@response.vpn_gateways.first.to_h)
+  end
+
+  def query_with(params)
+    catch_aws_errors do
+      @aws.compute_client.describe_vpn_gateways(params)
+    end
+  end
+
+  def attachments
+    return [] unless exists?
+
+    @response.vpn_gateways.first.vpc_attachments
+  end
+
+  def validate_identifier
+    raise ArgumentError, 'parameter `vpn_gateway_id` cannot be blank' if @opts[:vpn_gateway_id].blank?
+    raise ArgumentError, 'parameter `vpn_gateway_id` should start with `vgw-` followed by alpha numeric characters' if @opts[:vpn_gateway_id] !~ /^vgw-[a-z0-9]+$/
+  end
+end
