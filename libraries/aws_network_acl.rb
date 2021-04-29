2d71011c43ac928539028cb2959d2357062262c4
Sathish <sbabu@progress.com>
2021-04-30 04:44:49 +0530

    support network ACL(s) resource
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/aws_network_acl.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsNetworkACL < AwsResourceBase
+  name 'aws_network_acl'
+  desc 'Verifies settings for a single AWS Network ACL'
+
+  example "
+   describe aws_network_acl(network_acl_id: '014aef8a0689b8f43') do
+     it { should exist }
+   end
+
+   describe aws_network_acl('014aef8a0689b8f43') do
+     it { should exist }
+   end
+  "
+
+  def initialize(opts = {})
+    opts = { network_acl_id: opts } if opts.is_a?(String)
+    super
+    validate_parameters(required: [:network_acl_id])
+    validate_identifier
+    fetch
+  end
+
+  def exists?
+    !failed_resource? || !@response.blank?
+  end
+
+  def attached?
+    !associations.blank?
+  end
+
+  def detached?
+    !attached?
+  end
+
+  def to_s
+    "Network ACL ID: #{@opts[:network_acl_id]}"
+  end
+
+  private
+
+  def fetch
+    params = { network_acl_ids: [@opts[:network_acl_id]] }
+    @response = query_with(params)
+    return empty_response_warn unless @response
+    
+    network_acl_hash = network_acl.to_h
+    network_acl_hash[:associations] = associations.map do |association|
+      {network_acl_association_id: association.network_acl_association_id, subnet_id: association.subnet_id}
+    end
+    network_acl_hash[:ingress] = entries.select{|entry| !entry.egress }.map(&:to_h)
+    network_acl_hash[:egress] = entries.select{|entry| entry.egress }.map(&:to_h)
+    network_acl_hash.delete(:entries)
+    create_resource_methods(network_acl_hash)
+  end
+
+  def network_acl
+    return unless @response
+
+    @response.network_acls.first
+  end
+
+  def query_with(params)
+    catch_aws_errors do
+      @aws.compute_client.describe_network_acls(params)
+    end
+  end
+
+  def associations
+    return [] unless network_acl
+
+    network_acl.associations
+  end
+
+  def entries
+    return [] unless network_acl
+
+    network_acl.entries
+  end
+
+  def validate_identifier
+    raise ArgumentError, 'parameter `network_acl_id` cannot be blank' if @opts[:network_acl_id].blank?
+    raise ArgumentError, 'parameter `network_acl_id` should start with `vgw-` followed by alpha numeric characters' if @opts[:network_acl_id] !~ /^acl-[a-z0-9]+$/
+  end
+end
---
+++ libraries/aws_network_acls.rb
@@ -0,0 +1,71 @@
+
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsNetworkACLs < AwsResourceBase
+  name 'aws_network_acls'
+  desc 'Verifies settings for a collection of AWS internet gateways.'
+
+  example "
+    describe aws_network_acls do
+      its('count') { should eq 3 }
+    end
+
+    describe aws_network_acls.where(name: 'my-gateway') do
+      it { should exist }
+    end
+  "
+
+  attr_reader :table
+
+  FilterTable.create
+             .register_column(:network_acl_ids, field: :network_acl_id)
+             .register_column(:associations, field: :associations, style: :simple)
+             .register_column(:entries, field: :entries, style: :simple)
+             .register_column(:defaults, field: :defaults, style: :simple)
+             .register_column(:owner_ids, field: :owner_id)
+             .register_column(:tags, field: :tags)
+             .register_column(:vpc_ids, field: :vpc_id)
+             .install_filter_methods_on_resource(self, :table)
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch_data
+  end
+
+  def fetch_data
+    network_acls = []
+    paginate_request do |api_response|
+      network_acls += api_response.network_acls.map do |network_acl|
+        flat_hash_from(network_acl)
+      end
+    end
+    network_acls
+  end
+
+  private
+
+  def paginate_request
+    pagination_options = {max_results: 1000}
+    loop do
+      api_response = catch_aws_errors do
+        @aws.compute_client.describe_network_acls(pagination_options)
+      end
+      return [] if api_response.blank?
+
+      yield api_response
+      break unless api_response.next_token
+      pagination_options = { next_token: api_response.next_token }
+    end
+  end
+
+  def flat_hash_from(network_acl)
+    network_acl_hash = network_acl.to_h
+    network_acl_hash[:defaults] = network_acl_hash.delete(:is_default) ? network_acl.network_acl_id : nil
+    network_acl_hash[:tags] = network_acl.tags.blank? ? nil : map_tags(network_acl.tags)
+    network_acl_hash[:entries] = network_acl_hash[:entries].map{|entry| entry[:network_acl_id] = network_acl.network_acl_id; entry}
+    network_acl_hash
+  end
+end
