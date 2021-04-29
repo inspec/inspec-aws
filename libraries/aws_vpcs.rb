1f3cb3f65b32727d5940d8a8084a13eff5c55c5e
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-04-29 16:54:27 +0530

    Plural resouce tested and working fine.
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/aws_vpcs.rb
+++ libraries/aws_vpcs.rb
@@ -22,11 +22,8 @@ class AwsVpcs < AwsResourceBase
              .register_column(:instance_tenancys,  field: :instance_tenancy)
              .register_column(:is_defaults,        field: :is_default)
              .register_column(:tags,               field: :tags)
-             .register_column(:ipv_6_cidr_block_association_sets,               field: :ipv_6_cidr_block_association_set)
-             .register_column(:cidr_block_association_sets,                     field: :cidr_block_association_set)
-             .register_column(:association_ids, field: :association_id)
-             .register_column(:cidr_blocks, field: :cidr_block)
-             .register_column(:cidr_block_states, field: :cidr_block_state)
+             .register_column(:ipv_6_cidr_block_association_sets,               field: :ipv_6_cidr_block_association_set, style: :simple)
+             .register_column(:cidr_block_association_sets,                     field: :cidr_block_association_set, style: :simple)
              .install_filter_methods_on_resource(self, :table)
 
   def initialize(opts = {})
@@ -35,50 +32,29 @@ class AwsVpcs < AwsResourceBase
     @table = fetch_data
   end
 
-  # def fetch_data
-  #   vpc_rows = []
-  #   catch_aws_errors do
-  #     @vpcs = @aws.compute_client.describe_vpcs.to_h[:vpcs]
-  #   end
-  #   return [] if !@vpcs || @vpcs.empty?
-  #   require 'pry'; binding.pry
-  #   @vpcs.each do |vpc|
-  #     vpc_rows+=[{ vpc_id: vpc[:vpc_id],
-  #                  cidr_block: vpc[:cidr_block],
-  #                  dhcp_options_id: vpc[:dhcp_options_id],
-  #                  state: vpc[:state],
-  #                  is_default: vpc[:is_default],
-  #                  instance_tenancy: vpc[:instance_tenancy],
-  #                  tags: map_tags(vpc[:tags]),
-  #                  cidr_block_association_sets: vpc[:cidr_block_association_set],
-  #                  ipv_6_cidr_block_association_sets: map_ipv_6_cidr_block_association_set(vpc[:ipv_6_cidr_block_association_sets])
-  #     }]
-  #   end
-  #   @table = vpc_rows
-  # end
-
   def fetch_data
-    cidr_block_association_set_rows = []
-    pagination_options = {}
-    loop do
-      catch_aws_errors do
-        @api_response = @aws.compute_client.describe_vpcs(pagination_options)
-      end
-      return [] if !@api_response || @api_response.empty?
-      @api_response.vpcs.each do |res|
-        res.cidr_block_association_set.each do |cidr_block_association|
-          cidr_block_association_set_rows += [{
-                              association_id: cidr_block_association.association_id,
-                              cidr_block: cidr_block_association.cidr_block,
-                              cidr_block_state: cidr_block_association.cidr_block_state
-                            }]
-          # require 'byebug'; byebug
-        end
-        # require 'pry'; binding.pry
-      end
-      break unless @api_response.next_token
-      pagination_options = { next_token: @api_response.next_token }
+    vpc_rows = []
+    catch_aws_errors do
+      @vpcs = @aws.compute_client.describe_vpcs.to_h[:vpcs]
     end
-    @table = cidr_block_association_set_rows
+    return [] if !@vpcs || @vpcs.empty?
+    @vpcs.each do |vpc|
+      vpc_rows+=[{ vpc_id: vpc[:vpc_id],
+                   cidr_block: vpc[:cidr_block],
+                   dhcp_options_id: vpc[:dhcp_options_id],
+                   state: vpc[:state],
+                   is_default: vpc[:is_default],
+                   instance_tenancy: vpc[:instance_tenancy],
+                   tags: map_tags(vpc[:tags]),
+                   cidr_block_association_set: {
+                     cidr_block_association_set_association_ids: vpc[:cidr_block_association_set].first[:association_id],
+                     cidr_block_association_set_statuses: vpc[:cidr_block_association_set].first[:cidr_block_state][:state],
+                     cidr_block_association_set_status_messages: vpc[:cidr_block_association_set].first[:cidr_block_state][:status_message]
+                   },
+                   ipv_6_cidr_block_association_set: {},
+                 }]
+    # @table = vpc_rows
+    end
+    @table = vpc_rows
   end
 end
--- test/integration/verify/controls/aws_vpcs.rb
+++ test/integration/verify/controls/aws_vpcs.rb
@@ -7,7 +7,6 @@ aws_vpc_dhcp_options_id = attribute(:aws_vpc_dhcp_options_id, value: '', descrip
 aws_vpc_name = attribute(:aws_vpc_name, value: '', description: 'The AWS VPC name.')
 
 control 'aws-vpcs-1.0' do
-
   impact 1.0
   title 'Ensure AWS VPC plural resource has the correct properties.'
 
@@ -22,5 +21,24 @@ control 'aws-vpcs-1.0' do
     its('is_defaults')       { should be_in [true, false] }
     its('tags')              { should include('Name' => aws_vpc_name) }
   end
+end
 
+aws_cidr_block_association_set_association_id = attribute("aws_cidr_block_association_set_association_id", default: '', description: 'The association ID for a CIDR block associated with the VPC.')
+aws_cidr_block_association_set_cidr_block_state_state = attribute("aws_cidr_block_association_set_cidr_block_state_state", default: '', description: 'The state of a CIDR block associated with the VPC.')
+aws_cidr_block_association_set_cidr_block_state_status_message = attribute("aws_cidr_block_association_set_cidr_block_state_status_message", default: '', description: 'The status message of a CIDR block associated with the VPC.')
+
+control 'aws-vpcs-1.0' do
+  impact 1.0
+  title 'Ensure AWS VPC CIDR Block plural resource has the correct properties.'
+  describe aws_vpcs do
+    it { should exist }
+    its('count')             { should be <= 100 }
+    its('cidr_blocks')       { should include aws_vpc_cidr_block }
+    its ('ipv_6_cidr_block_association_sets') { should_not be_empty }
+    its ('cidr_block_association_sets') { should_not be_empty }
+    its('cidr_block_association_sets')       { should include(cidr_block_association_set_association_ids: aws_cidr_block_association_set_association_id,
+                                                              cidr_block_association_set_status_messages: aws_cidr_block_association_set_cidr_block_state_state,
+                                                              cidr_block_association_set_statuses: aws_cidr_block_association_set_cidr_block_state_status_message) }
+    its ('ipv_6_cidr_block_association_set') { should be_empty }
+  end
 end
