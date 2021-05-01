4c1ed5a566316c57409a85d849ca881e5c9fa802
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-05-01 23:44:40 +0530

    transit_gateway_route_tables singular and plural integration cases with controls
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/aws_ec2_transit_gateway_route_tables.rb
@@ -0,0 +1,23 @@
+aws_transit_gateway_route_table_id = attribute(:aws_transit_gateway_route_table_id, default: '', description: 'The ID of the transit gateway route table.')
+aws_transit_gateway_id = attribute(:aws_transit_gateway_id, default: '', description: 'The ID of the transit gateway.')
+aws_state = attribute(:aws_state, default: '', description: 'The state of the route table (available | deleting | deleted | pending).')
+aws_default_association_route_table = attribute(:aws_default_association_route_table, default: '', description: 'Indicates whether this is the default association route table for the transit gateway (true | false).')
+aws_default_propagation_route_table = attribute(:aws_default_propagation_route_table, default: '', description: 'Indicates whether this is the default propagation route table for the transit gateway (true | false)')
+aws_creation_time = attribute(:aws_creation_time, default: '', description: 'The creation time of the transit gateway route table.')
+aws_tags = attribute(:aws_tags, default: '', description: 'The tags of the transit gateway route table.')
+
+control 'aws-transit-gateway-route-tables-1.0' do
+    impact 1.0
+    title 'Describes one or more transit gateway route tables. By default, all transit gateway route tables are described. Alternatively, you can filter the results.'
+    describe aws_ec2_transit_gateway_route_tables do
+        it { should exist }
+        its('count') { should be <= 100 }
+        its('transit_gateway_route_table_id') {should include aws_transit_gateway_route_table_id }
+        its('transit_gateway_id') {should include aws_transit_gateway_id }
+        its('state') {should include aws_state }
+        its('default_association_route_table') {should include aws_default_association_route_table }
+        its('default_propagation_route_table') {should include aws_default_propagation_route_table }
+        its('creation_time') {should include aws_creation_time }
+        its('tags') { should be_empty }
+    end
+end
