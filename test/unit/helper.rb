2e03e71373dfb69d54c0a2a2c078782c795a43d0
Sathish <sbabu@progress.com>
2021-05-07 12:18:02 +0530

    require object to helper!
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/unit/helper.rb
+++ test/unit/helper.rb
@@ -1,7 +1,8 @@
+require 'object'
 require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/pride'
 require 'inspec/resource'
-require "inspec/log"
+require 'inspec/log'
 
 Inspec::Log.logger = Logger.new(nil)
