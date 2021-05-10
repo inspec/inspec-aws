9ccb9566e4462d7c749e8d27046f0e04ad8d256d
Sathish <sbabu@progress.com>
2021-05-10 23:55:48 +0530

    fix stack policy as json string
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/unit/resources/mock/aws_cloudformation_stack_policy_mock.rb
+++ test/unit/resources/mock/aws_cloudformation_stack_policy_mock.rb
@@ -3,26 +3,25 @@ require_relative 'aws_base_resource_mock'
 class AwsCloudformationStackPolicyMock < AwsBaseResourceMock
   def initialize
     super
-    @param_required = {}
-    @param_required[:stack_name] = @aws.any_string
-
-    @stack = Hash[@param_required]
-    @stack[:stack_name] = "testStack"
-    @stack[:stack_policy_body] =  '{Statement: [
-                                    {
-                                      Effect: "Deny",
-                                      Action: "Update:*",
-                                      Principal: "*",
-                                      Resource: "LogicalResourceId/ProductionDatabase"
-                                    },
-                                    {
-                                      Effect: "Allow",
-                                      Action: "Update:*",
-                                      Principal: "*",
-                                      Resource: "*"
-                                    }
-                                  ]
-                                }'
+    @stack = {}
+    @stack[:stack_name] = 'testStack'
+    stack_policy = {
+      Statement: [
+        {
+          Effect: 'Deny',
+          Action: 'Update:*',
+          Principal: '*',
+          Resource: 'LogicalResourceId/ProductionDatabase',
+        },
+        {
+          Effect: 'Allow',
+          Action: 'Update:*',
+          Principal: '*',
+          Resource: '*',
+        },
+      ],
+    }
+    @stack[:stack_policy_body] = stack_policy.to_json
   end
 
   def stub_data
