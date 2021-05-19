6b8db28342d623734586d535406e288ebdd87819
nirbhay <nkumar@chef.io>
2021-05-19 22:23:42 +0530

    add integration test and doc update
    Signed-off-by: nirbhay <nkumar@chef.io>

---
+++ libraries/aws_rds_option_group.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsRdsOptionGroup < AwsResourceBase
+  name 'aws_rds_option_group'
+  desc 'Verifies settings for an RDS Cluster'
+
+  example "
+    describe aws_rds_option_group(option_group_name: 'test-option_group_name') do
+      it { should exist }
+    end
+  "
+  def initialize(opts = {})
+    opts = { option_group_name: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(require_any_of: %i(option_group_name))
+
+    raise ArgumentError, "#{@__resource_name__}: .option_group_name must be a string" unless opts.key?(:option_group_name)
+    param = { option_group_name: opts[:option_group_name] }
+
+    catch_aws_errors do
+      @display_name = opts[:option_group_name]
+
+      begin
+        resp = @aws.rds_client.describe_option_groups(param)
+        return if resp.option_groups_list.empty?
+        @rds_cluster = resp.option_groups_list[0].to_h
+      rescue Aws::RDS::Errors::OptionGroupNotFoundFault
+        return
+      end
+      create_resource_methods(@rds_cluster)
+    end
+  end
+
+  def exists?
+    !@rds_cluster.to_s.empty?
+  end
+
+  def to_s
+    "RDS option groups #{@display_name}"
+  end
+end
---
+++ test/integration/verify/controls/aws_rds_option_groups.rb
@@ -0,0 +1,13 @@
+title 'Test single AWS Option Groups'
+
+describe aws_rds_group_option(option_group_name: 'default:aurora-5-6') do
+  it { should exist }
+  its('option_group_names')                { should include 'option-group-test-terraform' }
+  its('engine_names')                      { should include 'sqlserver-ee' }
+  its('option_group_description')          { should include 'Terraform Option Group' }
+  its('major_engine_versions')             { should include '11.00' }
+  its('option_group_arns')                 { should include 'IAM_ROLE_ARN_Value' }
+  its('vpc_ids')                           { should include nil }
+  its('allows_vpc_and_non_vpc_instance_memberships') { should include true }
+end
+
