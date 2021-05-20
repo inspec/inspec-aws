17ce75b3136054b634b6d127093ab56984225025
vsuzdaltsev <suzdaltsev_v@mail.ru>
2021-05-20 23:14:19 +0300

    Support for checking tags in aws_subnets resource
    Signed-off-by: vsuzdaltsev <suzdaltsev_v@mail.ru>

--- libraries/aws_subnets.rb
+++ libraries/aws_subnets.rb
@@ -23,6 +23,7 @@ class AwsSubnets < AwsResourceBase
              .register_column(:states,                  field: :state)
              .register_column(:subnet_ids,              field: :subnet_id)
              .register_column(:vpc_ids,                 field: :vpc_id)
+             .register_column(:tags,                    field: :tags)
              .install_filter_methods_on_resource(self, :table)
 
   def initialize(opts = {})
