f35bdfffbfda2d8a06cdbc12db9a207c47dba61f
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-05-21 14:31:22 +0530

    modified singular control
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_athena_work_group.rb
+++ test/integration/verify/controls/aws_athena_work_group.rb
@@ -1,14 +1,14 @@
-aws_name = attribute("aws_name", default: "test1", description: "")
-aws_state = attribute("aws_state", default: "ENABLED", description: "")
-aws_description = attribute("aws_description", default: "test1", description: "")
+aws_name = attribute("aws_name", default: "", description: "")
+aws_state = attribute("aws_state", default: "", description: "")
+aws_description = attribute("aws_description", default: "", description: "")
 aws_creation_time = attribute("aws_creation_time", default: "", description: "")
 aws_configuration_result_configuration = attribute("aws_configuration_result_configuration", default: "", description: "")
-aws_configuration_enforce_work_group_configuration = attribute("aws_configuration_enforce_work_group_configuration", default: false, description: "")
-aws_configuration_publish_cloud_watch_metrics_enabled = attribute("aws_configuration_publish_cloud_watch_metrics_enabled", default: true, description: "")
+aws_configuration_enforce_work_group_configuration = attribute("aws_configuration_enforce_work_group_configuration", default: "", description: "")
+aws_configuration_publish_cloud_watch_metrics_enabled = attribute("aws_configuration_publish_cloud_watch_metrics_enabled", default: "", description: "")
 aws_configuration_bytes_scanned_cutoff_per_query = attribute("aws_configuration_bytes_scanned_cutoff_per_query", default: "", description: "")
-aws_configuration_requester_pays_enabled = attribute("aws_configuration_requester_pays_enabled", default: false, description: "")
-aws_configuration_engine_version_selected_engine_version = attribute("aws_configuration_engine_version_selected_engine_version", default: "AUTO", description: "")
-aws_configuration_engine_version_effective_engine_version = attribute("aws_configuration_engine_version_effective_engine_version", default: "Athena engine version 1", description: "")
+aws_configuration_requester_pays_enabled = attribute("aws_configuration_requester_pays_enabled", default: "", description: "")
+aws_configuration_engine_version_selected_engine_version = attribute("aws_configuration_engine_version_selected_engine_version", default: "", description: "")
+aws_configuration_engine_version_effective_engine_version = attribute("aws_configuration_engine_version_effective_engine_version", default: "", description: "")
 
 
 describe aws_athena_work_group(work_group: 'test1') do
