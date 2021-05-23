2ff68eeb194ac94f9809967eed05cfeef73ea573
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-05-23 23:21:41 +0530

    test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_elasticsearchservice_domains.rb
+++
@@ -1,7 +0,0 @@
-describe aws_elasticsearchservice_domains do
-    it { should exist }
-  end
-  
-  describe aws_elasticsearchservice_domains do
-    its('domain_names') { should include "test1" }
-  end
--- test/integration/verify/controls/aws_glue_crawlers.rb
+++
@@ -1,7 +0,0 @@
-describe aws_glue_crawlers do
-    it { should exist }
-  end
-  
-  describe aws_glue_crawlers do
-    its('names') { should include 'test1' }
-  end
