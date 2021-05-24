67bf77027b2c6f4c1899fbff0558e30a115d06dd
nirbhay <nkumar@chef.io>
2021-05-24 10:20:58 +0530

    fix unit test cases
    Signed-off-by: nirbhay <nkumar@chef.io>

--- test/unit/resources/aws_rds_option_group_test.rb
+++ test/unit/resources/aws_rds_option_group_test.rb
@@ -1,23 +1,23 @@
 require 'helper'
-require 'aws_rds_option_groups'
+require 'aws_rds_option_group'
 require 'aws-sdk-core'
 
-class AwsRdsOptionGroupsConstructorTest < Minitest::Test
+class AwsRdsOptionGroupConstructorTest < Minitest::Test
 
-  def test_empty_params_ok
-    AwsRdsOptionGroups.new(client_args: { stub_responses: true })
+  def test_non_empty_params_ok
+    AwsRdsOptionGroup.new(option_group_name: 'default:aurora-5-6', client_args: { stub_responses: true })
   end
 
-  # def test_rejects_other_args
-  #   assert_raises(ArgumentError) { AwsRdsOptionGroups.new('rubbish') }
-  # end
-  #
-  # def test_instances_non_existing_for_empty_response
-  #   refute AwsRdsOptionGroups.new(client_args: { stub_responses: true }).exist?
-  # end
+  def test_rejects_other_args
+    assert_raises(ArgumentError) { AwsRdsOptionGroup.new('rubbish') }
+  end
+
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AwsRdsOptionGroup.new(rubbish: 9) }
+  end
 end
 
-class AwsRdsOptionGroupsHappyPathTest < Minitest::Test
+class AwsRdsOptionGroupHappyPathTest < Minitest::Test
 
   def setup
     data = {}
@@ -29,27 +29,31 @@ class AwsRdsOptionGroupsHappyPathTest < Minitest::Test
     mock_option_groups[:major_engine_version] = '5.6'
     mock_option_groups[:option_group_arn] = 'arn:aws:rds:us-east-2:112758395563:og:default:aurora-5-6'
     mock_option_groups[:allows_vpc_and_non_vpc_instance_memberships] = true
-    data[:data] = { :describe_option_groups => [mock_option_groups] }
+    data[:data] = { :option_groups_list => [mock_option_groups] }
     data[:client] = Aws::RDS::Client
-    @option_group = AwsRdsOptionGroups.new(client_args: { stub_responses: true }, stub_data: [data])
+    @option_group = AwsRdsOptionGroup.new(option_group_name: 'default:aurora-5-6',client_args: { stub_responses: true }, stub_data: [data])
   end
 
-
-
-  def test_instances_exists
-    assert @option_group.exist?
+  def test_instances_names
+    assert_equal(@option_group.option_group_name, 'default:aurora-5-6')
   end
 
-  def test_instances_ids
-    assert_equal(@option_group.option_group_name, ['default:aurora-5-6'])
+  def test_option_group_name_exists
+    assert @option_group.exists?
   end
 
-  def test_instances_filtering_not_there
-    @option_group.where(:option_group_name => 'default:aurora-5-6').exist?
+  def test_option_group_name_available
+    assert @option_group.available?
   end
 
-  def test_instances_filtering_there
-    assert @option_group.where(:option_group_arn => 'arn:aws:rds:us-east-2:112758395563:og:default:aurora-5-6').exist?
+  def test_option_group_engine_name
+    assert_equal(@option_group.engine_name, 'aurora')
   end
+
+  def test_option_group_family
+    assert_equal(@option_group.option_group_arn, 'arn:aws:rds:us-east-2:112758395563:og:default:aurora-5-6')
+  end
+  
 end
+
 
