cbb88b8f2a4ed4087fd17aaa42ef686180cc4a8c
Sathish <sbabu@progress.com>
2021-05-25 11:20:04 +0530

    VPC endpoint services resource(s)
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/aws_vpc_endpoint_service.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsVPCEndpointService < AwsResourceBase
+  name 'aws_vpc_endpoint_service'
+  desc 'Verifies settings for an AWS VPC End Point Service'
+
+  example "
+    describe aws_vpc_endpoint_service(service_name: 'aws.sagemaker.us-east-2.notebook') do
+      it { should exist }
+    end
+  "
+  SERVICE_TYPES = %w{Interface Gateway GatewayLoadBalancer}.freeze
+  BOOLEAN_FLAGS = %i(vpc_endpoint_policy_supported acceptance_required manages_vpc_endpoints).freeze
+  PRIVATE_DNS_NAME_VERIFICATION_STATES = %w{pendingVerification verified failed}.freeze
+
+  def initialize(opts = {})
+    super
+    validate_parameters(required: %i(service_name))
+
+    @display_name = @opts[:service_name]
+    fetch_response
+    create_instance_methods
+  end
+
+  def exists?
+    !failed_resource? && !!service_detail && !service_detail.empty?
+  end
+
+  SERVICE_TYPES.each do |service_type|
+    method_name = "#{service_type.gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').downcase}?"
+    define_method method_name do
+      self.service_type == service_type
+    end
+  end
+
+  BOOLEAN_FLAGS.each do |flag|
+    method_name = "#{flag}?"
+    define_method method_name do
+      service_detail.send(flag)
+    end
+  end
+
+  PRIVATE_DNS_NAME_VERIFICATION_STATES.each do |verification_state|
+    method_name = "private_dns_name_#{verification_state.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase}?"
+    define_method method_name do
+      service_detail.private_dns_name_verification_state == verification_state
+    end
+  end
+
+  def has_availability_zones?(zone_name)
+    service_detail.availability_zones.include?(zone_name)
+  end
+
+  def has_base_endpoint_dns_names?(dns)
+    service_detail.base_endpoint_dns_names.include?(dns)
+  end
+
+  def to_s
+    "AWS Service Name #{@display_name}"
+  end
+
+  private
+
+  def create_instance_methods
+    service_detail_hash = service_detail.to_h
+    service_detail_hash[:service_type] = service_detail.service_type.first.service_type
+    service_detail_hash.delete(:private_dns_names)
+    create_resource_methods(service_detail_hash)
+  end
+
+  def fetch_response
+    params = {
+      service_names: [@opts[:service_name]],
+    }
+    catch_aws_errors do
+      @response = @aws.compute_client.describe_vpc_endpoint_services(params)
+    end
+  end
+
+  def service_type
+    service_detail.service_type.first.service_type
+  end
+
+  def service_detail
+    raise StandardError, 'More than One Route is available' if @response.next_token
+
+    @response.service_details.first
+  end
+end
---
+++ libraries/aws_vpc_endpoint_services.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsVPCEndpointServices < AwsResourceBase
+  name 'aws_vpc_endpoint_services'
+  desc 'Verifies settings for a collection of AWS VPC End Point Services'
+
+  example "
+    describe aws_vpc_endpoint_services do
+      its('count') { should eq 3 }
+    end
+  "
+
+  attr_reader :table
+
+  FilterTable.create
+             .register_column(:service_names, field: :service_name)
+             .register_column(:service_ids, field: :service_id)
+             .register_column(:service_types, field: :service_type)
+             .register_column(:availability_zones, field: :availability_zones, style: :simple)
+             .register_column(:owners, field: :owner)
+             .register_column(:base_endpoint_dns_names, field: :base_endpoint_dns_names, style: :simple)
+             .register_column(:private_dns_names, field: :private_dns_name)
+             .register_column(:vpc_endpoint_policy_supported, field: :vpc_endpoint_policy_supported)
+             .register_column(:acceptance_required, field: :acceptance_required)
+             .register_column(:manages_vpc_endpoints, field: :manages_vpc_endpoints)
+             .register_column(:tags, field: :tags)
+             .register_column(:private_dns_name_verification_state, field: :private_dns_name_verification_state)
+             .install_filter_methods_on_resource(self, :table)
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch_data
+  end
+
+  private
+
+  def fetch_data
+    service_details = []
+    paginate_request do |api_response|
+      service_details += api_response.service_details.map do |service_detail|
+        flat_hash_from(service_detail)
+      end
+    end
+    service_details
+  end
+
+  def paginate_request
+    pagination_options = { max_results: 1000 }
+    loop do
+      api_response = catch_aws_errors do
+        @aws.compute_client.describe_vpc_endpoint_services(pagination_options)
+      end
+      return [] if api_response.nil? || api_response.empty?
+
+      yield api_response
+      break unless api_response.next_token
+      pagination_options = { next_token: api_response.next_token }
+    end
+  end
+
+  def flat_hash_from(service_detail)
+    service_detail_hash = service_detail.to_h
+    service_detail_hash[:service_type] = service_detail.service_type.first.service_type
+    service_detail_hash[:tags] = map_tags(service_detail.tags)
+    service_detail_hash.delete(:private_dns_names)
+    service_detail_hash
+  end
+end
