46e4e872c7fd4baef7f641dc068df6cc07768517
Sathish <sbabu@progress.com>
2021-05-27 00:50:38 +0530

    valid integ tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/aws_db_parameter_groups.rb
+++ test/integration/verify/controls/aws_db_parameter_groups.rb
@@ -1,4 +1,7 @@
 title 'Test AWS RDS Parameter Groups in bulk'
+aws_db_parameter_group_name = attribute(:aws_db_parameter_group_name, default: '', description: 'The name of the DB parameter group.')
+aws_db_parameter_group_family_name = attribute(:aws_db_parameter_group_family_name, default: '', description: 'The name of the DB parameter group family that this DB parameter group is compatible with.')
+aws_db_parameter_group_arn = attribute(:aws_db_parameter_group_arn, default: '', description: 'The Amazon Resource Name (ARN) for the DB parameter group.')
 
 control 'aws-db-parameter-groups-1.0' do
   impact 1.0
@@ -6,5 +9,8 @@ control 'aws-db-parameter-groups-1.0' do
 
   describe aws_db_parameter_groups do
     it { should exist }
+    its('db_parameter_group_names') { should include(aws_db_parameter_group_name) }
+    its('db_parameter_group_families') { should include(aws_db_parameter_group_family_name) }
+    its('db_parameter_group_arns') { should include(aws_db_parameter_group_arn) }
   end
 end
