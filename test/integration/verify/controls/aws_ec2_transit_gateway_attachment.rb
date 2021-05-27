b33223c561023c6703c5fd15c90f6579db51e61b
msys <you@example.com>
2021-05-27 15:46:19 +0530

    uncommented aws.tf and outputs.tf
    Signed-off-by: msys <you@example.com>

--- test/integration/verify/controls/aws_ec2_transit_gateway_attachment.rb
+++ test/integration/verify/controls/aws_ec2_transit_gateway_attachment.rb
@@ -1,4 +1,4 @@
-aws_transit_gateway_attachment_id = attribute(:aws_transit_gateway_attachment_id, default: '', description: 'The IDs of the attachments.')
+aws_transit_gateway_attachment_id = attribute(:aws_transit_gateway_attachment_id1, default: '', description: 'The IDs of the attachments.')
 aws_transit_gateway_id = attribute(:aws_transit_gateway_id, default: '', description: 'The ID of the transit gateway.')
 aws_transit_gateway_owner_id = attribute(:aws_transit_gateway_owner_id, default: '', description: 'The ID of the AWS account that owns the transit gateway.')
 
