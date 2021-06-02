d57a529d7279ee7e1965491e4d7191b53e1be631
msys <you@example.com>
2021-06-02 17:13:56 +0530

    fixed typos
    Signed-off-by: msys <you@example.com>

--- test/integration/build/outputs.tf
+++ test/integration/build/outputs.tf
@@ -301,5 +301,3 @@ output "aws_elasticache_replication_group_id" {
 output "aws_elasticache_replication_group_node_type" {
   value = aws_elasticache_replication_group.replication_group.node_type
 }
-
-q
