790bea737242a91ad6a689ecbfd40066e23914a3
Sathish <sbabu@progress.com>
2021-06-03 20:56:31 +0530

    unit and mock singular & plural services
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ test/unit/resources/aws_vpc_endpoint_service_test.rb
@@ -0,0 +1,80 @@
+require 'helper'
+require 'aws_vpc_endpoint_service'
+require 'aws-sdk-core'
+require_relative 'mock/aws_vpc_endpoint_service_mock'
+
+class AwsVPCEndpointServiceConstructorTest < Minitest::Test
+  def test_empty_params_not_ok
+    assert_raises(ArgumentError) { AwsVPCEndpointService.new(client_args: { stub_responses: true }) }
+  end
+
+  def test_raises_argument_error
+    assert_raises(ArgumentError) { AwsVPCEndpointService.new('not-1234') }
+  end
+
+  def test_raises_unrecognized_params
+    assert_raises(ArgumentError) { AwsVPCEndpointService.new(random: 99) }
+  end
+
+  def test_accept_valid_service_name
+    assert(AwsVPCEndpointService.new(service_name: 'aws.sagemaker.us-east-2.notebook'))
+  end
+
+  def test_raises_empty_name
+    assert_raises(ArgumentError) { AwsVPCEndpointService.new(service_name: '') }
+  end
+end
+
+class AwsVPCEndpointServiceConstructorIdTest < Minitest::Test
+  def setup
+    @mock = AwsVPCEndpointServiceMock.new
+    @vpc_end_point_service = AwsVPCEndpointService.new(service_name: @mock.service_name,
+                                     client_args: { stub_responses: true },
+                                     stub_data: [@mock.stub_data])
+  end
+
+  def test_service_name
+    assert_equal(@vpc_end_point_service.service_name, @mock.service_name)
+  end
+
+  def test_service_id
+    assert_equal(@vpc_end_point_service.service_id, @mock.service_id)
+  end
+
+  def test_service_type
+    assert_equal(@vpc_end_point_service.service_type, @mock.service_type)
+  end
+
+  def test_availability_zones
+    assert_equal(@vpc_end_point_service.availability_zones, @mock.availability_zones)
+  end
+
+  def test_owner
+    assert_equal(@vpc_end_point_service.owner, @mock.owner)
+  end
+
+  def test_base_endpoint_dns_names
+    assert_equal(@vpc_end_point_service.base_endpoint_dns_names, @mock.base_endpoint_dns_names)
+  end
+
+  def test_private_dns_name
+    assert_equal(@vpc_end_point_service.private_dns_name, @mock.private_dns_name)
+  end
+
+  def test_vpc_endpoint_policy_supported
+    assert(@vpc_end_point_service.vpc_endpoint_policy_supported?)
+  end
+
+  def test_acceptance_required
+    refute(@vpc_end_point_service.acceptance_required?)
+  end
+
+  def test_manages_vpc_endpoints
+    refute(@vpc_end_point_service.manages_vpc_endpoints?)
+  end
+
+  def test_private_dns_name_verified
+    assert(@vpc_end_point_service.private_dns_name_verified?)
+  end
+end
+
