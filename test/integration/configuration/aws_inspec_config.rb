f0a9198fa6179437981dfd76ac135732f9817fab
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-06-03 12:57:46 +0530

    dummy changes
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/configuration/aws_inspec_config.rb
+++ test/integration/configuration/aws_inspec_config.rb
@@ -209,7 +209,9 @@ module AWSInspecConfig
       # Only test execution is affected by this flag, resource creation via terraform is unaffected.
       # Default behaviour is for this to be disabled, enable by changing the below flag.
       aws_enable_cli_calls: 0,
-      aws_route_53_zone: "aws-route53-zone-#{add_random_string}"
+      aws_route_53_zone: "aws-route53-zone-#{add_random_string}",
+      aws_cluster_name: "test2",
+      aws_service_name: "service12",
   }
 
   def self.config
