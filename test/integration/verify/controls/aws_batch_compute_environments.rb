324b954b03faee0b509b2dcc2a984160b9a9070f
nirbhay <nkumar@chef.io>
2021-06-04 09:42:52 +0530

    fixed plural test case
    Signed-off-by: nirbhay <nkumar@chef.io>

--- test/integration/verify/controls/aws_batch_compute_environments.rb
+++ test/integration/verify/controls/aws_batch_compute_environments.rb
@@ -1,23 +1,18 @@
 aws_compute_environment_name = attribute("aws_compute_environment_name", default: "", description: "")
-aws_compute_environment_arn = attribute("aws_compute_environment_arn", default: "", description: "")
-aws_ecs_cluster_arn = attribute("aws_ecs_cluster_arn", default: "", description: "")
-aws_tags = attribute("aws_tags", default: "", description: "")
-aws_type = attribute("aws_type", default: "", description: "")
-aws_state = attribute("aws_state", default: "", description: "")
-aws_status = attribute("aws_status", default: "", description: "")
-aws_status_reason = attribute("aws_status_reason", default: "", description: "")
-aws_service_role = attribute("aws_service_role", default: "", description: "")
-
+aws_state = attribute("aws_batch_job_queue_status", default: "", description: "")
+aws_compute_resources_type = attribute("aws_type", default: "", description: "")
+aws_compute_resources_minv_cpus = attribute("aws_min_vcpus", default: "", description: "")
+aws_compute_resources_maxv_cpus = attribute("aws_max_vcpus", default: "", description: "")
 
 describe aws_batch_compute_environments do
   it { should exist }
   its('compute_environment_names') { should include aws_compute_environment_name }
-  its('compute_environment_arns') { should include aws_compute_environment_arn }
-  its('ecs_cluster_arns') { should include aws_ecs_cluster_arn }
-  its('tags') { should_not be_empty  }
-  its('types') { should include aws_type }
+  its('types') { should include "MANAGED" }
   its('states') { should include aws_state }
-  its('statuses') { should include aws_status }
-  its('status_reasons') { should include aws_status_reason }
-  its('service_roles') { should include aws_service_role }
+  its('statuses') { should include "VALID" }
+  its('status_reasons') { should include  "ComputeEnvironment Healthy"}
+  its('compute_resources.allocation_strategys') { should be_empty }
+  its('compute_resources.minv_cpus') { should be <= aws_compute_resources_minv_cpus  }
+  its('compute_resources.maxv_cpus') { should be <= aws_compute_resources_maxv_cpus }
+
 end
