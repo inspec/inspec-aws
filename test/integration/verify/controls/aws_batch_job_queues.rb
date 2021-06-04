8bd7effff47f59ea3ae98fc0c723a3ad457cfe03
msys <you@example.com>
2021-06-04 09:32:06 +0530

    fixed integration test
    Signed-off-by: msys <you@example.com>

--- test/integration/verify/controls/aws_batch_job_queues.rb
+++ test/integration/verify/controls/aws_batch_job_queues.rb
@@ -1,20 +1,15 @@
-aws_job_queue_name = attribute("aws_job_queue_name", default: "", description: "")
-aws_job_queue_arn = attribute("aws_job_queue_arn", default: "", description: "")
-aws_state = attribute("aws_state", default: "", description: "")
-aws_status = attribute("aws_status", default: "", description: "")
-aws_status_reason = attribute("aws_status_reason", default: "", description: "")
-aws_priority = attribute("aws_priority", default: 1, description: "")
-aws_compute_environment_order = attribute("aws_compute_environment_order", default: 1, description: "")
-aws_compute_environment_compute_environment = attribute("aws_compute_environment_compute_environment", default: "", description: "")
-aws_tags = attribute("aws_tags", default: "", description: "")
+aws_job_queue_name = attribute("aws_batch_job_queue_name", default: "", description: "")
+aws_job_queue_arn = attribute("batch_job_queue_arn", default: "", description: "")
+aws_state = attribute("aws_batch_job_queue_status", default: "", description: "")
+aws_priority = attribute("aws_batch_job_queue_priority", default: 1, description: "")
+aws_compute_environment_compute_environment = attribute("batch_job_queue_compute_environments", default: "", description: "")
+
 
 describe aws_batch_job_queues do
   it { should exist }
   its('job_queue_names') { should include aws_job_queue_name }
   its('job_queue_arns') { should include aws_job_queue_arn }
   its('states') { should include aws_state }
-  its('statuses') { should include aws_status }
-  its('status_reasons') { should include aws_status_reason }
   its('priorities') { should include aws_priority }
-  its('tags') { should_not be_empty }
+  its('compute_environment_order.first.compute_environments') { should be <= 1 }
 end
