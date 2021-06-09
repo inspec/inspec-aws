21c577146710b102cf115e88ae3d3e6637cd255e
nirbhay1997 <nkumar@chef.io>
2021-06-09 10:56:35 +0530

    removed unused resources
    Signed-off-by: nirbhay1997 <nkumar@chef.io>

--- test/integration/build/aws.tf
+++ test/integration/build/aws.tf
@@ -1979,26 +1979,6 @@ resource "aws_elasticache_replication_group" "replication_group" {
   at_rest_encryption_enabled    = true
   transit_encryption_enabled    = false
 }
-
-resource "aws_lb" "aws_lb2" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
-  }
-}
 resource "aws_lb" "test" {
   name               = "test-lb-tf-i"
   internal           = false
