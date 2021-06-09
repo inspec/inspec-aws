e51c7a881c0880392ce0388eb4be1482d70b0820
nirbhay1997 <nkumar@chef.io>
2021-06-09 10:48:57 +0530

    fixed integration tests
    Signed-off-by: nirbhay1997 <nkumar@chef.io>

--- test/integration/verify/controls/aws_elasticloadbalancingv2_load_balancer.rb
+++ test/integration/verify/controls/aws_elasticloadbalancingv2_load_balancer.rb
@@ -1,4 +1,4 @@
-aws_elbv2_load_balancer_arn = attribute(:aws_elbv2_load_balancer_arn, value: '', description: '')
+aws_elbv2_load_balancer_arn = attribute(:load_balancer_arn, value: '', description: '')
 
 control 'aws-eelbv2-load-balancer-1.0' do
 
@@ -11,24 +11,17 @@ control 'aws-eelbv2-load-balancer-1.0' do
   
   describe aws_elasticloadbalancingv2_load_balancer(load_balancer_arn: aws_elbv2_load_balancer_arn) do
       its('load_balancer_arn') { should eq aws_elbv2_load_balancer_arn }
-      its('dns_name') { should eq "test1-1277654690.us-east-2.elb.amazonaws.com" }
       its('canonical_hosted_zone_id') { should eq "Z3AADJGX6KTTL2" }
       # its('created_time') { should eq "2021-05-17 18:01:12.78 UTC" }
-      its('load_balancer_name') { should eq "test1" }
+      its('load_balancer_name') { should eq "test-lb-tf-i" }
       its('scheme') { should eq "internet-facing" }
-      its('vpc_id') { should eq "vpc-6d9d7505" }
       its('state.code') { should eq "active" }
       its('state.reason') { should be_empty }
       its('type') { should eq "application" }
-  
       its('availability_zones') { should_not be_empty }
       its('availability_zones.first.zone_name') { should eq "us-east-2b" }
-      its('availability_zones.first.subnet_id') { should eq "subnet-22043459" }
       its('availability_zones.first.outpost_id') { should be_empty }
-  
       its('availability_zones.first.load_balancer_addresses') { should be_empty }
-  
-      its('security_groups') { should include "sg-00dd73829ccf99146" }
       its('ip_address_type') { should eq "ipv4" }
       its('customer_owned_ipv_4_pool') { should be_empty }
   end
--- test/integration/verify/controls/aws_elasticloadbalancingv2_load_balancers.rb
+++ test/integration/verify/controls/aws_elasticloadbalancingv2_load_balancers.rb
@@ -1,4 +1,4 @@
-aws_elbv2_load_balancer_arn = attribute(:aws_elbv2_load_balancer_arn, value: '', description: '')
+aws_elbv2_load_balancer_arn = attribute(:load_balancer_arn, value: '', description: '')
 
 control 'aws-eelbv2-load-balancers-1.0' do
 
@@ -11,18 +11,11 @@ control 'aws-eelbv2-load-balancers-1.0' do
   
   describe aws_elasticloadbalancingv2_load_balancers(load_balancer_arn: aws_elbv2_load_balancer_arn) do
       its('load_balancer_arns') { should include aws_elbv2_load_balancer_arn }
-      its('dns_names') { should include "test1-1277654690.us-east-2.elb.amazonaws.com" }
       its('canonical_hosted_zone_ids') { should include "Z3AADJGX6KTTL2" }
-      # its('created_times') { should include "2021-05-17 18:01:12.78 UTC" }
-      its('load_balancer_names') { should include "test1" }
+      its('load_balancer_names') { should include "test-lb-tf-i" }
       its('schemes') { should include "internet-facing" }
-      its('vpc_ids') { should include "vpc-6d9d7505" }
-      # its('states.code') { should include "active" }
-      # its('states.reason') { should be_empty }
-      its('types') { should eq "application" }
-  
+      its('types') { should include "application" }
       its('availability_zones') { should_not be_empty }
-      # its('security_groups') { should include "sg-00dd73829ccf99146" }
       its('ip_address_types') { should include "ipv4" }
       its('customer_owned_ipv_4_pools') { should_not be_empty }
   end
