983e538988f6fbf0ecd22870eebbd6a8aa25db24
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-06-14 15:22:07 +0530

    paginations added
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/aws_ecs_task_definitions.rb
+++ libraries/aws_ecs_task_definitions.rb
@@ -5,6 +5,7 @@ require 'aws_backend'
 class AWSECSTaskDefinitions < AwsResourceBase
   name 'aws_ecs_task_definitions'
   desc 'Returns a list of task definitions that are registered to your account.'
+
   example "
     describe aws_ecs_task_definitions do
       it { should exist }
@@ -25,14 +26,16 @@ class AWSECSTaskDefinitions < AwsResourceBase
   def fetch_data
     table_rows = []
     pagination_options = {}
-    catch_aws_errors do
-      @api_response = @aws.ecs_client.list_task_definitions(pagination_options)
-    end
-    return [] if !@api_response || @api_response.empty?
-    @api_response.each do |res|
-      table_rows += [{
-        task_definition_arns: res.task_definition_arns,
-      }]
+    loop do
+      catch_aws_errors do
+        @api_response = @aws.ecs_client.list_task_definitions(pagination_options)
+      end
+      return table_rows if !@api_response || @api_response.empty?
+      @api_response.each do |res|
+        table_rows += [{ task_definition_arns: res.task_definition_arns }]
+      end
+      break unless @api_response.next_token
+      pagination_options = { next_token: @api_response.next_token }
     end
     @table = table_rows
   end
