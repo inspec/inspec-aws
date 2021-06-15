c400f10b02104e7cba7cc97120906ae33f987192
nirbhay1997 <nkumar@progress.com>
2021-06-15 12:34:19 +0530

    fixed integration tests
    Signed-off-by: nirbhay1997 <nkumar@progress.com>

--- test/integration/verify/controls/aws_elasticloadbalancingv2_listener_rules.rb
+++ test/integration/verify/controls/aws_elasticloadbalancingv2_listener_rules.rb
@@ -1,11 +1,11 @@
-aws_elbv2_rule_arn = attribute(:aws_elbv2_rule_arn, value: '', description: '')
+listener_arn = attribute(:listener_arn, value: '', description: '')
 
 control 'aws-elbv2-listener-rules1-1.0' do
 
   impact 1.0
   title 'Ensure AWS ELBv2 Listerner Rules has the correct properties.'
 
-  describe aws_elasticloadbalancingv2_listener_rules(rule_arns: aws_elbv2_rule_arn) do
+  describe aws_elasticloadbalancingv2_listener_rules(listener_arn: listener_arn) do
     it { should exist }
   end
 end
@@ -15,7 +15,7 @@ control 'aws-elbv2-listener-rules2-1.0' do
     impact 1.0
     title 'Ensure AWS ELBv2 Listerner Rules has the correct properties.'
   
-    describe aws_elasticloadbalancingv2_listener_rules(rule_arns: aws_elbv2_rule_arn) do
+    describe aws_elasticloadbalancingv2_listener_rules(listener_arn: listener_arn) do
       its('rule_arns') { should include aws_elbv2_rule_arn }
       its('priorities') { should include "100" }
       its('conditions') { should_not be_empty }
