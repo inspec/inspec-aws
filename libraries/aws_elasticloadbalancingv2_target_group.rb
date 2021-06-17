5ebb19c336875649571a05d9806f8e6a0dbd0d0d
nirbhay1997 <nkumar@progress.com>
2021-06-17 17:07:46 +0530

    minor fix
    Signed-off-by: nirbhay1997 <nkumar@progress.com>

--- libraries/aws_elasticloadbalancingv2_target_group.rb
+++ libraries/aws_elasticloadbalancingv2_target_group.rb
@@ -7,18 +7,18 @@ class AWSElasticLoadBalancingV2TargetGroup < AwsResourceBase
   desc 'Describes the specified target groups or all of your target groups. By default, all target groups are described. Alternatively, you can specify one of the following to filter the results: the ARN of the load balancer, the names of one or more target groups, or the ARNs of one or more target groups.'
 
   example "
-    describe aws_elasticloadbalancingv2_target_group(target_group_arns: 'test') do
+    describe aws_elasticloadbalancingv2_target_group(target_group_arns: "test-arn") do
       it { should exist }
     end
   "
   def initialize(opts = {})
-    opts = { target_group_arns: opts } if opts.is_a?(String)
+    opts = { target_group_arn: opts } if opts.is_a?(String)
     super(opts)
-    validate_parameters(required: [:target_group_arns])
-    raise ArgumentError, "#{@__resource_name__}: target_group_arns must be provided" unless opts[:target_group_arns] && !opts[:target_group_arns].empty?
-    @display_name = opts[:target_group_arns]
+    validate_parameters(required: [:target_group_arn])
+    raise ArgumentError, "#{@__resource_name__}: target_group_arns must be provided" unless opts[:target_group_arn] && !opts[:target_group_arn].empty?
+    @display_name = opts[:target_group_arn]
     catch_aws_errors do
-      resp = @aws.elb_client_v2.describe_target_groups({ target_group_arns: [opts[:target_group_arns]] })
+      resp = @aws.elb_client_v2.describe_target_groups({ target_group_arns: [opts[:target_group_arn]] })
       @target_groups = resp.target_groups[0].to_h
       create_resource_methods(@target_groups)
     end
@@ -26,15 +26,11 @@ class AWSElasticLoadBalancingV2TargetGroup < AwsResourceBase
 
   def id
     return nil unless exists?
-    @target_groups[:target_group_arns]
+    @target_groups[:target_group_arn]
   end
 
   def exists?
     !@target_groups.nil? && !@target_groups.empty?
-  end
-
-  def encrypted?
-    @target_groups[:encrypted]
   end
 
   def to_s
