9750ed0b15799b784895f225aeb38d8df8fcf818
nirbhay1997 <nkumar@progress.com>
2021-06-29 11:59:25 +0530

    midified u test
    Signed-off-by: nirbhay1997 <nkumar@progress.com>

--- test/unit/resources/aws_lambda_permission_test.rb
+++ test/unit/resources/aws_lambda_permission_test.rb
@@ -22,37 +22,25 @@ class AWSLambdaPermissionSuccessPathTest < Minitest::Test
   def setup
     data = {}
     data[:method] = :get_policy
-
     mock_data = "{\"Version\":\"2012-10-17\",\"Id\":\"default\",\"Statement\":[{\"Sid\":\"AllowExecutionFromSqs\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"sqs.amazonaws.com\"},\"Action\":\"lambda:InvokeFunction\",\"Resource\":\"arn:aws:lambda:us-east-2:112758395563:function:test_Lambda\",\"Condition\":{\"ArnLike\":{\"AWS:SourceArn\":\"arn:aws:sqs:us-east-2:112758395563:terraform-example-queue\"}}}]}"
-
-    # mock_data[:policy] = JSON.parse("{test1}")
-    # mock_data[:policy] = JSON.parse("test1")
-    # mock_data[:effect] = 'test1'
-    # mock_data[:principal] = 'test1'
-    # mock_data[:action] = 'test1'
-    # mock_data[:resource] = 'test1'
     data[:data] = {"policy" => mock_data}
     data[:client] = Aws::Lambda::Client
     @resp = AWSLambdaPermission.new(function_name: 'test_Lambda', Sid: 'AllowExecutionFromSqs', client_args: { stub_responses: true }, stub_data: [data])
   end
 
-  # def test_policy
-  #   assert_equal(@resp.policy,  "")
-  # end
+  def test_sid
+    assert_equal(@resp.sid,  "AllowExecutionFromSqs")
+  end
 
   def test_effect
     assert_equal(@resp.effect, 'Allow')
   end
 
-  # def test_principal
-  #   assert_equal(@resp.principal, 'test1')
-  # end
-  #
-  # def test_action
-  #   assert_equal(@resp.action, 'test1')
-  # end
-  #
-  # def test_resource
-  #   assert_equal(@resp.resource, 'test1')
-  # end
+  def test_resource
+    assert_equal(@resp.resource, "arn:aws:lambda:us-east-2:112758395563:function:test_Lambda")
+  end
+
+  def test_action
+    assert_equal(@resp.action, "lambda:InvokeFunction")
+  end
 end
--- test/unit/resources/aws_lambda_permissions_test.rb
+++ test/unit/resources/aws_lambda_permissions_test.rb
@@ -1,55 +1,44 @@
-# require 'helper'
-# require 'aws_lambda_permissions'
-# require 'aws-sdk-core'
-#
-# class AWSLambdaPermissionsConstructorTest < Minitest::Test
-#
-#   def test_empty_params_ok
-#     AWSLambdaPermissions.new(function_name: 'test1', client_args: { stub_responses: true })
-#   end
-#
-#   def test_rejects_other_args
-#     assert_raises(ArgumentError) { AWSLambdaPermissions.new('rubbish') }
-#   end
-#
-#   def test_functions_non_existing_for_empty_response
-#     refute AWSLambdaPermissions.new(function_name: 'test1', client_args: { stub_responses: true }).exist?
-#   end
-# end
-#
-# class AWSLambdaPermissionsHappyPathTest < Minitest::Test
-#
-#   def setup
-#     data = {}
-#     data[:method] = :get_policy
-#     mock_data = {}
-#     mock_data[:sid] = 'test1'
-#     mock_data[:effect] = 'test1'
-#     mock_data[:principal] = 'test1'
-#     mock_data[:action] = 'test1'
-#     mock_data[:resource] = 'test1'
-#     data[:data] = mock_data
-#     data[:client] = Aws::Lambda::Client
-#     @resp = AWSLambdaPermissions.new(function_name: 'test1', client_args: { stub_responses: true }, stub_data: [data])
-#   end
-#
-#   def test_sids
-#     assert_equal(@resp.sids, ['test1'])
-#   end
-#
-#   def test_effects
-#     assert_equal(@resp.effects, ['test1'])
-#   end
-#
-#   def test_principals
-#     assert_equal(@resp.principals, ['test1'])
-#   end
-#
-#   def test_actions
-#     assert_equal(@resp.actions, ['test1'])
-#   end
-#
-#   def test_resources
-#     assert_equal(@resp.resources, ['test1'])
-#   end
-# end
+require 'helper'
+require 'aws_lambda_permissions'
+require 'aws-sdk-core'
+
+class AWSLambdaPermissionsConstructorTest < Minitest::Test
+
+  def test_empty_params_not_ok
+    assert_raises(ArgumentError) {AWSLambdaPermissions.new(function_name: "", client_args: { stub_responses: true })}
+  end
+
+  def test_rejects_other_args
+    assert_raises(ArgumentError) { AWSLambdaPermissions.new('rubbish') }
+  end
+end
+
+class AWSLambdaPermissionsHappyPathTest < Minitest::Test
+
+  def setup
+    data = {}
+    data[:method] = :get_policy
+    mock_data = "{\"Version\":\"2012-10-17\",\"Id\":\"default\",\"Statement\":[{\"Sid\":\"AllowExecutionFromSqs\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"sqs.amazonaws.com\"},\"Action\":\"lambda:InvokeFunction\",\"Resource\":\"arn:aws:lambda:us-east-2:112758395563:function:test_Lambda\",\"Condition\":{\"ArnLike\":{\"AWS:SourceArn\":\"arn:aws:sqs:us-east-2:112758395563:terraform-example-queue\"}}}]}"
+    data[:data] = {"policy" => mock_data}
+    data[:client] = Aws::Lambda::Client
+    @resp = AWSLambdaPermissions.new(function_name: 'test_Lambda', client_args: { stub_responses: true }, stub_data: [data])
+  end
+
+  def test_sids
+    assert_equal(@resp.sids, ["AllowExecutionFromSqs"]
+    )
+  end
+
+  def test_effects
+    assert_equal(@resp.effects, ['Allow'])
+  end
+
+  def test_actions
+    assert_equal(@resp.actions, ["lambda:InvokeFunction"])
+  end
+
+
+  def test_resources
+    assert_equal(@resp.resources, ['arn:aws:lambda:us-east-2:112758395563:function:test_Lambda'])
+  end
+end
