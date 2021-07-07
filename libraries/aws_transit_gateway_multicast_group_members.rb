e71f741ed3b6e886938430ea8773d5bb570d6199
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-07-07 14:36:18 +0530

    transit gateway
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ libraries/aws_transit_gateway_multicast_group_members.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSTransitGatewayMulticastGroupMembers < AwsResourceBase
+  name 'aws_transit_gateway_multicast_group_members'
+  desc 'Searches one or more transit gateway multicast groups and returns the group membership information.'
+
+  example "
+    describe aws_transit_gateway_multicast_group_members do
+      it { should exist }
+    end
+
+    describe aws_transit_gateway_multicast_group_members do
+      its('group_ip_addresses') { should include 'group_ip_address' }
+    end
+  "
+
+  attr_reader :table
+
+  FilterTable.create
+             .register_column(:group_ip_addresses,                    field: :group_ip_address)
+             .register_column(:transit_gateway_attachment_ids,        field: :transit_gateway_attachment_id)
+             .register_column(:subnet_ids,                            field: :subnet_id)
+             .register_column(:resource_ids,                          field: :resource_id)
+             .register_column(:resource_types,                        field: :resource_type)
+             .register_column(:resource_owner_ids,                    field: :resource_owner_id)
+             .register_column(:group_members,                         field: :group_member)
+             .register_column(:group_sources,                         field: :group_source)
+             .register_column(:member_types,                          field: :member_type)
+             .register_column(:source_types,                          field: :source_type)
+             .install_filter_methods_on_resource(self, :table)
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch_data
+  end
+
+  def fetch_data
+    pagination_options = {}
+    rows = []
+    pagination_options[:max_results] = 100
+    loop do
+      catch_aws_errors do
+        @api_response = @aws.compute_client.describe_transit_gateway_multicast_domains(pagination_options)
+      end
+      return rows if !@api_response || @api_response.empty?
+      @api_response.transit_gateway_multicast_domains.each do |resp|
+        rows += [{ group_ip_address: resp.group_ip_address,
+                   transit_gateway_attachment_id: resp.transit_gateway_attachment_id,
+                   subnet_id: resp.subnet_id,
+                   resource_id: resp.resource_id,
+                   resource_type: resp.resource_type,
+                   resource_owner_id: resp.resource_owner_id,
+                   group_member: resp.group_member,
+                   group_source: resp.group_source,
+                   member_type: resp.member_type,
+                   source_type: resp.source_type }]
+      end
+      break unless @api_response.next_token
+      pagination_options[:next_token] = @api_response.next_token
+    end
+    rows
+  end
+end
---
+++ test/integration/verify/controls/aws_transit_gateway_multicast_domain.rb
@@ -0,0 +1,23 @@
+control 'aws_transit_gateway_multicast_domain' do
+  impact 1.0
+  title 'Describes one or more transit gateway multicast domains.'
+  describe aws_transit_gateway_multicast_domain(transit_gateway_multicast_domain_id: "tgw-mcast-domain-0814d49959a72bcd5") do
+    it { should exist }
+  end
+
+  describe aws_transit_gateway_multicast_domain(transit_gateway_multicast_domain_id: "tgw-mcast-domain-0814d49959a72bcd5") do
+    its('transit_gateway_multicast_domain_id') { should eq 'tgw-mcast-domain-0814d49959a72bcd5' }
+    its('transit_gateway_id') { should eq 'tgw-0e995e1955e41d44b' }
+    its('transit_gateway_multicast_domain_arn') { should eq 'arn:aws:ec2:us-east-2:112758395563:transit-gateway-multicast-domain/tgw-mcast-domain-0814d49959a72bcd5' }
+    its('owner_id') { should eq '112758395563' }
+    its('state') { should eq 'available' }
+    # its('creation_time') { should_not be_empty }
+    its('tags') { should_not be_empty }
+  end
+
+  describe aws_transit_gateway_multicast_domain(transit_gateway_multicast_domain_id: "tgw-mcast-domain-0814d49959a72bcd5") do
+    its('options.igmpv_2_support') { should eq 'disable' }
+    its('options.static_sources_support') { should eq 'enable' }
+    its('options.auto_accept_shared_associations') { should eq 'enable' }
+  end
+end
