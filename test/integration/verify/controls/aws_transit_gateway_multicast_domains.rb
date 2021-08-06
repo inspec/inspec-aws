4eff821450473ed495bb141ada13feb7dbf297f3
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-08-06 12:10:08 +0530

    changes done in the integration tests with dummy values
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_transit_gateway_multicast_domains.rb
+++ test/integration/verify/controls/aws_transit_gateway_multicast_domains.rb
@@ -6,10 +6,10 @@ skip_control 'aws_transit_gateway_multicast_domains' do
   end
 
   describe aws_transit_gateway_multicast_domains do
-    its('transit_gateway_multicast_domain_ids') { should include '' }
-    its('transit_gateway_ids') { should include '' }
-    its('transit_gateway_multicast_domain_arns') { should include '' }
-    its('owner_ids') { should include '' }
+    its('transit_gateway_multicast_domain_ids') { should include 'tgw-mcast-domain-1234567890' }
+    its('transit_gateway_ids') { should include 'tgw-1234567890' }
+    its('transit_gateway_multicast_domain_arns') { should include 'tgw-1234567890' }
+    its('owner_ids') { should include '1234567890' }
     its('states') { should include 'available' }
     its('tags') { should_not be_empty }
   end
