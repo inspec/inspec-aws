c0db0a44df392cf1b015177d3fc986d71613ae9a
nirbhay1997 <nkumar@progress.com>
2021-08-17 00:20:59 +0530

    pushing test cases
    Signed-off-by: nirbhay1997 <nkumar@progress.com>

--- my-p/controls/example.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-describe aws_iam_virtual_mfa_devices do
-  it { should exist }
-  its('user_names') { should include 'shaik-iam' }
-  its('arns') { should include 'test-iam' }
-  its('user_ids') { should include 'test-iam' }
-  its('path') { should include '/' }
-  its('create_date') { should include Time.parse('2017-11-09 20:40:47.000000000 +0000') }
-end
