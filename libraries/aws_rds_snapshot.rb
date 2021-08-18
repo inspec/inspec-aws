24e88e37e25b03a9dfc97b9853940e97791f7870
Rohit Joshi <rohit.prasad.joshi@sap.com>
2021-08-18 10:22:49 -0700

    adding libraries for 'rds snapshot'
    Signed-off-by: Rohit Joshi <rohit.prasad.joshi@sap.com>

---
+++ libraries/aws_rds_snapshot.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsRdsSnapshot < AwsResourceBase
+  name 'aws_rds_snapshot'
+  desc 'Verifies settings for an RDS snapshot instance'
+
+  example "
+    describe aws_rds_snapshot(db_snapshot_identifier: 'test-instance-id') do
+      it { should exist }
+    end
+  "
+
+  def initialize(opts = {})
+    opts = { db_snapshot_identifier: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: [:db_snapshot_identifier])
+
+    raise ArgumentError, "#{@__resource_name__}: db_snapshot_identifier must start with a letter followed by up to 62 letters/numbers/hyphens." if opts[:db_snapshot_identifier] !~ /^[a-z]{1}[0-9a-z\-]{0,62}$/
+
+    catch_aws_errors do
+      @display_name = opts[:db_snapshot_identifier]
+
+      begin
+        resp = @aws.rds_client.describe_db_snapshots(db_snapshot_identifier: opts[:db_snapshot_identifier])
+        return if resp.db_snapshots.empty?
+        @rds_snapshot = resp.db_snapshots[0].to_h
+      rescue Aws::RDS::Errors::DBSnapshotNotFound
+        return
+      end
+      create_resource_methods(@rds_snapshot)
+    end
+  end
+
+  def has_encrypted_snapshot?
+    @rds_snapshot[:encrypted]
+  end
+  alias encrypted? has_encrypted_snapshot?
+
+  def exists?
+    !@rds_snapshot.nil? && !@rds_snapshot.empty?
+  end
+
+  def to_s
+    "RDS Snapshot #{@display_name}"
+  end
+end
