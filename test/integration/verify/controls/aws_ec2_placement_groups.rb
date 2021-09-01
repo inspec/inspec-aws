25ec532fe7b7396b76aaae3e884a69a8aa027c72
Nirbhay Kumar <nkumar@progress.com>
2021-09-01 07:40:27 +0530

    adeed integrations tests
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

--- test/integration/verify/controls/aws_ec2_placement_groups.rb
+++ test/integration/verify/controls/aws_ec2_placement_groups.rb
@@ -1,1 +1,16 @@
 
+aws_placement_group_name = attribute(:aws_placement_group_name, value: '', description: 'The AWS EC2 Placement groups.')
+
+
+title 'Test WS EC2 Placement groups in bulk'
+control 'aws_placement_group_name-1.0' do
+
+  impact 1.0
+  title 'Ensure AWS EC2 Placement groups resource has the correct properties.'
+
+  describe aws_ec2_placement_group do
+    it { should exist }
+    its('strategy') { should include "cluster" }
+    its('aws_placement_group_name') { should include aws_placement_group_name  }
+  end
+end
