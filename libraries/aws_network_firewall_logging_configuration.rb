dda200e753d3b6525313c1a6b5dfc5884539f1c3
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-09-03 13:04:16 +0530

    all docs done
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/aws_network_firewall_logging_configuration.rb
+++ libraries/aws_network_firewall_logging_configuration.rb
@@ -37,4 +37,16 @@ class AWSNetworkFirewallLoggingConfiguration < AwsResourceBase
   def to_s
     "Firewall Name: #{@display_name}"
   end
+
+  def logging_configuration_log_destination_configs_log_type
+    (logging_configuration.map(&:log_destination_configs)).map(&:log_type)
+  end
+
+  def logging_configuration_log_destination_configs_log_destination_type
+    (logging_configuration.map(&:log_destination_configs)).map(&:log_destination_type)
+  end
+
+  def logging_configuration_log_destination_configs_log_destination
+    (logging_configuration.map(&:log_destination_configs)).map(&:log_destination)
+  end
 end
