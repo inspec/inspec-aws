5d66eee5dbf2bc5c4a946b3660f334a8572b6a0d
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-09-05 21:57:16 +0530

    unit test changes
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/unit/resources/aws_ec2_vpc_peering_connection_test.rb
+++ test/unit/resources/aws_ec2_vpc_peering_connection_test.rb
@@ -2,38 +2,50 @@ require 'helper'
 require 'aws_ec2_vpc_peering_connection'
 require 'aws-sdk-core'
 
-class AWSEc2VPCPeeringConnectionConstructorTest < Minitest::Test
+class AWSEC2VPCPeeringConnectionConstructorTest < Minitest::Test
 
   def test_empty_params_not_ok
-    assert_raises(ArgumentError) { AWSEc2VPCPeeringConnection.new(client_args: { stub_responses: true }) }
+    assert_raises(ArgumentError) { AWSEC2VPCPeeringConnection.new(client_args: { stub_responses: true }) }
   end
 
   def test_empty_param_arg_not_ok
-    assert_raises(ArgumentError) { AWSEc2VPCPeeringConnection.new(vpc_peering_connection_id: '', client_args: { stub_responses: true }) }
+    assert_raises(ArgumentError) { AWSEC2VPCPeeringConnection.new(vpc_peering_connection_id: '', client_args: { stub_responses: true }) }
   end
 
   def test_rejects_unrecognized_params
-    assert_raises(ArgumentError) { AWSEc2VPCPeeringConnection.new(unexpected: 9) }
+    assert_raises(ArgumentError) { AWSEC2VPCPeeringConnection.new(unexpected: 9) }
   end
 end
 
-class AWSEc2VPCPeeringConnectionSuccessPathTest < Minitest::Test
+class AWSEC2VPCPeeringConnectionSuccessPathTest < Minitest::Test
 
   def setup
     data = {}
     data[:method] = :describe_vpc_peering_connections
     mock_data = {}
     mock_data[:vpc_peering_connection_id] = "test1"
+    mock_data[:status] = {
+      code: "test1",
+      message: "test1"
+    }
     data[:data] = { vpc_peering_connections: [mock_data] }
     data[:client] = Aws::EC2::Client
-    @vpc_peering_connections = AWSEc2VPCPeeringConnection.new(vpc_peering_connection_id: 'test1', client_args: { stub_responses: true }, stub_data: [data])
+    @resp = AWSEC2VPCPeeringConnection.new(vpc_peering_connection_id: 'test1', client_args: { stub_responses: true }, stub_data: [data])
   end
 
   def test_vpc_peering_connections_exists
-    assert @vpc_peering_connections.exists?
+    assert @resp.exists?
   end
 
   def test_vpc_peering_connection_id
-    assert_equal(@vpc_peering_connections.vpc_peering_connection_id, "test1")
+    assert_equal(@resp.vpc_peering_connection_id, "test1")
+  end
+
+  def test_status_code
+    assert_equal(@resp.status.code, "test1")
+  end
+
+  def test_status_message
+    assert_equal(@resp.status.message, "test1")
   end
 end
