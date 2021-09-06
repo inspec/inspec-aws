982a6ec71a3477f6d06cf230d15a81e86613d7ee
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2021-09-06 09:18:01 +0530

    Update test/integration/verify/controls/aws_ec2_placement_groups.rb
    Co-authored-by: Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>

--- test/integration/verify/controls/aws_ec2_placement_groups.rb
+++ test/integration/verify/controls/aws_ec2_placement_groups.rb
@@ -8,7 +8,7 @@ control 'aws_placement_group_name-1.0' do
   impact 1.0
   title 'Ensure AWS EC2 Placement groups resource has the correct properties.'
 
-  describe aws_ec2_placement_group do
+  describe aws_ec2_placement_groups do
     it { should exist }
     its('strategy') { should include "cluster" }
     its('aws_placement_group_name') { should include aws_placement_group_name  }
