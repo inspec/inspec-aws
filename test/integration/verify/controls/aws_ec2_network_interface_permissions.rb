83093cec5226b70ead70d89afad1d9559d98f660
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2021-09-06 09:14:42 +0530

    Update test/integration/verify/controls/aws_ec2_network_interface_permissions.rb
    Co-authored-by: Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>

--- test/integration/verify/controls/aws_ec2_network_interface_permissions.rb
+++ test/integration/verify/controls/aws_ec2_network_interface_permissions.rb
@@ -4,7 +4,7 @@ control 'aws_ec2_network_interface_permissions-1.0' do
   impact 1.0
   title 'Ensure all the AWS Network Interface permissions has the correct properties.'
 
-  describe describe aws_ec2_network_interface_permissions do
+  describe aws_ec2_network_interface_permissions do
     it { should exist }
     its('network_interface_ids') { should include "test-val" }
     its('aws_account_ids') { should include 'test-val'  }
