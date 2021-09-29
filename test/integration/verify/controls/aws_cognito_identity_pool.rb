b4271f4618056557023c8bafca123a4c3451a46b
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-09-29 13:01:32 +0530

    minor modifications
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_cognito_identity_pool.rb
+++ test/integration/verify/controls/aws_cognito_identity_pool.rb
@@ -2,18 +2,13 @@ aws_identity_pool_id = attribute("aws_identity_pool_id", value: "", description:
 aws_identity_pool_name = attribute("aws_identity_pool_name", value: "", description: "")
 aws_allow_unauthenticated_identities = attribute("aws_allow_unauthenticated_identities", value: false, description: "")
 
-control 'aws-auto-identity-pool1-1.0' do
+control 'aws-auto-identity-pool-1.0' do
   impact 1.0
   title 'Ensure AWS Identity Pool has the correct properties.'
 
   describe aws_cognito_identity_pool(identity_pool_id: aws_identity_pool_id) do
     it { should exist }
   end
-end
-
-control 'aws-auto-identity-pool2-1.0' do
-  impact 1.0
-  title 'Ensure AWS Identity Pool has the correct properties.'
 
   describe aws_cognito_identity_pool(identity_pool_id: aws_identity_pool_id) do
     its('identity_pool_id') { should eq aws_identity_pool_id }
@@ -24,11 +19,6 @@ control 'aws-auto-identity-pool2-1.0' do
     its('saml_provider_arns') { should be_empty }
     its('identity_pool_tags') { should be_empty }
   end
-end
-
-control 'aws-auto-identity-pool3-1.0' do
-  impact 1.0
-  title 'Ensure AWS Identity Pool has the correct properties.'
 
   describe aws_cognito_identity_pool(identity_pool_id: "dummy") do
     it { should_not exist }
