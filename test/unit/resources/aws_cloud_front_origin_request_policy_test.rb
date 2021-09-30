e4a27f7233b7ca9b32d00cea32922a77ff1a6e0b
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-09-30 17:02:37 +0530

    added more tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/unit/resources/aws_cloud_front_origin_request_policy_test.rb
+++ test/unit/resources/aws_cloud_front_origin_request_policy_test.rb
@@ -8,8 +8,12 @@ class AWSCloudFrontOriginRequestPolicyConstructorTest < Minitest::Test
     assert_raises(ArgumentError) { AWSCloudFrontOriginRequestPolicy.new(client_args: { stub_responses: true }) }
   end
 
-  def test_rejects_other_args
-    assert_raises(ArgumentError) { AWSCloudFrontOriginRequestPolicy.new(ids: 'rubbish') }
+  def test_empty_param_arg_not_ok
+    assert_raises(ArgumentError) { AWSCloudFrontOriginRequestPolicy.new(id: '', client_args: { stub_responses: true }) }
+  end
+
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AWSCloudFrontOriginRequestPolicy.new(unexpected: 9) }
   end
 end
 
@@ -20,22 +24,59 @@ class AWSCloudFrontOriginRequestPolicyHappyPathTest < Minitest::Test
     data[:method] = :get_origin_request_policy
     mock_parameter = {}
     mock_parameter[:id] = "test1"
-    mock_parameter[:last_modified_time] = Time.now
-    mock_parameter[:origin_request_policy_config] = {comment: 'test1' , name: 'test1' , headers_config: {header_behavior: 'test1'}, cookies_config: {cookie_behavior: 'test1'}, query_strings_config: { query_string_behavior: 'test1'} }
+    mock_parameter[:last_modified_time] = Time.parse("2013-06-11T23:52:02Z2020-06-05T11:30:39.730000+01:00")
+    mock_parameter[:origin_request_policy_config] = { comment: 'test1',
+                                                      name: 'test1',
+                                                      headers_config: {
+                                                        header_behavior: 'test1',
+                                                        headers: {
+                                                          quantity: 1
+                                                        }
+                                                      },
+                                                      cookies_config: {
+                                                        cookie_behavior: 'test1'
+                                                      },
+                                                      query_strings_config: {
+                                                        query_string_behavior: 'test1'
+                                                      } }
     data[:data] = { origin_request_policy: mock_parameter }
     data[:client] = Aws::CloudFront::Client
-    @origin_request_policy = AWSCloudFrontOriginRequestPolicy.new(id: 'test1', client_args: { stub_responses: true }, stub_data: [data])
+    @resp = AWSCloudFrontOriginRequestPolicy.new(id: 'test1', client_args: { stub_responses: true }, stub_data: [data])
   end
 
   def origin_request_policy_exists
-    assert @origin_request_policy.exists?
+    assert @resp.exists?
   end
 
-  def test_origin_request_policy_id
-    assert_equal(@origin_request_policy.id, 'test1')
+  def test_id
+    assert_equal(@resp.id, 'test1')
   end
 
-  def test_origin_request_policy_etag
-    assert_equal(@origin_request_policy.origin_request_policy_config.name , 'test1')
+  def test_last_modified_time
+    assert_equal(@resp.last_modified_time, Time.parse("2013-06-11T23:52:02Z2020-06-05T11:30:39.730000+01:00"))
+  end
+
+  def test_origin_request_policy_config_comment
+    assert_equal(@resp.origin_request_policy_config.name , 'test1')
+  end
+
+  def test_origin_request_policy_config_comment
+    assert_equal(@resp.origin_request_policy_config.comment , 'test1')
+  end
+
+  def test_origin_request_policy_config_headers_config_header_behavior
+    assert_equal(@resp.origin_request_policy_config.headers_config.header_behavior , 'test1')
+  end
+
+  def test_origin_request_policy_config_headers_config_headers_quantity
+    assert_equal(@resp.origin_request_policy_config.headers_config.headers.quantity , 1)
+  end
+
+  def test_origin_request_policy_config_cookies_config_cookie_behavior
+    assert_equal(@resp.origin_request_policy_config.cookies_config.cookie_behavior , 'test1')
+  end
+
+  def test_origin_request_policy_config_query_strings_config_query_string_behavior
+    assert_equal(@resp.origin_request_policy_config.query_strings_config.query_string_behavior , 'test1')
   end
 end
