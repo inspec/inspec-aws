ed53e6427cd716e3e6592155cbe3c1c0053d7dde
Nirbhay Kumar <nkumar@progress.com>
2021-10-01 12:26:34 +0530

    unit test
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

--- test/unit/resources/aws_s3_access_points_test.rb
+++ test/unit/resources/aws_s3_access_points_test.rb
@@ -19,7 +19,7 @@ class AWSS3AccessPointsHappyPathTest < Minitest::Test
     data = {}
     data[:method] = :list_bucket_metrics_configurations
     mock_parameter = {}
-    mock_parameter[:filter] = { access_point_arn: 'test-id' ,and: { access_point_arn: 'test-id' } }
+    mock_parameter[:filter] = { access_point_arn: 'test-id', and: { access_point_arn: 'test-id' } }
     data[:data] = { :metrics_configuration_list => [mock_parameter] }
     data[:client] = Aws::S3::Client
     @resp = AWSS3AccessPoints.new(bucket_name: "test1", client_args: { stub_responses: true }, stub_data: [data])
