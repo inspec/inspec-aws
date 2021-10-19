4df2e64f45b977c85efdcbab1e861ea5e02a9cfc
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-10-19 13:58:41 +0530

    singular resource added
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/aws_iam_ssh_public_key.rb
@@ -0,0 +1,21 @@
+skip_control 'aws-iam-ssh-public-key-1.0' do
+  impact 1.0
+  title 'Test the properties of the iam ssh public key.'
+
+  describe aws_iam_ssh_public_key(user_name: 'USER_NAME', ssh_public_key_id: 'SSH_PUBLIC_KEY_ID', encoding: 'SSH') do
+    it { should exist }
+  end
+
+  describe aws_iam_ssh_public_key(user_name: 'USER_NAME', ssh_public_key_id: 'SSH_PUBLIC_KEY_ID', encoding: 'SSH') do
+    its('user_name') { should eq 'USER_NAME' }
+    its('ssh_public_key_id') { should eq 'SSH_PUBLIC_KEY_ID' }
+    its('fingerprint') { should_not be_empty }
+    its('ssh_public_key_body') { should_not be_empty }
+    its('status') { should eq 'Active' }
+    its('upload_date') { should_not be_empty }
+  end
+
+  describe aws_iam_ssh_public_key(user_name: 'USER_NAME', ssh_public_key_id: 'SSH_PUBLIC_KEY_ID', encoding: 'SSH') do
+    its('ssh_key_age_valid') { should eq true }
+  end
+end
