9e90557a57d83d7ca6b1fb71af064664b792f76b
Nirbhay Kumar <nkumar@progress.com>
2021-10-22 08:37:15 +0530

    typo
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

--- test/integration/verify/controls/aws_iam_server_certificate.rb
+++ test/integration/verify/controls/aws_iam_server_certificate.rb
@@ -11,7 +11,7 @@ control 'aws_iam_server_certificate-1.0' do
     its('server_certificate_name') { should eq server_certificate_name }
     its('server_certificate_id') { should eq server_certificate_id }
     its('path') { should eq '/' }
-    its('arn') { should eq 'arn:aws:iam::112758395563:server-certificate/some_test_cert' }
+    its('arn') { should eq 'arn:aws:iam::9999999999:server-certificate/some_test_cert' }
     its('upload_date') { should eq Time.parse('2021-09-08T01:58:17Z') }
     its('expiration') { should eq Time.parse('2022-09-08T01:56:24Z') }
   end
--- test/integration/verify/controls/aws_iam_server_certificates.rb
+++ test/integration/verify/controls/aws_iam_server_certificates.rb
@@ -11,7 +11,7 @@ control 'aws_iam_server_certificates-1.0' do
     its('server_certificate_names') { should include server_certificate_name }
     its('server_certificate_ids') { should include server_certificate_id }
     its('paths') { should include '/' }
-    its('arns') { should include 'arn:aws:iam::112758395563:server-certificate/some_test_cert' }
+    its('arns') { should include 'arn:aws:iam::9999999999:server-certificate/some_test_cert' }
     its('upload_date') { should include Time.parse('2021-09-08T01:58:17Z') }
     its('expiration_date') { should include Time.parse('2022-09-08T01:56:24Z') }
   end
