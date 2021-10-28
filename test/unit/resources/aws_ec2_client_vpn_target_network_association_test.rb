c1834b467250d94ce95cae786361539e2f651630
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2021-10-28 17:35:32 +0530

    Update test/unit/resources/aws_ec2_client_vpn_target_network_association_test.rb

--- test/unit/resources/aws_ec2_client_vpn_target_network_association_test.rb
+++ test/unit/resources/aws_ec2_client_vpn_target_network_association_test.rb
@@ -11,8 +11,8 @@ class AWSEC2ClientVPNTargetNetworkAssociationConstructorTest < Minitest::Test
     AWSEC2ClientVPNTargetNetworkAssociation.new(client_vpn_endpoint_id: 'test', association_id: 'test', client_args: { stub_responses: true })
   end
 
-  def test_accepts_vpc_endpoint_id_as_hash
-    AWSEC2ClientVPNTargetNetworkAssociation.new(client_vpn_endpoint_id: 'test',association_id: 'test', client_args: { stub_responses: true })
+  def test_accepts_accepts_vpc_endpoint_id_association_id_as_hash
+    AWSEC2ClientVPNTargetNetworkAssociation.new(client_vpn_endpoint_id: 'test', association_id: 'test', client_args: { stub_responses: true })
   end
 
   def test_rejects_unrecognized_params
