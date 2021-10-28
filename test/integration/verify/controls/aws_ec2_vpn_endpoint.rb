a0e0f82f957ba1f111f75ba3bcc3883495ef88cc
Nirbhay Kumar <nkumar@progress.com>
2021-10-28 18:22:34 +0530

    typo fix
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

--- test/integration/verify/controls/aws_ec2_vpn_endpoint.rb
+++ test/integration/verify/controls/aws_ec2_vpn_endpoint.rb
@@ -9,11 +9,11 @@ skip_control 'aws_ec2_client_vpn_endpoint-1.0' do
     it { should exist }
     its('client_vpn_endpoint_id') { should eq client_vpn_endpoint_id }
     its('description') { should eq "Example Client VPN endpoint" }
-    its('dns_name') { should_not eq '*.cvpn-endpoint-9999999999.prod.clientvpn.us-east-2.amazonaws.com' }
+    its('dns_name') { should_not eq '*.cvpn-endpoint-1234567890.prod.clientvpn.us-east-2.amazonaws.com' }
     its('split_tunnel') { should eq false }
     its('vpn_protocol') { should eq 'openvpn' }
     its('transport_protocol') { should eq 'udp' }
     its('vpn_port') { should_not eq 44375 }
-    its('server_certificate_arn') { should_not eq 'arn:aws:acm:us-east-2:999999999999:certificate/a20fe841-b1ef-4785-aefb-e69838eacdcb' }
+    its('server_certificate_arn') { should_not eq 'arn:aws:acm:us-east-2:1234567890:certificate/a20fe841-b1ef-4785-aefb-e69838eacdcb' }
   end
 end
--- test/integration/verify/controls/aws_ec2_vpn_endpoints.rb
+++ test/integration/verify/controls/aws_ec2_vpn_endpoints.rb
@@ -9,11 +9,11 @@ skip_control 'aws_ec2_client_vpn_endpoints-1.0' do
     it { should exist }
     its('client_vpn_endpoint_ids') { should include client_vpn_endpoint_id }
     its('descriptions') { should include "Example Client VPN endpoint" }
-    its('dns_names') { should_not include '*.cvpn-endpoint-99999999999.prod.clientvpn.us-east-2.amazonaws.com' }
+    its('dns_names') { should_not include '*.cvpn-endpoint-1234567890.prod.clientvpn.us-east-2.amazonaws.com' }
     its('split_tunnels') { should include false }
     its('vpn_protocols') { should include 'openvpn' }
     its('transport_protocols') { should include 'udp' }
     its('vpn_ports') { should_not include 44365 }
-    its('server_certificate_arns') { should_not include 'arn:aws:acm:us-east-2:9999999999:certificate/a20fe841-b1ef-4785-aefb-e69838eacdcb' }
+    its('server_certificate_arns') { should_not include 'arn:aws:acm:us-east-2:1234567890:certificate/a20fe841-b1ef-4785-aefb-e69838eacdcb' }
   end
 end
