321ad10c0833ef8cf909d3b932c16c5b179908ec
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2021-10-28 17:10:11 +0530

    Update test/unit/resources/aws_ec2_client_vpn_endpoint_test.rb
    Co-authored-by: Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>

--- test/unit/resources/aws_ec2_client_vpn_endpoint_test.rb
+++ test/unit/resources/aws_ec2_client_vpn_endpoint_test.rb
@@ -11,7 +11,7 @@ class AWSEC2ClientVPNEndpointConstructorTest < Minitest::Test
     AWSEC2ClientVPNEndpoint.new(client_vpn_endpoint_id: 'test', client_args: { stub_responses: true })
   end
 
-  def test_accepts_vpc_endpoint_id_as_hash
+  def test_accepts_client_vpn_endpoint_id_as_hash
     AWSEC2ClientVPNEndpoint.new(client_vpn_endpoint_id: 'test', client_args: { stub_responses: true })
   end
 
