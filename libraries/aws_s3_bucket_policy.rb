be4ae879556d99524ef42026af26a8c8f937d181
Nirbhay Kumar <nkumar@progress.com>
2021-11-24 17:18:45 +0530

    added resourcce and test
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

---
+++ libraries/aws_s3_bucket_policy.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSRDSS3Policy < AwsResourceBase
+  name 'aws_s3_bucket_policy'
+  desc 'Retrieves information about a bucket policy'
+
+  example "
+    describe aws_s3_bucket_policy(bucket: 'DB_PROXY_NAME') do
+      it { should exist }
+    end
+  "
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters(required: %i(bucket))
+    raise ArgumentError, "#{@__resource_name__}: bucket must be provided" unless opts[:bucket] && !opts[:bucket].empty?
+    @display_name = opts[:bucket]
+    catch_aws_errors do
+      resp = @aws.s3_client.get_bucket_policy({ bucket: opts[:bucket] })
+      @res = resp.policy
+      create_resource_methods(@res)
+    end
+  end
+
+  def exists?
+    !@res.nil? && !@res.empty?
+  end
+
+  def to_s
+    "S3 Bucket name: #{@display_name}"
+  end
+end
