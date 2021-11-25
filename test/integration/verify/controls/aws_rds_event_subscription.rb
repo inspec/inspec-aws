ab77e61cf6cc6d91774354fbb3e9a3e1fde30f0a
Nirbhay Kumar <nkumar@progress.com>
2021-11-25 12:26:46 +0530

    updated
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

--- test/integration/verify/controls/aws_rds_event_subscription.rb
+++ test/integration/verify/controls/aws_rds_event_subscription.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
-event_subscription = attribute(:event_subscription, value: '', description: '')
+event_subscription = attribute(:event_subscription, value: '', description: 'The AWS Auto Scaling Group name.')
 
 control 'aws_rds_event_subscription-1.0' do
   impact 1.0
   title 'Test the properties of Event Subscription.'
   describe aws_rds_event_subscription(subscription_name: event_subscription) do
     it { should exist }
-    its('customer_aws_id') { should eq '11384673629' }
+    its('customer_aws_id') { should_not eq '11384673629' }
     its('cust_subscription_id') { should eq event_subscription }
     its('enabled') { should eq true }
-    its('event_subscription_arn') { should eq 'arn:aws:rds:us-east-2:11384673629:es:test-event-sub' }
+    its('event_subscription_arn') { should_not eq 'arn:aws:rds:us-east-2:112758395563:es:test-event-sub' }
   end
 end
+
--- test/integration/verify/controls/aws_rds_event_subscriptions.rb
+++ test/integration/verify/controls/aws_rds_event_subscriptions.rb
@@ -6,9 +6,9 @@ control 'aws_rds_event_subscriptions-1.0' do
   title 'Test the properties of Event Subscriptions.'
   describe aws_rds_event_subscriptions do
     it { should exist }
-    its('customer_aws_ids') { should include '11384673629' }
+    its('customer_aws_ids') { should_not include '11384673629' }
     its('cust_subscription_ids') { should include event_subscription }
     its('enabled') { should include true }
-    its('event_subscription_arns') { should include 'arn:aws:rds:us-east-2:11384673629:es:test-event-sub' }
+    its('event_subscription_arns') { should_not include 'arn:aws:rds:us-east-2:11384673629:es:test-event-sub' }
   end
 end
