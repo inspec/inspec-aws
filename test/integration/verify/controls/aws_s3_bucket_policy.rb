7d878f19639b9c340b29d76c16ec7ffdfce1495b
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2021-12-05 12:28:27 +0530

    Update test/integration/verify/controls/aws_s3_bucket_policy.rb

--- test/integration/verify/controls/aws_s3_bucket_policy.rb
+++ test/integration/verify/controls/aws_s3_bucket_policy.rb
@@ -1,8 +1,10 @@
-# frozen_string_literal: true
 bucket_name = attribute(:bucket_name, value: '')
-control 'aws_s3_bucket_policy-1.0' do
+
+control 'aws-s3-bucket-policy-1.0' do
+
   impact 1.0
-  title 'Test the properties of a S3 Bucket Policy.'
+  title 'Test the properties of an S3 Bucket Policy.'
+  
   describe aws_s3_bucket_policy(bucket: bucket_name) do
     it { should exist }
     its('policy') { should_not empty }
