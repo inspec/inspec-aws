9d5abe26a3f5c362c413f6c556c3c64f9fdc411e
Nirbhay Kumar <nkumar@progress.com>
2021-12-12 03:16:50 +0530

    resources and tests
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

---
+++ libraries/aws_waf_byte_match_set.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSWAFByteMatchSet < AwsResourceBase
+  name 'aws_waf_byte_match_set'
+  desc 'Describes one WAF byte set.'
+
+  example "
+    describe aws_waf_byte_match_set(byte_match_set_id: 'BucketName') do
+      it { should exits }
+    end
+  "
+
+  def initialize(opts = {})
+    opts = { byte_match_set_id: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: %i(byte_match_set_id))
+    raise ArgumentError, "#{@__resource_name__}: byte_match_set_id must be provided" unless opts[:byte_match_set_id] && !opts[:byte_match_set_id].empty?
+    @display_name = opts[:byte_match_set_id]
+    catch_aws_errors do
+      resp = @aws.waf_client.get_byte_match_set({ byte_match_set_id: opts[:byte_match_set_id] })
+      @resp = resp.byte_match_set.to_h
+      create_resource_methods(@resp)
+    end
+  end
+
+  def byte_match_set_id
+    return nil unless exists?
+    @resp[:byte_match_set_id]
+  end
+
+  def exists?
+    !@resp.nil? && !@resp.empty?
+  end
+
+  def to_s
+    "Resource ID: #{@display_name}"
+  end
+end
---
+++ test/unit/resources/aws_waf_byte_match_sets_test.rb
@@ -0,0 +1,36 @@
+require 'helper'
+require 'aws_waf_byte_match_sets'
+require 'aws-sdk-core'
+
+class AWSWAFByteMatchSetsConstructorTest < Minitest::Test
+
+  def test_empty_params_ok
+    AWSWAFByteMatchSets.new(client_args: { stub_responses: true })
+  end
+
+  def test_rejects_other_args
+    assert_raises(ArgumentError) { AWSWAFByteMatchSets.new(test: 'rubbish') }
+  end
+end
+
+class AWSWAFByteMatchSetsHappyPathTest < Minitest::Test
+
+  def setup
+    data = {}
+    data[:method] = :list_rules
+    mock_data = {}
+    mock_data[:byte_match_set_id] = 'byte_match_set_ids'
+    mock_data[:name] = 'name'
+    data[:data] = mock_data
+    data[:client] = Aws::WAF::Client
+    @resp = AWSWAFByteMatchSets.new(client_args: { stub_responses: true }, stub_data: [data])
+  end
+
+  def test_rule_exists
+    assert @resp.exist?
+  end
+
+  def test_rule_ids
+    assert_equal(@resp.byte_match_set_ids, ['byte_match_set_ids'])
+  end
+end
