b29026699f922f0dac9f902321fe5b2b3eb64880
Nirbhay Kumar <nkumar@progress.com>
2021-12-12 18:33:01 +0530

    resource and unit tests
    Signed-off-by: Nirbhay Kumar <nkumar@progress.com>

---
+++ libraries/aws_waf_sql_injection_match_set.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSWAFSQLInjectionMatchSet < AwsResourceBase
+  name 'aws_waf_sql_injection_match_set'
+  desc 'Describes one WAF SQL Injection match set.'
+
+  example "
+    describe aws_waf_sql_injection_match_set(sql_injection_match_set_id: 'ResourceId') do
+      it { should exits }
+    end
+  "
+
+  def initialize(opts = {})
+    opts = { sql_injection_match_set_id: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: %i(sql_injection_match_set_id))
+    raise ArgumentError, "#{@__resource_name__}: sql_injection_match_set_id must be provided" unless opts[:sql_injection_match_set_id] && !opts[:sql_injection_match_set_id].empty?
+    @display_name = opts[:sql_injection_match_set_id]
+    catch_aws_errors do
+      resp = @aws.waf_client.get_sql_injection_match_set({ sql_injection_match_set_id: opts[:sql_injection_match_set_id] })
+      @resp = resp.sql_injection_match_set.to_h
+      create_resource_methods(@resp)
+    end
+  end
+
+  def sql_injection_match_set_id
+    return nil unless exists?
+    @resp[:sql_injection_match_set_id]
+  end
+
+  def exists?
+    !@resp.nil? && !@resp.empty?
+  end
+
+  def to_s
+    "Resource ID: #{@display_name}"
+  end
+end
