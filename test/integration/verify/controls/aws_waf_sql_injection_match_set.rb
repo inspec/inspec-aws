0acd9d4213db7ae7a64f5f359c58046ff35cf016
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2021-12-13 10:00:32 +0530

    Update test/integration/verify/controls/aws_waf_sql_injection_match_set.rb
    Co-authored-by: Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>

--- test/integration/verify/controls/aws_waf_sql_injection_match_set.rb
+++ test/integration/verify/controls/aws_waf_sql_injection_match_set.rb
@@ -4,6 +4,7 @@ aws_waf_sql_injection_match_set_id = attribute(:aws_waf_sql_injection_match_set_
 control 'aws_waf_sql_injection_match_set-1.0' do
   impact 1.0
   title 'Test the properties of single WAF SQL Injection Match Set.'
+  
   describe aws_waf_sql_injection_match_set(sql_injection_match_set_id: aws_waf_sql_injection_match_set_id) do
     it { should exist }
     its('name') { should eq aws_waf_sql_injection_match_set_name }
