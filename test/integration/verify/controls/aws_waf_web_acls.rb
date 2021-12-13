6c0284a0deef3545dfec338e15c84d0b9db53125
NIRBHAY KUMAR <42607997+Nirbhay1997@users.noreply.github.com>
2021-12-13 09:56:08 +0530

    Update test/integration/verify/controls/aws_waf_web_acls.rb
    Co-authored-by: Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>

--- test/integration/verify/controls/aws_waf_web_acls.rb
+++ test/integration/verify/controls/aws_waf_web_acls.rb
@@ -4,6 +4,7 @@ aws_waf_web_acl_id = attribute(:aws_waf_web_acl_id, value: '')
 control 'aws_waf_web_acl-1.0' do
   impact 1.0
   title 'Test the properties of all the WAF ACLs.'
+  
   describe aws_waf_web_acls do
     it { should exist }
     its('names') { should include aws_waf_web_acl_metric_name }
