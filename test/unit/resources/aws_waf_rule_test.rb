eb829de9b9ec51300d70a27c077bd29b3d9f414f
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2021-12-13 09:19:04 +0530

    Update test/unit/resources/aws_waf_rule_test.rb

--- test/unit/resources/aws_waf_rule_test.rb
+++ test/unit/resources/aws_waf_rule_test.rb
@@ -40,7 +40,7 @@ class AWSWAFRuleHappyPathTest < Minitest::Test
     assert_equal(@resp.rule_id, 'test1')
   end
 
-  def test_rule_id
+  def test_name
     assert_equal(@resp.name, 'test1')
   end
 end
