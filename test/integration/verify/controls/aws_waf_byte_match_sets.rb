00378115af12400a637083823e4beb868aa83dcd
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-12-14 14:50:04 +0530

    added the integration tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_waf_byte_match_sets.rb
+++ test/integration/verify/controls/aws_waf_byte_match_sets.rb
@@ -1,13 +1,18 @@
-aws_waf_byte_match_set_name = attribute(:aws_waf_byte_match_set_name, value: '')
-byte_match_set_ids = attribute(:byte_match_set_ids, value: '')
+aws_waf_byte_match_set_id = attribute(aws_waf_byte_match_set_id, value: '', description: '')
+aws_waf_byte_match_set_name = attribute(aws_waf_byte_match_set_name, value: '', description: '')
 
-control 'aws_waf_byte_match_sets-1.0' do
+title 'Ensure the byte match sets have the correct properties.'
+
+control 'aws-waf-byte-match-sets-1.0' do
   impact 1.0
-  title 'Test the properties of all the WAF Byte Match Sets.'
+
   describe aws_waf_byte_match_sets do
     it { should exist }
+  end
+
+  describe aws_waf_byte_match_sets do
+    its('byte_match_set_ids') { should include aws_waf_byte_match_set_id }
     its('names') { should include aws_waf_byte_match_set_name }
-    its('byte_match_set_ids') { should include byte_match_set_ids }
   end
 end
 
