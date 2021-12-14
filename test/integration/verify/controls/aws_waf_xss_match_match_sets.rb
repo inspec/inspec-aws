38d85a908b869f15316e477e803ef29b29dc6b4c
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-12-14 15:51:26 +0530

    updated the tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_waf_xss_match_match_sets.rb
+++ test/integration/verify/controls/aws_waf_xss_match_match_sets.rb
@@ -1,9 +1,11 @@
-aws_waf_xss_match_set_name = attribute(:aws_waf_ipset_id, value: 'tfsize_constraints')
-aws_waf_xss_match_set_id = attribute(:aws_waf_ipset_name, value: '3901a561-ae05-4a6d-a139-6519f8819b98')
+aws_waf_xss_match_set_name = attribute(:aws_waf_xss_match_set_name, value: '')
+aws_waf_xss_match_set_id = attribute(:aws_waf_xss_match_set_id, value: '')
 
-control 'aws_waf_size_constraint_sets-1.0' do
+control 'aws-waf-size-constraint-sets-1.0' do
+
   impact 1.0
   title 'Test the properties of all WAF xss match set.'
+
   describe aws_waf_xss_match_sets do
     it { should exist }
     its('names') { should include aws_waf_xss_match_set_name }
