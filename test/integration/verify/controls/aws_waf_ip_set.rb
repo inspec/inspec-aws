a88ae9f50d21a512eedf31f1ff05c8c440e073cb
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-12-14 15:28:19 +0530

    added the integration tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_waf_ip_set.rb
+++ test/integration/verify/controls/aws_waf_ip_set.rb
@@ -1,12 +1,20 @@
-aws_waf_ip_set_name = attribute(:aws_waf_ipset_id, value: 'tfIPSet')
-aws_waf_ipset_id = attribute(:aws_waf_ipset_name, value: 'df3af4d8-26bd-4083-8d77-a58bef4184d1')
+aws_waf_ipset_id = attribute(aws_waf_ipset_id, value: '', description: '')
+aws_waf_ipset_name = attribute(aws_waf_ipset_name, value: '', description: '')
 
-control 'aws_waf_ip_set-1.0' do
+title 'Ensure the ip set have the correct properties.'
+
+control 'aws-waf-ip-set-1.0' do
   impact 1.0
-  title 'Test the properties of a WAF IP Set.'
+
   describe aws_waf_ip_set(ip_set_id: aws_waf_ipset_id) do
     it { should exist }
-    its('name') { should eq aws_waf_ip_set_name }
+  end
+
+  describe aws_waf_ip_set(ip_set_id: aws_waf_ipset_id) do
     its('ip_set_id') { should eq aws_waf_ipset_id }
+    its('name') { should eq aws_waf_ipset_name }
+
+    its('ip_set_descriptors_types') { should include 'IPV4' }
+    its('ip_set_descriptors_values') { should include '10.0.0.0/16' }
   end
 end
--- test/integration/verify/controls/aws_waf_ip_sets.rb
+++ test/integration/verify/controls/aws_waf_ip_sets.rb
@@ -1,12 +1,17 @@
-aws_waf_ip_set_name = attribute(:aws_waf_ipset_id, value: 'tfIPSet')
-aws_waf_ipset_id = attribute(:aws_waf_ipset_name, value: 'df3af4d8-26bd-4083-8d77-a58bef4184d1')
+aws_waf_ipset_id = attribute(aws_waf_ipset_id, value: '', description: '')
+aws_waf_ipset_name = attribute(aws_waf_ipset_name, value: '', description: '')
 
-control 'aws_waf_ip_sets-1.0' do
+title 'Ensure the ip sets have the correct properties.'
+
+control 'aws-waf-ip-sets-1.0' do
   impact 1.0
-  title 'Test the properties of all the WAF IP Sets.'
+
   describe aws_waf_ip_sets do
     it { should exist }
-    its('names') { should include aws_waf_ip_set_name }
+  end
+
+  describe aws_waf_ip_sets do
     its('ip_set_ids') { should include aws_waf_ipset_id }
+    its('names') { should include aws_waf_ipset_name }
   end
 end
