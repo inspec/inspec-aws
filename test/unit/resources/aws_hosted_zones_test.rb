44130abb5a7641a55ebb7de30f8a03d35b748d79
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-12-14 12:49:35 +0530

    fixed the unit tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/unit/resources/aws_hosted_zones_test.rb
+++ test/unit/resources/aws_hosted_zones_test.rb
@@ -12,50 +12,28 @@ end
 class AWSHostedZonesSuccessPathTest < Minitest::Test
 
   def setup
-    data = {}
-    data[:method] = :list_hosted_zones
     mock_data = {}
-    mock_data[:marker] = 'test1'
-    mock_data[:is_truncated] = true
-    mock_data[:max_items] = 1
-    mock_data[:hosted_zones] = [
-      id: 'test1',
-      name: 'test1',
-      caller_reference: 'test1',
-      config: {},
-      resource_record_set_count: 10,
-      linked_service: {},
-    ]
-    data[:data] = mock_data
-    data[:client] = Aws::Route53::Client
-    @resp = AWSHostedZones.new(client_args: { stub_responses: true }, stub_data: [data])
+    mock_data[:method] = :list_hosted_zones
+    mock_data[:data]   = { hosted_zones:
+                              [{name: "test1", id: "test1", caller_reference: "test1"}],
+                            marker: "nil", is_truncated: false, next_marker: "nil", max_items: 100}
+    mock_data[:client] = Aws::Route53::Client
+    @resp = AWSHostedZones.new(client_args: { stub_responses: true }, stub_data: [mock_data])
   end
 
-  def test_hosted_zones_exist
-    assert @resp.exists?
+  def test_hosted_zones_exists
+    assert @resp.exist?
   end
 
-  def test_ids
+  def ids
     assert_equal(@resp.ids, ['test1'])
   end
 
-  def test_names
+  def names
     assert_equal(@resp.names, ['test1'])
   end
 
-  def test_caller_references
+  def caller_references
     assert_equal(@resp.caller_references, ['test1'])
-  end
-
-  def test_configs
-    assert_equal(@resp.configs, [{}])
-  end
-
-  def test_resource_record_set_counts
-    assert_equal(@resp.resource_record_set_counts, [10])
-  end
-
-  def test_linked_services
-    assert_equal(@resp.linked_services, [{}])
   end
 end
