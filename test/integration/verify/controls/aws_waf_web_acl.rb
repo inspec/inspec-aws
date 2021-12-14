620a93de6f3e2166ae713b355bd7c4e6c760e189
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2021-12-14 15:08:13 +0530

    added the integration tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_waf_web_acl.rb
+++ test/integration/verify/controls/aws_waf_web_acl.rb
@@ -1,13 +1,27 @@
-aws_waf_web_acl_metric_name = attribute(:aws_waf_web_acl_metric_name, value: '')
-aws_waf_web_acl_id = attribute(:aws_waf_web_acl_id, value: '')
+aws_waf_web_acl_id = attribute(aws_waf_web_acl_id, value: '', description: '')
+aws_waf_web_acl_name = attribute(aws_waf_web_acl_name, value: '', description: '')
 
-control 'aws_waf_web_acl-1.0' do
+title 'Ensure the web acl have the correct properties.'
+
+control 'aws-waf-web-acl-1.0' do
   impact 1.0
-  title 'Test the properties of single WAF ACL.'
-  
+
   describe aws_waf_web_acl(web_acl_id: aws_waf_web_acl_id) do
     it { should exist }
-    its('name') { should eq aws_waf_web_acl_metric_name }
+  end
+
+  describe aws_waf_web_acl(web_acl_id: aws_waf_web_acl_id) do
     its('web_acl_id') { should eq aws_waf_web_acl_id }
+    its('name') { should eq aws_waf_web_acl_name }
+
+    its('rules_priorities') { should_not be_empty }
+    its('rules_rule_ids') { should_not be_empty }
+    its('rules_actions') { should_not be_empty }
+    its('rules_action_types') { should include 'BLOCK' }
+    its('rules_override_actions') { should_not be_empty }
+    its('rules_override_action_types') { should_not be_empty }
+    its('rules_types') { should_not be_empty }
+    its('rules_excluded_rules') { should_not be_empty }
+    its('rules_excluded_rules_rule_ids') { should_not be_empty }
   end
 end
