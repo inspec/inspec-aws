88b5d714a0f7e226a5f8a95f6c8673bb3fb41922
Nirbhay <nkumar@progress.com>
2022-02-21 20:41:52 +0530

    aws_api_gateway_response
    Signed-off-by: Nirbhay <nkumar@progress.com>

---
+++ libraries/aws_api_gateway_responses.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSApiGatewayResponses < AwsResourceBase
+  name 'aws_api_gateway_responses'
+  desc 'Retrieves API Gateway Responses.'
+
+  example "
+    describe aws_api_gateway_documentation_part(rest_api_id: REST_API_ID) do
+      it { should exist }
+    end
+  "
+
+  attr_reader :table
+
+  FilterTable.create
+             .register_column(:response_type, field: :response_type)
+             .register_column(:status_code, field: :status_code)
+             .register_column(:response_parameters, field: :response_parameters)
+             .register_column(:response_templates, field: :response_templates)
+             .register_column(:default_response, field: :default_response)
+             .install_filter_methods_on_resource(self, :table)
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters(required: %i(rest_api_id))
+    raise ArgumentError, "#{@__resource_name__}: rest_api_id must be provided" unless opts[:rest_api_id] && !opts[:rest_api_id].empty?
+    @table = fetch_data
+  end
+
+  def fetch_data
+    catch_aws_errors do
+      @table = @aws.apigateway_client.get_gateway_responses(rest_api_id: opts[:rest_api_id]).map do |table|
+        table.items.map { |table_name| {
+          response_type: table_name.response_type,
+          status_code: table_name.status_code,
+          response_parameters: table_name.response_parameters["String"],
+          response_templates: table_name.response_templates["String"],
+          default_response: table_name.default_response,
+        }
+        }
+      end.flatten
+    end
+  end
+end
---
+++ test/unit/resources/aws_api_gateway_response_test.rb
@@ -0,0 +1,59 @@
+require 'helper'
+require 'aws_api_gateway_response'
+require 'aws-sdk-core'
+
+class AWSApiGatewayResponseConstructorTest < Minitest::Test
+
+  def test_empty_params_not_ok
+    assert_raises(ArgumentError) { AWSApiGatewayResponse.new(rest_api_id: '', model_name: '', client_args: { stub_responses: true }) }
+  end
+
+  def test_empty_param_arg_not_ok
+    assert_raises(ArgumentError) { AWSApiGatewayResponse.new(rest_api_id: '', model_name: '', client_args: { stub_responses: true }) }
+  end
+
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AWSApiGatewayResponse.new(unexpected: 9) }
+  end
+end
+
+class AWSApiGatewayResponseHappyPathTest < Minitest::Test
+
+  def setup
+    data = {}
+    data[:method] = :get_gateway_response
+    mock_data = {}
+    mock_data[:id] = 'test'
+    mock_data[:name] = 'test'
+    mock_data[:description] = 'test'
+    mock_data[:content_type] = 'test'
+    mock_data[:schema] = 'test'
+    data[:data] = mock_data
+    data[:client] = Aws::APIGateway::Client
+    @resp = AWSApiGatewayResponse.new(rest_api_id: 'test', model_name: 'test', client_args: { stub_responses: true }, stub_data: [data])
+  end
+
+  def test_model_exists
+    assert @resp.exist?
+  end
+
+  def test_id
+    assert_equal(@resp.id, 'test')
+  end
+
+  def test_name
+    assert_equal(@resp.name, 'test')
+  end
+
+  def test_description
+    assert_equal(@resp.description, 'test')
+  end
+
+  def test_content_type
+    assert_equal(@resp.content_type, 'test')
+  end
+
+  def test_schema
+    assert_equal(@resp.schema, 'test')
+  end
+end
