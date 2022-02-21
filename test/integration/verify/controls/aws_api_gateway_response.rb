d6f0e23675ef24b8253a1f9b3b3c1b82c7c03f42
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-02-21 21:21:09 +0530

    added the integration tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/aws_api_gateway_response.rb
@@ -0,0 +1,17 @@
+aws_api_gateway_gateway_response_response_type = attribute("aws_api_gateway_gateway_response_response_type", value: "", description: "")
+aws_api_gateway_gateway_response_rest_api_id = attribute("aws_api_gateway_gateway_response_rest_api_id", value: "", description: "")
+
+control 'aws-api-gateway-response-1.0' do
+  impact 1.0
+  title 'Test the properties of the api gateway response.'
+
+  describe aws_api_gateway_response(rest_api_id: aws_api_gateway_gateway_response_rest_api_id, response_type: aws_api_gateway_gateway_response_response_type) do
+    it { should exist }
+  end
+
+  describe aws_api_gateway_response(rest_api_id: aws_api_gateway_gateway_response_rest_api_id, response_type: aws_api_gateway_gateway_response_response_type) do
+    its('response_type') { should eq aws_api_gateway_gateway_response_response_type }
+    its('status_code') { should eq "401" }
+    its('default_response') { should eq false }
+  end
+end
---
+++ test/integration/verify/controls/aws_api_gateway_responses.rb
@@ -0,0 +1,17 @@
+aws_api_gateway_gateway_response_response_type = attribute("aws_api_gateway_gateway_response_response_type", value: "", description: "")
+aws_api_gateway_gateway_response_rest_api_id = attribute("aws_api_gateway_gateway_response_rest_api_id", value: "", description: "")
+
+control 'aws-api-gateway-responses-1.0' do
+  impact 1.0
+  title 'Test the properties of the api gateway responses.'
+
+  describe aws_api_gateway_responses(rest_api_id: aws_api_gateway_gateway_response_rest_api_id) do
+    it { should exist }
+  end
+
+  describe aws_api_gateway_responses(rest_api_id: aws_api_gateway_gateway_response_rest_api_id) do
+    its('response_types') { should include aws_api_gateway_gateway_response_response_type }
+    its('status_codes') { should include "401" }
+    its('default_responses') { should include false }
+  end
+end
