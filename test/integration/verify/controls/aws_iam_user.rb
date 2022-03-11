51f1def830bd4dd46d92f66003a890ea49e58165
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-03-11 21:30:59 +0530

    adding the integration test in the singular resource
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_iam_user.rb
+++ test/integration/verify/controls/aws_iam_user.rb
@@ -12,4 +12,16 @@ control 'aws-iam-user-1.0' do
     its ('username') { should eq aws_iam_user_name}
     its ('user_arn') { should_not be_nil}
   end
+
+  describe aws_iam_user(user_name: aws_iam_user_name) do
+    it { should exist }
+    its("user_path") { should eq '/' }
+    its("username") { should eq user_name }
+    its("user_id") { should eq '' }
+    its("user_arn") { should eq '' }
+    its("user_create_date") { should eq Time.parse('') }
+    its("user_password_last_used") { should eq Time.parse('') }
+    its("user_permissions_boundary") { should_not be_empty }
+    its("user_tags") { should be_empty }
+  end
 end
