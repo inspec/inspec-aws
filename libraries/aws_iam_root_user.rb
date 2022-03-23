47eb84452dcb759f4de04082c645b6468b5a1b2a
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-03-23 14:25:36 +0530

    added missing attributes
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/aws_iam_root_user.rb
+++ libraries/aws_iam_root_user.rb
@@ -11,20 +11,55 @@ class AwsIamRootUser < AwsResourceBase
     end
   "
 
-  attr_reader :summary_account, :virtual_devices
+  attr_reader :summary_account, :virtual_devices,
+              :total_access_key_per_user_quota, :total_access_keys_present, :total_account_mfa_enabled,
+              :total_account_signing_certificates_present, :total_attached_policies_per_group_quota,
+              :total_attached_policies_per_role_quota, :total_attached_policies_per_user_quota,
+              :total_global_endpoint_token_version, :total_group_policy_size_quota, :total_groups, :total_groups_per_user_quota,
+              :total_groups_quota, :total_mfa_devices, :total_mfa_devices_in_use, :total_policies, :total_policies_quota,
+              :total_policy_size_quota, :total_policy_versions_in_use, :total_policy_versions_in_use_quota,
+              :total_server_certificates, :total_server_certificates_quota,
+              :total_signing_certificates_per_user_quota, :total_user_policy_size_quota,
+              :total_users, :total_users_quota, :total_versions_per_policy_quota
 
   def initialize(opts = {})
     super(opts)
     validate_parameters
-
     catch_aws_errors do
       @summary_account = @aws.iam_client.get_account_summary.summary_map
       @virtual_devices = @aws.iam_client.list_virtual_mfa_devices.virtual_mfa_devices
     end
+    @total_access_key_per_user_quota = @summary_account['AccessKeysPerUserQuota']
+    @total_access_keys_present = @summary_account['AccountAccessKeysPresent']
+    @total_account_mfa_enabled = @summary_account['AccountMFAEnabled']
+    @total_account_signing_certificates_present = @summary_account['AccountSigningCertificatesPresent']
+    @total_attached_policies_per_group_quota = @summary_account['AttachedPoliciesPerGroupQuota']
+    @total_attached_policies_per_role_quota = @summary_account['AttachedPoliciesPerRoleQuota']
+    @total_attached_policies_per_user_quota = @summary_account['AttachedPoliciesPerUserQuota']
+    @total_global_endpoint_token_version = @summary_account['GlobalEndpointTokenVersion']
+    @total_group_policy_size_quota = @summary_account['GroupPolicySizeQuota']
+    @total_groups = @summary_account['Groups']
+    @total_groups_per_user_quota = @summary_account['GroupsPerUserQuota']
+    @total_groups_quota = @summary_account['GroupsQuota']
+    @total_mfa_devices = @summary_account['MFADevices']
+    @total_mfa_devices_in_use = @summary_account['MFADevicesInUse']
+    @total_policies = @summary_account['Policies']
+    @total_policies_quota = @summary_account['PoliciesQuota']
+    @total_policy_size_quota = @summary_account['PolicySizeQuota']
+    @total_policy_versions_in_use = @summary_account['PolicyVersionsInUse']
+    @total_policy_versions_in_use_quota = @summary_account['PolicyVersionsInUseQuota']
+    @total_server_certificates = @summary_account['ServerCertificates']
+    @total_server_certificates_quota = @summary_account['ServerCertificatesQuota']
+    @total_signing_certificates_per_user_quota = @summary_account['SigningCertificatesPerUserQuota']
+    @total_user_policy_size_quota = @summary_account['UserPolicySizeQuota']
+    @total_users = @summary_account['Users']
+    @total_users_quota = @summary_account['UsersQuota']
+    @total_versions_per_policy_quota = @summary_account['VersionsPerPolicyQuota']
+
   end
 
   def has_access_key?
-    @summary_account['AccountAccessKeysPresent'] == 1
+    @summary_account['AccountAccessKeysPresent'].present?
   end
 
   def has_mfa_enabled?
@@ -46,19 +81,19 @@ class AwsIamRootUser < AwsResourceBase
   end
 
   def has_account_signing_certificate?
-    @summary_account['AccountSigningCertificatesPresent'] == 1
+    @summary_account['AccountSigningCertificatesPresent'].present?
   end
 
   def has_MFA_devices_in_use?
-    @summary_account['MFADevicesInUse'] == 1
+    @summary_account['MFADevicesInUse'] >= 1
   end
 
   def has_policy_versions_in_use?
-    @summary_account['PolicyVersionsInUse'] == 1
+    @summary_account['PolicyVersionsInUse'] >= 1
   end
 
   def has_min_users?
-    @summary_account['Users'] == 1
+    @summary_account['Users'] >= 1
   end
 
   def to_s
--- test/integration/verify/controls/aws_iam_root_user.rb
+++ test/integration/verify/controls/aws_iam_root_user.rb
@@ -1,14 +1,44 @@
-control 'aws-iam-root-user-1.0' do
-
+skip_control 'aws-iam-root-user-1.0' do
   impact 1.0
   title 'Test the AWS IAM Root User'
 
   describe aws_iam_root_user do
-#
-#   Leaving below as examples:
-#
-#    it { should have_mfa_enabled }
-#    it { should have_virtual_mfa_enabled }
-    it { should_not have_access_key }
+    it { should have_access_key }
+    it { should have_account_signing_certificate }
+    it { should have_MFA_devices_in_use }
+    it { should have_policy_versions_in_use }
+    it { should have_min_users }
+    it { should have_virtual_mfa_enabled }
+    it { should_not have_hardware_mfa_enabled }
+    it { should have_mfa_enabled }
+  end
+
+  describe aws_iam_root_user do
+    its('total_access_key_per_user_quota') { should eq 2 }
+    its('total_access_keys_present') { should eq 0 }
+    its('total_account_mfa_enabled') { should eq 1 }
+    its('total_account_signing_certificates_present') { should eq 0 }
+    its('total_attached_policies_per_group_quota') { should eq 10 }
+    its('total_attached_policies_per_role_quota') { should eq 10 }
+    its('total_attached_policies_per_user_quota') { should eq 10 }
+    its('total_global_endpoint_token_version') { should eq 1 }
+    its('total_group_policy_size_quota') { should eq 5120 }
+    its('total_groups') { should eq 16 }
+    its('total_groups_per_user_quota') { should eq 10 }
+    its('total_groups_quota') { should eq 300 }
+    its('total_mfa_devices') { should eq 2 }
+    its('total_mfa_devices_in_use') { should eq 2 }
+    its('total_policies') { should eq 124 }
+    its('total_policies_quota') { should eq 1500 }
+    its('total_policy_size_quota') { should eq 6144 }
+    its('total_policy_versions_in_use') { should eq 221 }
+    its('total_policy_versions_in_use_quota') { should eq 10000 }
+    its('total_server_certificates') { should eq 1 }
+    its('total_server_certificates_quota') { should eq 20 }
+    its('total_signing_certificates_per_user_quota') { should eq 2 }
+    its('total_user_policy_size_quota') { should eq 2048 }
+    its('total_users') { should eq 94 }
+    its('total_users_quota') { should eq 5000 }
+    its('total_versions_per_policy_quota') { should eq 5 }
   end
 end
