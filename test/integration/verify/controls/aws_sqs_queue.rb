1cdbcc6d74b9ab9ebe657eff1f3caa98e43fddf7
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-04-17 20:42:54 +0530

    added the policy tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_sqs_queue.rb
+++ test/integration/verify/controls/aws_sqs_queue.rb
@@ -27,10 +27,20 @@ end
 control 'aws-sqs-queue-2.0' do
   impact 1.0
   title 'Ensure AWS SQS Queue is SQS managed.'
-
+  
   describe aws_sqs_queue(queue_url) do
     it { should exist }
     its("SqsManagedSseEnabled") { should eq "true" }
     its("sqs_managed") { should eq true }
+  end
+end
+
+control 'aws-sqs-queue-3.0' do
+  impact 1.0
+  title 'Check the policy permissions.'
+  
+  describe aws_sqs_queue(queue_url) do
+    it { should be_policy_statement_principal_all_permissions_enabled }
+    it { should be_policy_statement_action_all_permissions_enabled }
   end
 end
