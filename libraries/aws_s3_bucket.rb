583dec9fee2d2dab915ae225289fab79b92c53d3
Rohit Joshi <rohit.prasad.joshi@sap.com>
2022-04-18 18:23:40 -0500

    adding 'prevent public access' attribute for s3 bucket
    Signed-off-by: Rohit Joshi <rohit.prasad.joshi@sap.com>

--- libraries/aws_s3_bucket.rb
+++ libraries/aws_s3_bucket.rb
@@ -68,6 +68,17 @@ class AwsS3Bucket < AwsResourceBase
     end
   end
 
+  def prevent_public_access?
+    return false unless exists?
+    catch_aws_errors do
+      public_access_config = @aws.storage_client.get_public_access_block(bucket: @bucket_name).public_access_block_configuration
+      @prevent_public_access ||= public_access_config.block_public_acls == true &&
+                                 public_access_config.ignore_public_acls == true &&
+                                 public_access_config.block_public_policy == true &&
+                                 public_access_config.restrict_public_buckets == true
+    end
+  end
+
   def has_default_encryption_enabled?
     return false unless exists?
     @has_default_encryption_enabled ||= catch_aws_errors do
