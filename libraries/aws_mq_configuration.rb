57735d18ca421a0817e793a4b6190523b33afa5b
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-06-02 14:51:54 +0530

    adding the mq changes
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/aws_mq_configuration.rb
+++ libraries/aws_mq_configuration.rb
@@ -4,37 +4,37 @@ require 'aws_backend'
 
 class AWSMQConfiguration < AwsResourceBase
   name 'aws_mq_configuration'
-  desc 'Describes a Amazon MQ configuration.'
-
-  example "
-    describe aws_mq_configuration(configuration_id: 'test1') do
+  desc 'Returns information about the specified configuration.'
+  example <<-EXAMPLE
+    describe aws_mq_configuration(configuration_id: 'CONFIGURATION_ID') do
       it { should exist }
     end
-  "
+  EXAMPLE
 
   def initialize(opts = {})
-    opts = { configuration_id: opts } if opts.is_a?(String)
     super(opts)
-    validate_parameters(required: [:configuration_id])
-
-    raise ArgumentError, "#{@__resource_name__}: configuration_id must be provided" unless opts[:configuration_id] && !opts[:configuration_id].empty?
+    validate_parameters(required: %i(configuration_id))
+    raise ArgumentError, "#{@__resource_name__}: configuration_id must be provided" if opts[:configuration_id].blank?
     @display_name = opts[:configuration_id]
-    resp = @aws.mq_client.describe_configuration({ configuration_id: opts[:configuration_id] })
-    @configurations = resp.to_h
-    create_resource_methods(@configurations)
+    catch_aws_errors do
+      resp = @aws.mq_client.describe_configuration({ configuration_id: opts[:configuration_id] })
+      @res = resp.to_h
+      @arn = @res[:arn]
+      create_resource_methods(@res)
+    end
+  end
+
+  def resource_id
+    @res? @arn: @display_name
+  end
+
+  def exists?
+    !@res.blank?
   end
 
   def configuration_id
     return nil unless exists?
-    @configurations[:configuration_id]
-  end
-
-  def resource_id
-    @configurations? @configurations[:configuration_id] : @display_name
-  end
-
-  def exists?
-    !@configurations.nil? && !@configurations.empty?
+    @res[:id]
   end
 
   def to_s
