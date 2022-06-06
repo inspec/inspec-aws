44415d437f09ec324629a7e017b3f0dd8a67ad1b
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-06-06 14:59:28 +0530

    added the singular and plural resources
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ libraries/aws_api_gateway_vpc_link.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSApiGatewayVPCLink < AwsResourceBase
+  name 'aws_api_gateway_vpc_link'
+  desc 'Gets a specified VPC link under the callers account in a region.'
+
+  example "
+    describe aws_api_gateway_vpc_link(vpc_link_id: 'VPC_LINK_ID') do
+      it { should exist }
+    end
+  "
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters(required: %i(vpc_link_id))
+    raise ArgumentError, "#{@__resource_name__}: vpc_link_id must be provided" if opts[:vpc_link_id].blank?
+    @display_name = opts[:vpc_link_id]
+    catch_aws_errors do
+      resp = @aws.apigateway_client.get_vpc_link({ vpc_link_id: opts[:vpc_link_id] })
+      @res = resp.to_h
+      create_resource_methods(@res)
+    end
+  end
+
+  def exists?
+    !@res.blank?
+  end
+
+  def vpc_link_id
+    return nil unless exists?
+    @res[:vpc_link_id]
+  end
+
+  def to_s
+    "VPC Link ID: #{@display_name}"
+  end
+end
---
+++ libraries/aws_api_gateway_vpc_links.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSApiGatewayVPCLinks < AwsCollectionResourceBase
+  name 'aws_api_gateway_vpc_links'
+  desc 'Gets the VpcLinks collection under the callers account in a selected region.'
+
+  example "
+    describe aws_api_gateway_vpc_links do
+      it { should exist }
+    end
+  "
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch(client: :apigateway_client, operation: :get_vpc_links).items.map(&:to_h)
+    populate_filter_table_from_response
+  end
+end
