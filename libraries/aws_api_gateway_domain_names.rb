60eb1b258fab361baec9a8d2aaa090b9420c7971
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-06-06 16:19:32 +0530

    added the libraries of the resource
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ libraries/aws_api_gateway_domain_names.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AWSApiGatewayDomainNames < AwsCollectionResourceBase
+  name 'aws_api_gateway_domain_names'
+  desc 'Represents a collection of DomainName resources.'
+
+  example "
+    describe aws_api_gateway_domain_names do
+      it { should exist }
+    end
+  "
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch(client: :apigateway_client, operation: :get_domain_names).items.map(&:to_h)
+    populate_filter_table_from_response
+  end
+end
