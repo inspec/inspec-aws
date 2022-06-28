84eed5450ab7a2524c66e3e018a87790bf1cbf15
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-06-28 16:37:38 +0530

    adding the singular doc
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/aws_api_gateway_v2_authorizer.rb
@@ -0,0 +1,31 @@
+aws_apigatewayv2_authorizer_id = input(:aws_apigatewayv2_authorizer_id, value: '', description: '')
+aws_apigatewayv2_authorizer_api_id = input(:aws_apigatewayv2_authorizer_api_id, value: '', description: '')
+aws_apigatewayv2_authorizer_authorizer_type = input(:aws_apigatewayv2_authorizer_authorizer_type, value: '', description: '')
+aws_apigatewayv2_authorizer_identity_sources = input(:aws_apigatewayv2_authorizer_identity_sources, value: '', description: '')
+aws_apigatewayv2_authorizer_jwt_configuration_audience = input(:aws_apigatewayv2_authorizer_jwt_configuration_audience, value: '', description: '')
+aws_apigatewayv2_authorizer_jwt_configuration_issuer = input(:aws_apigatewayv2_authorizer_jwt_configuration_issuer, value: '', description: '')
+aws_apigatewayv2_authorizer_name = input(:aws_apigatewayv2_authorizer_name, value: '', description: '')
+
+control 'aws-api-gateway-v2-api-authorizer' do
+  impact 1.0
+  title 'Ensure API Gateway API Authorizer resource has the correct properties.'
+  
+  describe aws_api_gateway_v2_authorizer(api_id: aws_apigatewayv2_authorizer_api_id, authorizer_id: aws_apigatewayv2_authorizer_id) do
+    it { should exist }
+  end
+  
+  describe aws_api_gateway_v2_authorizer(api_id: aws_apigatewayv2_authorizer_api_id, authorizer_id: aws_apigatewayv2_authorizer_id) do
+    its('authorizer_credentials_arn') { should be_empty }
+    its('authorizer_id') { should eq aws_apigatewayv2_authorizer_id }
+    its('authorizer_result_ttl_in_seconds') { should be_empty }
+    its('authorizer_type') { should eq aws_apigatewayv2_authorizer_authorizer_type }
+    its('authorizer_uri') { should be_empty }
+    its('identity_source') { should include aws_apigatewayv2_authorizer_identity_sources }
+    its('identity_validation_expression') { should be_empty }
+    its('jwt_configuration.audience') { should include aws_apigatewayv2_authorizer_jwt_configuration_audience }
+    its('jwt_configuration.issuer') { should eq aws_apigatewayv2_authorizer_jwt_configuration_issuer }
+    its('name') { should eq aws_apigatewayv2_authorizer_name }
+    its('authorizer_payload_format_version') { should be_empty }
+    its('enable_simple_responses') { should be_empty }
+  end
+end
---
+++ test/integration/verify/controls/aws_api_gateway_v2_authorizers.rb
@@ -0,0 +1,32 @@
+aws_apigatewayv2_authorizer_id = input(:aws_apigatewayv2_authorizer_id, value: '', description: '')
+aws_apigatewayv2_authorizer_api_id = input(:aws_apigatewayv2_authorizer_api_id, value: '', description: '')
+aws_apigatewayv2_authorizer_authorizer_type = input(:aws_apigatewayv2_authorizer_authorizer_type, value: '', description: '')
+aws_apigatewayv2_authorizer_identity_sources = input(:aws_apigatewayv2_authorizer_identity_sources, value: '', description: '')
+aws_apigatewayv2_authorizer_name = input(:aws_apigatewayv2_authorizer_name, value: '', description: '')
+
+control 'aws-api-gateway-v2-api-authorizers' do
+  impact 1.0
+  title 'Ensure API Gateway API Authorizers resource has the correct properties.'
+  
+  describe aws_api_gateway_v2_authorizers(api_id: aws_apigatewayv2_authorizer_api_id) do
+    it { should exist }
+  end
+  
+  describe aws_api_gateway_v2_authorizers(api_id: aws_apigatewayv2_authorizer_api_id) do
+    its('authorizer_credentials_arns') { should be_empty }
+    its('authorizer_ids') { should include aws_apigatewayv2_authorizer_id }
+    its('authorizer_result_ttl_in_seconds') { should be_empty }
+    its('authorizer_types') { should include aws_apigatewayv2_authorizer_authorizer_type }
+    its('authorizer_uris') { should be_empty }
+    its('identity_sources') { should include aws_apigatewayv2_authorizer_identity_sources }
+    its('identity_validation_expressions') { should be_empty }
+    its('jwt_configurations') { should_not be_empty }
+    its('names') { should include aws_apigatewayv2_authorizer_name }
+    its('authorizer_payload_format_versions') { should be_empty }
+    its('enable_simple_responses') { should be_empty }
+  end
+end
+
+
+
+
