8c84bfbc06ab31be0b40d43ec2aedc2248b5e1c6
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-06-29 17:48:19 +0530

    adding more tests in the plural resource
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_api_gateway_v2_authorizers.rb
+++ test/integration/verify/controls/aws_api_gateway_v2_authorizers.rb
@@ -1,8 +1,8 @@
-aws_apigatewayv2_authorizer_id = input(:aws_apigatewayv2_authorizer_id, value: '', description: '')
-aws_apigatewayv2_authorizer_api_id = input(:aws_apigatewayv2_authorizer_api_id, value: '', description: '')
-aws_apigatewayv2_authorizer_authorizer_type = input(:aws_apigatewayv2_authorizer_authorizer_type, value: '', description: '')
-aws_apigatewayv2_authorizer_identity_sources = input(:aws_apigatewayv2_authorizer_identity_sources, value: '', description: '')
-aws_apigatewayv2_authorizer_name = input(:aws_apigatewayv2_authorizer_name, value: '', description: '')
+aws_apigatewayv2_authorizer_id = input(:aws_apigatewayv2_authorizer_id, value: '', description: 'The authorizer identifier.')
+aws_apigatewayv2_authorizer_api_id = input(:aws_apigatewayv2_authorizer_api_id, value: '', description: 'The API identifier.')
+aws_apigatewayv2_authorizer_authorizer_type = input(:aws_apigatewayv2_authorizer_authorizer_type, value: '', description: 'The authorizer type.')
+aws_apigatewayv2_authorizer_identity_sources = input(:aws_apigatewayv2_authorizer_identity_sources, value: '', description: 'The identity source for which authorization is requested.')
+aws_apigatewayv2_authorizer_name = input(:aws_apigatewayv2_authorizer_name, value: '', description: 'The authorizer name.')
 
 control 'aws-api-gateway-v2-api-authorizers' do
   impact 1.0
@@ -10,6 +10,10 @@ control 'aws-api-gateway-v2-api-authorizers' do
   
   describe aws_api_gateway_v2_authorizers(api_id: aws_apigatewayv2_authorizer_api_id) do
     it { should exist }
+  end
+  
+  describe aws_api_gateway_v2_authorizers(api_id: aws_apigatewayv2_authorizer_api_id) do
+    its('count') { should >= 1 }
   end
   
   describe aws_api_gateway_v2_authorizers(api_id: aws_apigatewayv2_authorizer_api_id) do
@@ -24,6 +28,10 @@ control 'aws-api-gateway-v2-api-authorizers' do
     its('names') { should include aws_apigatewayv2_authorizer_name }
     its('authorizer_payload_format_versions') { should be_empty }
     its('enable_simple_responses') { should be_empty }
+  end
+
+  describe aws_api_gateway_v2_authorizers(api_id: 'dummy') do
+    it { should_not exist }
   end
 end
 
