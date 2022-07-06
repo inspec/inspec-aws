3bbeb7be924690df037cd81f4fa04248e1f366ea
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-07-06 16:37:45 +0530

    adding a negative test in  singular integration test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/aws_api_gateway_v2_deployment.rb
+++ test/integration/verify/controls/aws_api_gateway_v2_deployment.rb
@@ -18,4 +18,8 @@ control 'aws-api-gateway-v2-deployment' do
     its('deployment_status_message') { should be_empty }
     its('description') { should eq aws_apigatewayv2_deployment_description }
   end
+
+  describe aws_api_gateway_v2_deployment(api_id: aws_apigatewayv2_deployment_api_id, deployment_id: 'dummy') do
+    it { should_not exist }
+  end
 end
