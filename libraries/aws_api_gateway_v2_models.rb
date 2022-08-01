20e21b28bc3b2ed0662748427f480371cbc4af9c
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-08-01 10:16:26 +0530

    plural resource added
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ libraries/aws_api_gateway_v2_models.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'aws_backend'
+
+class AwsApiGatewayV2Models < AwsCollectionResourceBase
+  name 'aws_api_gateway_v2_models'
+  desc 'Gets the Models for an API.'
+  example <<-EXAMPLE
+    describe aws_api_gateway_v2_models(api_id: 'APP_ID') do
+      it { should exist }
+    end
+  EXAMPLE
+
+  def initialize(opts = {})
+    opts = { api_id: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: %i(api_id))
+    raise ArgumentError, "#{@__resource_name__}: api_id must be provided!" if opts[:api_id].blank?
+    catch_aws_errors do
+      @table = fetch(client: :apigatewayv2_client, operation: :get_models, kwargs: { api_id: opts[:api_id] }).items.map(&:to_h)
+      populate_filter_table_from_response
+    end
+  end
+end
