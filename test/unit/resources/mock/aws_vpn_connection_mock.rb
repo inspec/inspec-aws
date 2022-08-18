470d17def5ef034777f2fa4fa48922b142c376e8
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-08-18 15:53:28 +0530

    adding more test in the unit test of the resource
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/unit/resources/mock/aws_vpn_connection_mock.rb
+++ test/unit/resources/mock/aws_vpn_connection_mock.rb
@@ -1,22 +1,41 @@
 require_relative 'aws_base_resource_mock'
 
 class AwsVPNConnectionMock < AwsBaseResourceMock
-  attr_reader :state, :type, :vpc_attachments, :vpn_gateway_id, :tags, :vpn_connection_id
+  
+  attr_reader :customer_gateway_configuration, :customer_gateway_id, :category,
+              :state, :type, :vpc_attachments, :vpn_gateway_id, :tags, :vpn_connection_id,
+              :transit_gateway_id, :core_network_arn, :core_network_attachment_arn, :gateway_association_state
 
   def initialize
     super
+    @customer_gateway_configuration = 'test_customer_gateway_configuration'
+    @customer_gateway_id = 'test_customer_gateway_id'
+    @category = 'test_category'
     @state = 'available'
     @type = 'ipsec.1'
-    @vpn_connection_id = 'vc-014a12345'
+    @vpn_connection_id = 'vpn-014a12345'
     @vpn_gateway_id = 'vgw-014a123456789abcde'
+    @transit_gateway_id = 'test_transit_gateway_id'
+    @core_network_arn = 'test_core_network_arn'
+    @core_network_attachment_arn = 'test_core_network_attachment_arn'
+    @gateway_association_state = 'associated'
     @tags = [{ key: 'Name', value: 'my-vpn' }]
+    
     @vpn_connection = {
       vpn_connections: [{
-        state: state,
-        type: type,
-        vpn_connection_id: vpn_connection_id,
-        tags: tags,
-      }],
+                          customer_gateway_configuration: customer_gateway_configuration,
+                          customer_gateway_id: customer_gateway_id,
+                          category: category,
+                          state: state,
+                          type: type,
+                          vpn_connection_id: vpn_connection_id,
+                          vpn_gateway_id: vpn_gateway_id,
+                          transit_gateway_id: transit_gateway_id,
+                          core_network_arn: core_network_arn,
+                          core_network_attachment_arn: core_network_attachment_arn,
+                          gateway_association_state: gateway_association_state,
+                          tags: tags,
+                        }],
     }
   end
 
