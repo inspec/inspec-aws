7994706d2b5153bbf5ac0211580e824ab98c4ddf
sa-progress <sanand@progress.com>
2023-08-01 16:16:40 +0530

    sample profile for s3 bucket added
    Signed-off-by: sa-progress <sanand@progress.com>

--- sample_profile/controls/example.rb
+++ sample_profile/controls/example.rb
@@ -1,19 +1,19 @@
-# Copyright: 2018, The Authors
 
-title "Sample Section"
+control 'aws-ec2-capacity-reservations-1.0' do
+  title 'Describes one or more of your Capacity Reservations.'
 
-# Plural resources can be inspected to check for specific resource details
-control "aws-vpcs-multi-region-status-check" do                             # A unique ID for this control.
-  impact 1.0                                                                # The criticality, if this control fails.
-  title 'Check AWS VPCs in all regions have status "available"'             # A human-readable title.
-  aws_regions.region_names.each do |region|                                 # Loop over all available AWS regions
-    aws_vpcs(aws_region: region).vpc_ids.each do |vpc|                      # Find all VPCs in a single AWS region
-      describe aws_vpc(aws_region: region, vpc_id: vpc) do                  # The test itself.
-        it { should exist }                                                 # Confirms AWS VPC exists
-        it { should be_available }                                          # Confirms AWS VPC has status "available"
-      end
-    end
+  describe aws_ec2_capacity_reservations do
+    it { should exist }
+  end
+
+  describe aws_ec2_capacity_reservations do
+    its('tenancies') { should include 'default' }
+    its('total_instance_counts') { should include 1 }
+    its('ebs_optimized') { should include false }
+    its('ephemeral_storages') { should include false }
+    its('states') { should include 'active' }
+    its('end_date_types') { should include 'unlimited' }
+    its('instance_match_criterias') { should include 'open' }
+    its('tags') { should_not be_empty }
   end
 end
-
-
