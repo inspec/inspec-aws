f60b682535c96dd90a6b2cfa45236b939f9650c9
Rob de Heer <rdeheer@Robs-MacBook-Pro-2.local>
2023-09-17 11:09:45 -0700

    Added support for aws waf v2

---
+++ libraries/aws_wafv2_sql_injection_match_set.rb
@@ -0,0 +1,54 @@
+require "aws_backend"
+
+class AWSWAFV2SQLInjectionMatchSet < AwsResourceBase
+  name "aws_wafv2_sql_injection_match_set"
+  desc "Describes one WAF SQL Injection match set."
+
+  example "
+    describe aws_wafv2_sql_injection_match_set(sql_injection_match_set_id: 'SQL_INJECTION_MATCH_SET_ID') do
+      it { should exits }
+    end
+  "
+
+  def initialize(opts = {})
+    opts = { sql_injection_match_set_id: opts } if opts.is_a?(String)
+    super(opts)
+    validate_parameters(required: %i(sql_injection_match_set_id))
+    raise ArgumentError, "#{@__resource_name__}: sql_injection_match_set_id must be provided" unless opts[:sql_injection_match_set_id] && !opts[:sql_injection_match_set_id].empty?
+    @display_name = opts[:sql_injection_match_set_id]
+    catch_aws_errors do
+      resp = @aws.waf_client_v2.get_sql_injection_match_set({ sql_injection_match_set_id: opts[:sql_injection_match_set_id] })
+      @resp = resp.sql_injection_match_set.to_h
+      create_resource_methods(@resp)
+    end
+  end
+
+  def sql_injection_match_set_id
+    return nil unless exists?
+    @resp[:sql_injection_match_set_id]
+  end
+
+  def exists?
+    !@resp.nil? && !@resp.empty?
+  end
+
+  def to_s
+    "SQL Injection Match Set ID: #{@display_name}"
+  end
+
+  def sql_injection_match_tuples_field_to_match_types
+    sql_injection_match_tuples.map(&:field_to_match).map(&:type)
+  end
+
+  def sql_injection_match_tuples_field_to_match_data
+    sql_injection_match_tuples.map(&:field_to_match).map(&:data)
+  end
+
+  def sql_injection_match_tuples_text_transformations
+    sql_injection_match_tuples.map(&:text_transformation)
+  end
+
+  def resource_id
+    @display_name
+  end
+end
---
+++ libraries/aws_wafv2_web_acls.rb
@@ -0,0 +1,37 @@
+require "aws_backend"
+
+class AWSWAFV2WebACLS < AwsResourceBase
+  name "aws_wafv2_web_acls"
+  desc "Verifies settings for all the Web ACLs."
+
+  example "
+    describe aws_wafv2_web_acls do
+      it { should exist }
+    end
+  "
+
+  attr_reader :table
+
+  def initialize(opts = {})
+    super(opts)
+    validate_parameters
+    @table = fetch_data
+  end
+
+  FilterTable.create
+    .register_column(:web_acl_ids, field: :web_acl_id, style: :simple)
+    .register_column(:names, field: :name, style: :simple)
+    .install_filter_methods_on_resource(self, :table)
+
+  def fetch_data
+    catch_aws_errors do
+      @resp = @aws.waf_client_v2.list_web_acls.map do |table|
+        table.map { |table_name| {
+          web_acl_id: table_name.web_acls.map(&:web_acl_id),
+          name: table_name.web_acls.map(&:name),
+        }
+        }
+      end.flatten
+    end
+  end
+end
