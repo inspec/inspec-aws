fa773f97de79c5ef096b0218c4202de812d63282
Aaron Lippold <lippold@gmail.com>
2023-11-17 20:50:48 -0500

    standardized classes and removed filter tables
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- libraries/aws_account.rb
+++ libraries/aws_account.rb
@@ -1,4 +1,5 @@
 require "aws_backend"
+# require "pry"
 
 class AwsPrimaryAccount < AwsResourceBase
   name "aws_primary_contact"
@@ -11,81 +12,81 @@ class AwsPrimaryAccount < AwsResourceBase
     end
   EXAMPLE
 
-  attr_reader :table, :raw_data
-
-  FilterTable
-    .create
-    .register_column(:address_line_1, field: :address_line_1, style: :simple)
-    .register_column(:adress_line_2, field: :adress_line_2, style: :simple)
-    .register_column(:address_line_3, field: :address_line_3, style: :simple)
-    .register_column(:city, field: :city, style: :simple)
-    .register_column(:company_name, field: :company_name, style: :simple)
-    .register_column(:country_code, field: :country_code, style: :simple)
-    .register_column(
-      :district_or_county,
-      field: :district_or_county,
-      style: :simple
-    )
-    .register_column(:full_name, field: :full_name, style: :simple)
-    .register_column(:phone_number, field: :phone_number, style: :simple)
-    .register_column(:postal_code, field: :postal_code, style: :simple)
-    .register_column(:state_or_region, field: :state_or_region, style: :simple)
-    .register_column(:website_url, field: :website_url, style: :simple)
-    .register_custom_matcher(:configured?) { |x| x.entries.any? }
-    .install_filter_methods_on_resource(self, :table)
+  attr_reader :raw_data,
+              :api_response,
+              :address_line_1,
+              :address_line_2,
+              :address_line_3,
+              :city,
+              :country_code,
+              :company_name,
+              :district_or_county,
+              :full_name,
+              :phone_number,
+              :postal_code,
+              :state_or_region,
+              :website_url,
+              :aws_account_id
 
   def initialize(opts = {})
+    @raw_data = {}
     super(opts)
     validate_parameters
     begin
-      @aws.account_client.get_contact_information
+      catch_aws_errors { @aws_account_id = fetch_aws_account }
+      @api_response =
+        @aws.account_client.get_contact_information.contact_information || nil
     rescue Aws::Account::Errors::ResourceNotFoundException
       skip_resource(
         "The Primary contact has not been configured for this AWS Account."
       )
-      @failed_resource = true
-      return
+      return [] if !@api_response || @api_response.empty?
     end
-    @table = fetch_data
+
+    unless @api_response.nil?
+      @api_response
+        .members
+        .map(&:to_s)
+        .each do |key|
+          instance_variable_set("@#{key}", @api_response.send(key))
+        end
+      @raw_data = @api_response.to_h.transform_keys(&:to_s)
+    else
+      @address_line_1,
+      @address_line_2,
+      @address_line_3,
+      @city,
+      @country_code,
+      @company_name,
+      @district_or_county,
+      @full_name,
+      @phone_number,
+      @postal_code,
+      @state_or_region,
+      @website_url =
+        nil
+    end
+  end
+
+  def configured?
+    !@api_response.nil? || !@raw_data
   end
 
   def resource_id
-    "AWS Account for #{@table[0][:full_name]}" ||
-      "AWS Account Contact Information"
+    if @aws_account_id
+      "AWS Primary Contact for account: #{@aws_account_id}"
+    else
+      "AWS Account Primary Contact Information"
+    end
   end
 
   def to_s
-    "AWS Account Primary Contact"
-  end
-
-  def fetch_data
-    @raw_data = []
-    loop do
-      catch_aws_errors do
-        @api_response =
-          @aws.account_client.get_contact_information.contact_information
-      end
-      return [] if !@api_response || @api_response.empty?
-
-      @raw_data << {
-        address_line_1: @api_response.address_line_1,
-        address_line_2: @api_response.address_line_2,
-        address_line_3: @api_response.address_line_3,
-        city: @api_response.city,
-        company_name: @api_response.company_name,
-        country_code: @api_response.country_code,
-        district_or_county: @api_response.district_or_county,
-        full_name: @api_response.full_name,
-        phone_number: @api_response.phone_number,
-        postal_code: @api_response.postal_code,
-        state_or_region: @api_response.state_or_region,
-        website_url: @api_response.website_url
-      }
-      break
+    if @aws_account_id
+      "AWS Primary Contact for account: #{@aws_account_id}"
+    else
+      "AWS Account Primary Contact"
     end
-    @raw_data
   end
-
   class AwsBillingAccount < AwsResourceBase
     name "aws_billing_contact"
     desc "Verifies the billing contact information for an AWS Account."
@@ -97,66 +98,76 @@ class AwsPrimaryAccount < AwsResourceBase
       end
     EXAMPLE
 
-    attr_reader :table, :raw_data
-
-    FilterTable
-      .create
-      .register_column(:email_address, field: :email_address, style: :simple)
-      .register_column(:name, field: :name, style: :simple)
-      .register_column(:phone_number, field: :phone_number, style: :simple)
-      .register_column(:title, field: :title, style: :simple)
-      .register_custom_matcher(:configured?) { |x| x.entries.any? }
-      .install_filter_methods_on_resource(self, :table)
+    attr_reader :raw_data,
+                :api,
+                :api_response,
+                :email_address,
+                :name,
+                :phone_number,
+                :title,
+                :aws_account_id
 
     def initialize(opts = {})
       super(opts)
+      @raw_data = {}
       validate_parameters
       begin
-        @aws.account_client.get_alternate_contact(
-          { alternate_contact_type: "BILLING" }
-        )
+        catch_aws_errors { @aws_account_id = fetch_aws_account }
+        @api_response = fetch_aws_alternate_contact(@aws, "billing")
       rescue Aws::Account::Errors::ResourceNotFoundException
         skip_resource(
           "The BILLING contact has not been configured for this AWS Account."
         )
-        @failed_resource = true
-        return
+        return [] if !@api_response || @api_response.empty?
       end
-      @table = fetch_data
+
+      unless @api_response.nil?
+        @api_response
+          .members
+          .map(&:to_s)
+          .each do |key|
+            instance_variable_set("@#{key}", @api_response.send(key))
+          end
+        @raw_data = @api_response.to_h.transform_keys(&:to_s)
+      else
+        @name, @email_address, @phone_number, @title = nil
+      end
+    end
+
+    def configured?
+      !@api_response.nil? || !@raw_data
     end
 
     def resource_id
-      "AWS Billing for #{@table[0][:name]}" || "AWS Account Billing Information"
+      if @aws_account_id
+        "AWS Billing Contact for account: #{@aws_account_id}"
+      else
+        "AWS Billing Contact Information"
+      end
     end
 
     def to_s
-      "AWS Account Billing Contact"
+      if @aws_account_id
+        "AWS Billing Contact for account: #{@aws_account_id}"
+      else
+        "AWS Account Primary Contact"
+      end
     end
 
-    def fetch_data
-      @raw_data = []
-      loop do
-        catch_aws_errors do
-          @api_response =
-            @aws
-              .account_client
-              .get_alternate_contact({ alternate_contact_type: "BILLING" })
-              .alternate_contact
-        end
-        return [] if !@api_response || @api_response.empty?
+    private
 
-        @raw_data << {
-          email_address: @api_response.email_address,
-          name: @api_response.name,
-          phone_number: @api_response.phone_number,
-          title: @api_response.title
-        }
-        break
-      end
-      @raw_data
+    def fetch_aws_account
+      arn = @aws.sts_client.get_caller_identity({}).arn
+      arn.split(":")[4]
+    end
+
+    def fetch_aws_alternate_contact(aws_client, type)
+      aws_client
+        .account_client
+        .get_alternate_contact({ alternate_contact_type: "#{type.uppercase}" })
+        .alternate_contact
     end
   end
-
   class AwsAccountOperationsContact < AwsResourceBase
     name "aws_operations_contact"
     desc "Verifies the operations contact information for an AWS Account."
@@ -168,67 +179,76 @@ class AwsPrimaryAccount < AwsResourceBase
       end
     EXAMPLE
 
-    attr_reader :table, :raw_data
-
-    FilterTable
-      .create
-      .register_column(:email_address, field: :email_address, style: :simple)
-      .register_column(:name, field: :name, style: :simple)
-      .register_column(:phone_number, field: :phone_number, style: :simple)
-      .register_column(:title, field: :title, style: :simple)
-      .register_custom_matcher(:configured?) { |x| x.entries.any? }
-      .install_filter_methods_on_resource(self, :table)
+    attr_reader :raw_data,
+                :api,
+                :api_response,
+                :email_address,
+                :name,
+                :phone_number,
+                :title,
+                :aws_account_id
 
     def initialize(opts = {})
       super(opts)
+      @raw_data = {}
       validate_parameters
       begin
-        @aws.account_client.get_alternate_contact(
-          { alternate_contact_type: "OPERATIONS" }
-        )
+        catch_aws_errors { @aws_account_id = fetch_aws_account }
+        @api_response = fetch_aws_alternate_contact(@aws, "operations")
       rescue Aws::Account::Errors::ResourceNotFoundException
         skip_resource(
           "The Operations contact has not been configured for this AWS Account."
         )
-        @failed_resource = true
-        return
+        return [] if !@api_response || @api_response.empty?
       end
-      @table = fetch_data
+
+      unless @api_response.nil?
+        @api_response
+          .members
+          .map(&:to_s)
+          .each do |key|
+            instance_variable_set("@#{key}", @api_response.send(key))
+          end
+        @raw_data = @api_response.to_h.transform_keys(&:to_s)
+      else
+        @name, @email_address, @phone_number, @title = nil
+      end
+    end
+
+    def configured?
+      !@api_response.nil? || !@raw_data
     end
 
     def resource_id
-      "AWS Operations Contact for #{@table[0][:name]}" ||
-        "AWS Account Operations Contact Information"
+      if @aws_account_id
+        "AWS Operations Contact for #{@aws_account_id}"
+      else
+        "AWS Operations Contact Information"
+      end
     end
 
     def to_s
-      "AWS Account Operations Contact Information"
+      if @aws_account_id
+        "AWS Account Operations Contact for #{@aws_account_id}"
+      else
+        "AWS Account Operations Contact"
+      end
     end
 
-    def fetch_data
-      @raw_data = []
-      loop do
-        catch_aws_errors do
-          @api_response =
-            @aws
-              .account_client
-              .get_alternate_contact({ alternate_contact_type: "OPERATIONS" })
-              .alternate_contact
-        end
-        return [] if !@api_response || @api_response.empty?
+    private
 
-        @raw_data << {
-          email_address: @api_response.email_address,
-          name: @api_response.name,
-          phone_number: @api_response.phone_number,
-          title: @api_response.title
-        }
-        break
-      end
-      @raw_data
+    def fetch_aws_account
+      arn = @aws.sts_client.get_caller_identity({}).arn
+      arn.split(":")[4]
+    end
+
+    def fetch_aws_alternate_contact(aws_client, type)
+      aws_client
+        .account_client
+        .get_alternate_contact({ alternate_contact_type: "#{type.uppercase}" })
+        .alternate_contact
     end
   end
-
   class AwsAccountSecurityContact < AwsResourceBase
     name "aws_security_contact"
     desc "Verifies the security contact information for an AWS Account."
@@ -240,64 +260,81 @@ class AwsPrimaryAccount < AwsResourceBase
       end
     EXAMPLE
 
-    attr_reader :table, :raw_data
-
-    FilterTable
-      .create
-      .register_column(:email_address, field: :email_address, style: :simple)
-      .register_column(:name, field: :name, style: :simple)
-      .register_column(:phone_number, field: :phone_number, style: :simple)
-      .register_column(:title, field: :title, style: :simple)
-      .register_custom_matcher(:configured?) { |x| x.entries.any? }
-      .install_filter_methods_on_resource(self, :table)
+    attr_reader :raw_data,
+                :api,
+                :api_response,
+                :email_address,
+                :name,
+                :phone_number,
+                :title,
+                :aws_account_id
 
     def initialize(opts = {})
       super(opts)
+      @raw_data = {}
       validate_parameters
       begin
-        @aws.account_client.get_alternate_contact(
-          { alternate_contact_type: "SECURITY" }
-        )
+        catch_aws_errors { @aws_account_id = fetch_aws_account }
+        @api_response = fetch_aws_alternate_contact(@aws, "security")
       rescue Aws::Account::Errors::ResourceNotFoundException
         skip_resource(
           "The Security contact has not been configured for this AWS Account."
         )
-        @failed_resource = true
-        return
+        return [] if !@api_response || @api_response.empty?
       end
-      @table = fetch_data
+
+      unless @api_response.nil?
+        @api_response
+          .members
+          .map(&:to_s)
+          .each do |key|
+            instance_variable_set("@#{key}", @api_response.send(key))
+          end
+        @raw_data = @api_response.to_h.transform_keys(&:to_s)
+      else
+        @name, @email_address, @phone_number, @title = nil
+      end
+    end
+
+    def configured?
+      !@api_response.nil? || !@raw_data
     end
 
     def resource_id
-      "AWS Security Contact for #{@table[0][:name]}" ||
-        "AWS Account Security Contact Information"
+      if @aws_account_id
+        "AWS Security Contact for account: #{@aws_account_id}"
+      else
+        "AWS Security Contact Information"
+      end
     end
 
     def to_s
-      "AWS Account Security Contact"
-    end
-
-    def fetch_data
-      @raw_data = []
-      loop do
-        catch_aws_errors do
-          @api_response =
-            @aws
-              .account_client
-              .get_alternate_contact({ alternate_contact_type: "SECURITY" })
-              .alternate_contact
-        end
-        return [] if !@api_response || @api_response.empty?
-
-        @raw_data << {
-          email_address: @api_response.email_address,
-          name: @api_response.name,
-          phone_number: @api_response.phone_number,
-          title: @api_response.title
-        }
-        break
+      if @aws_account_id
+        "AWS Security Contact for account: #{@aws_account_id}"
+      else
+        "AWS Account Security Contact"
       end
-      @raw_data
     end
+
+    private
+
+    def fetch_aws_account
+      arn = @aws.sts_client.get_caller_identity({}).arn
+      arn.split(":")[4]
+    end
+  end
+
+  def fetch_aws_alternate_contact(aws_client, type)
+    aws_client
+      .account_client
+      .get_alternate_contact({ alternate_contact_type: "#{type.uppercase}" })
+      .alternate_contact
+  end
+
+  private
+
+  def fetch_aws_account
+    arn = @aws.sts_client.get_caller_identity({}).arn
+    arn.split(":")[4]
   end
 end
