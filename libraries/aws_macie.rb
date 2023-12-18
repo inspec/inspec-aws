75aa2a86db8715780aabceadfd1d94ecf035eec4
wdower <57142072+wdower@users.noreply.github.com>
2023-12-18 23:50:24 +0000

    added a jobs filtertable to macie resource
    Signed-off-by: wdower <57142072+wdower@users.noreply.github.com>

--- libraries/aws_macie.rb
+++ libraries/aws_macie.rb
@@ -1,5 +1,40 @@
 require "aws_backend"
 
+class AwsMacieJobTable
+
+  FilterTable.create
+    .register_column(:bucket_criteria,         field: :bucket_criteria)
+    .register_column(:bucket_definitions,             field: :bucket_definitions)
+    .register_column(:created_at,     field: :created_at)
+    .register_column(:job_id,   field: :job_id)
+    .register_column(:job_status,         field: :job_status)
+    .register_column(:job_type,           field: :job_type)
+    .register_column(:last_run_error_status,        field: :last_run_error_status)
+    .register_column(:name,        field: :name)
+    .register_column(:user_paused_details, field: :user_paused_details)
+    .install_filter_methods_on_resource(self, :job_table)
+
+  attr_reader :job_table, :job_name
+
+  def monitoring?(buckets)
+    self.bucket_definitions.any? { |bd| 
+      bd.any? { |account|
+        (buckets - account[:buckets]).empty?  
+      }
+    }
+  end
+
+  def initialize(job_table, job_name = nil)
+    @job_table = job_table
+    @job_name = job_name
+  end
+
+  def to_s
+    @job_name
+  end
+end
+
+
 class AWSMacie < AwsResourceBase
   name "aws_macie"
   desc "Gets information about Macie status and configuration."
@@ -11,7 +46,7 @@ class AWSMacie < AwsResourceBase
     end
   "
 
-  attr_reader :session, :jobs, :buckets, :organization_configuration
+  attr_reader:buckets, :organization_configuration
 
   def initialize(opts = {})
     @raw_data = {}
@@ -27,7 +62,9 @@ class AWSMacie < AwsResourceBase
       begin
         @session = @aws.macie_client.get_macie_session
         @jobs = @aws.macie_client.list_classification_jobs
-        @buckets = @aws.macie_client.describe_buckets
+        @jobs_table = []
+        @buckets = @aws.macie_client.describe_buckets.buckets
+        @buckets_table = []
       rescue Aws::Errors::NoSuchEndpointError
         skip_resource(
           "The account contact endpoint is not available in this segment, please review this via the AWS Management Console.",
@@ -39,6 +76,19 @@ class AWSMacie < AwsResourceBase
     end
   end
 
+  def jobs
+    return [] unless @jobs
+    AwsMacieJobTable.new(@jobs.items.map(&:to_h))
+  end
+
+  def monitoring_buckets?(buckets)
+    return false unless @jobs
+    b = [buckets] unless buckets.is_a?(Array)
+    jobs.monitoring?(b)
+  end
+
+  alias monitoring_bucket? monitoring_buckets? 
+  
   # jobs.items.first.bucket_definitions.first.to_h[:buckets]
   # aws.macie_client.describe_buckets.buckets.count
   # aws.macie_client.list_classification_jobs.items.first['bucket_definitions']
@@ -72,5 +122,12 @@ class AWSMacie < AwsResourceBase
 
   def to_s
     "AWS Macie"
+  end
+
+  private
+
+  def fetch_aws_region
+    arn = @aws.sts_client.get_caller_identity({}).arn
+    arn.split(":")[1]
   end
 end
