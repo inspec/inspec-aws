7486e2403fa8ab604bee3fc07c7177706645afd1
wdower <57142072+wdower@users.noreply.github.com>
2023-12-19 21:08:42 +0000

    working out findings table in macie
    Signed-off-by: wdower <57142072+wdower@users.noreply.github.com>

--- libraries/aws_macie.rb
+++ libraries/aws_macie.rb
@@ -3,23 +3,23 @@ require "aws_backend"
 class AwsMacieJobTable
 
   FilterTable.create
-    .register_column(:bucket_criteria,         field: :bucket_criteria)
+    .register_column(:bucket_criteria, field: :bucket_criteria)
     .register_column(:bucket_definitions, field: :bucket_definitions)
-    .register_column(:created_at,     field: :created_at)
-    .register_column(:job_id,   field: :job_id)
+    .register_column(:created_at, field: :created_at)
+    .register_column(:job_id, field: :job_id)
     .register_column(:job_status,         field: :job_status)
     .register_column(:job_type,           field: :job_type)
-    .register_column(:last_run_error_status,        field: :last_run_error_status)
-    .register_column(:name,        field: :name)
+    .register_column(:last_run_error_status, field: :last_run_error_status)
+    .register_column(:name, field: :name)
     .register_column(:user_paused_details, field: :user_paused_details)
     .install_filter_methods_on_resource(self, :job_table)
 
   attr_reader :job_table, :job_name
 
   def monitoring?(buckets)
-    self.entries.each do |job|
+    entries.each do |job|
       job[:bucket_definitions].each do |bd|
-        buckets = buckets - bd[:buckets] 
+        buckets -= bd[:buckets]
       end
     end
     buckets.empty?
@@ -40,12 +40,12 @@ class AwsMacieBucketTable
   FilterTable.create
     .register_column(:account_id,         field: :account_id)
     .register_column(:allows_unencrypted_object_uploads, field: :allows_unencrypted_object_uploads)
-    .register_column(:bucket_arn,     field: :bucket_arn)
+    .register_column(:bucket_arn, field: :bucket_arn)
     .register_column(:bucket_created_at,   field: :bucket_created_at)
     .register_column(:bucket_name,         field: :bucket_name)
-    .register_column(:classifiable_object_count,           field: :classifiable_object_count)
-    .register_column(:classifiable_size_in_bytes,        field: :classifiable_size_in_bytes)
-    .register_column(:error_code,        field: :error_code)
+    .register_column(:classifiable_object_count, field: :classifiable_object_count)
+    .register_column(:classifiable_size_in_bytes, field: :classifiable_size_in_bytes)
+    .register_column(:error_code, field: :error_code)
     .register_column(:error_message, field: :error_message)
     .register_column(:job_details, field: :job_details)
     .register_column(:last_automated_discovery_time, field: :last_automated_discovery_time)
@@ -78,40 +78,40 @@ class AwsMacieBucketTable
   end
 end
 
-# class AwsMacieFindingTable
+class AwsMacieFindingTable
 
-#   FilterTable.create
-#     .register_column(:account_id,         field: :account_id)
-#     .register_column(:archived, field: :archived)
-#     .register_column(:category,     field: :category)
-#     .register_column(:classification_details,   field: :classification_details)
-#     .register_column(:count,         field: :count)
-#     .register_column(:created_at,           field: :created_at)
-#     .register_column(:description,        field: :description)
-#     .register_column(:id,        field: :id)
-#     .register_column(:partition, field: :partition)
-#     .register_column(:policy_details, field: :policy_details)
-#     .register_column(:region, field: :region)
-#     .register_column(:resources_affected, field: :resources_affected)
-#     .register_column(:sample, field: :sample)
-#     .register_column(:schema_version, field: :schema_version)
-#     .register_column(:severity, field: :severity)
-#     .register_column(:title, field: :title)
-#     .register_column(:type, field: :type)
-#     .register_column(:updated_at, field: :updated_at)
-#     .install_filter_methods_on_resource(self, :job_table)
+  FilterTable.create
+    .register_column(:account_id, field: :account_id)
+    .register_column(:archived, field: :archived)
+    .register_column(:category, field: :category)
+    .register_column(:classification_details, field: :classification_details)
+    .register_column(:count, field: :count)
+    .register_column(:created_at, field: :created_at)
+    .register_column(:description, field: :description)
+    .register_column(:id,        field: :id)
+    .register_column(:partition, field: :partition)
+    .register_column(:policy_details, field: :policy_details)
+    .register_column(:region, field: :region)
+    .register_column(:resources_affected, field: :resources_affected)
+    .register_column(:sample, field: :sample)
+    .register_column(:schema_version, field: :schema_version)
+    .register_column(:severity, field: :severity)
+    .register_column(:title, field: :title)
+    .register_column(:type, field: :type)
+    .register_column(:updated_at, field: :updated_at)
+    .install_filter_methods_on_resource(self, :job_table)
 
-#   attr_reader :findings_table, :findings_name
+  attr_reader :findings_table, :findings_name
 
-#   def initialize(findings_table, findings_name = nil)
-#     @findings_table = findings_table
-#     @findings_name = findings_name
-#   end
+  def initialize(findings_table, findings_name = nil)
+    @findings_table = findings_table
+    @findings_name = findings_name
+  end
 
-#   def to_s
-#     @findings_name.present? ? @findings_name : "AWS Macie Findings"
-#   end
-# end
+  def to_s
+    @findings_name.present? ? @findings_name : "AWS Macie Findings"
+  end
+end
 
 class AWSMacie < AwsResourceBase
   name "aws_macie"
@@ -123,8 +123,6 @@ class AWSMacie < AwsResourceBase
       it { should be_monitoring(['arn1', 'arn2', 'arn3']) }
     end
   "
-
-  attr_reader:buckets, :organization_configuration
 
   def initialize(opts = {})
     @raw_data = {}
@@ -143,16 +141,14 @@ class AWSMacie < AwsResourceBase
         @jobs.present? ? @jobs_table = AwsMacieJobTable.new(@jobs.items.map(&:to_h)) : @jobs_table = []
         @buckets = @aws.macie_client.describe_buckets
         @buckets.present? ? @buckets_table = AwsMacieBucketTable.new(@buckets.buckets.map(&:to_h)) : @buckets_table = []
-        # @findings = @aws.macie_client.get_findings
-        # @findings.present? ? @findings_table = AwsMacieFindingTable.new(@findings.findings.map(&:to_h)) : @findings_table = []
+        @findings = []
+        @findings_table = []
       rescue Aws::Errors::NoSuchEndpointError
         skip_resource(
           "The account contact endpoint is not available in this segment, please review this via the AWS Management Console.",
         )
       end
       return [] if (!@session or @session.empty?) || (!@jobs or @jobs.empty?) || (!@bucket or @bucket.empty?)
-      # Can't do this until we setup the user to be a Macie Admin?
-      # @organization_configuration = @aws.macie_client.describe_organization_configuration
     end
   end
 
@@ -169,8 +165,16 @@ class AWSMacie < AwsResourceBase
     @buckets_table
   end
 
-  def findings
-    @findings_table
+  def findings(finding_ids, sort_criteria = nil)
+    catch_aws_errors do
+      begin
+        require "pry"
+        pry
+        findings = @aws.macie_client.get_findings(finding_ids, sort_criteria)
+
+        findings.present? ? AwsMacieFindingTable.new(@findings.findings.map(&:to_h)) : []
+      end
+    end
   end
 
   def monitoring_buckets?(buckets)
@@ -179,28 +183,7 @@ class AWSMacie < AwsResourceBase
     jobs.monitoring?(b)
   end
 
-  alias monitoring_bucket? monitoring_buckets? 
-  
-  # jobs.items.first.bucket_definitions.first.to_h[:buckets]
-  # aws.macie_client.describe_buckets.buckets.count
-  # aws.macie_client.list_classification_jobs.items.first['bucket_definitions']
-
-  # it may be more strait forward to have multiple small resources so this one doesn't get hug
-
-  # aws_macie base resource
-  #   - this may have 'session' method via get_macie_session
-  #   - members via list_members
-  #   - list_organization_admin_accounts ?
-  #   - perhaps owner etc.
-  #   - usuage_totals - via get_usage_totals
-  # aws_macie_jobs ...
-  # aws_macie_job(job_id) ...
-  # aws_macie_buckets
-  # aws_macie_buckets(bucket_name or arn)
-  # aws_macie_findings
-  #   - list_findings
-  # aws_macie_finding
-  #   - get_finding_statistics(finding_id)
+  alias monitoring_bucket? monitoring_buckets?
 
   def enabled?
     @session.status == "ENABLED"
